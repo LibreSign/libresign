7580e5e490f3877ea69694e35cda7aab0a7d1d49
Vitor Mattos <vitor@php.rio>
2021-01-10 22:17:01 -0300

    Upgrade to Nextcloud 20

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -6,20 +6,29 @@ use OCA\Files\Event\LoadSidebar;
 use OCA\Libresign\Listener\LoadSidebarListener;
 use OCA\Libresign\Storage\ClientStorage;
 use OCP\AppFramework\App;
-use OCP\EventDispatcher\IEventDispatcher;
+use OCP\AppFramework\Bootstrap\IBootContext;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
-class Application extends App
+class Application extends App implements IBootstrap
 {
     public const APP_ID = 'libresign';
 
     public function __construct()
     {
         parent::__construct(self::APP_ID);
-        $container = $this->getContainer();
-        $dispatcher = $container->query(IEventDispatcher::class);
-        $dispatcher->addServiceListener(LoadSidebar::class, LoadSidebarListener::class);
+    }
 
-        $container->registerService(ClientStorage::class, function ($c) {
+    public function boot(IBootContext $context): void {
+    }
+
+    public function register(IRegistrationContext $context): void {
+        $context->registerEventListener(
+            LoadSidebar::class,
+            LoadSidebarListener::class
+        );
+
+        $context->registerService(ClientStorage::class, function ($c) {
             return new ClientStorage(
                 $c->query('ServerContainer')->getUserFolder()
             );
