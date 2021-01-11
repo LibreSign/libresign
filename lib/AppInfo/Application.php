b8f58c38be8e387df62e4e66790990e382c63c52
Vitor Mattos <vitor@php.rio>
2021-01-11 08:22:13 -0300

    Autoload

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -23,6 +23,7 @@ class Application extends App implements IBootstrap
     }
 
     public function register(IRegistrationContext $context): void {
+        include_once __DIR__ . '/../../vendor/autoload.php';
         $context->registerEventListener(
             LoadSidebar::class,
             LoadSidebarListener::class
