51509ea26bb2cb08184bde43ffef7ed328161845
Vitor Mattos <vitor@php.rio>
2021-02-08 00:02:49 -0300

    Organize

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -2,13 +2,17 @@
 
 return [
 	'routes' => [
+		// API
 		['name' => 'api#preflighted_cors', 'url' => '/api/0.1/{path}',
 			'verb' => 'OPTIONS', 'requirements' => ['path' => '.+'], ],
 		['name' => 'libresign#sign', 'url' => '/api/0.1/sign', 'verb' => 'POST'],
 		['name' => 'signature#generate', 'url' => '/api/0.1/signature/generate', 'verb' => 'POST'],
 		['name' => 'signature#hasRootCert', 'url' => '/api/0.1/signature/has-root-cert', 'verb' => 'GET'],
+		// Admin config
 		['name' => 'admin#generateCertificate', 'url' => '/api/0.1/admin/certificate', 'verb' => 'POST'],
 		['name' => 'admin#loadCertificate', 'url' => '/api/0.1/admin/certificate', 'verb' => 'GET'],
+
+		// Pages
 		['name' => 'page#index', 'url' => '/', 'verb' => 'GET'],
 		['name' => 'page#external', 'url' => '/external', 'verb' => 'GET'],
 	],
