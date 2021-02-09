baa1f36784cdd85dd2ea9b4df8e8d90d560750cf
Vitor Mattos <vitor@php.rio>
2021-02-09 19:54:39 -0300

    Remove accept dot in filename

--- lib/Service/WebhookService.php
+++ lib/Service/WebhookService.php
@@ -67,8 +67,8 @@ class WebhookService {
 		if (empty($data['name'])) {
 			throw new \Exception((string)$this->l10n->t('Name is mandatory'));
 		}
-		if (!preg_match('/^[\w \-_\.]+$/', $data['name'])) {
-			throw new \Exception((string)$this->l10n->t('The name can only contain "a-z", "A-Z", "0-9" and "-_." chars.'));
+		if (!preg_match('/^[\w \-_]+$/', $data['name'])) {
+			throw new \Exception((string)$this->l10n->t('The name can only contain "a-z", "A-Z", "0-9" and "-_" chars.'));
 		}
 		if (empty($data['file'])) {
 			throw new \Exception((string)$this->l10n->t('Empty file'));
