aec83e609751fd304d02d5e6dfce92363e4cab97
Valdnet <47037905+Valdnet@users.noreply.github.com>
2021-04-26 11:57:36 +0200

    l10n: Change to an ellipsis

--- src/views/AdminFormLibresign.vue
+++ src/views/AdminFormLibresign.vue
@@ -139,7 +139,7 @@ export default {
 	methods: {
 		async generateCertificate() {
 			this.formDisabled = true
-			this.submitLabel = t('libresign', 'Generating certificate...')
+			this.submitLabel = t('libresign', 'Generating certificate …')
 			try {
 				const response = await axios.post(
 					generateUrl('/apps/libresign/api/0.1/admin/certificate'),
