549c03a19d8d2a3b893185c8e89673d3c7ec961e
Vitor Mattos <vitor@php.rio>
2021-05-15 11:38:00 -0300

    Increase coverage

--- tests/Unit/Handler/CfsslHandlerTest.php
+++ tests/Unit/Handler/CfsslHandlerTest.php
@@ -2,14 +2,18 @@
 
 namespace OCA\Libresign\Tests\Unit\Service;
 
+use Http\Client\Exception\TransferException;
 use OCA\Libresign\Handler\CfsslHandler;
+use OCP\Http\Client\IClient;
+use OCP\Http\Client\IClientService;
+use OCP\Http\Client\IResponse;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @internal
  */
 final class CfsslHandlerTest extends TestCase {
-	public function testGenerateCertificateWithInvalidData() {
+	public function testGenerateCertificateWithInvalidHost() {
 		$class = new CfsslHandler();
 		$this->expectErrorMessageMatches('/Could not resolve host/');
 		$class->generateCertificate();
@@ -25,5 +29,91 @@ final class CfsslHandlerTest extends TestCase {
 		$class = new CfsslHandler();
 		$this->expectErrorMessageMatches('/Cannot set non existing property/');
 		$class->fooBar();
+	}
+
+	public function testGenerateCertificateWhenCfsslReturningInvalidData() {
+		$class = new CfsslHandler();
+		$this->expectErrorMessage('Error while generating certificate keys!');
+		$this->expectExceptionCode(500);
+		$response = $this->createMock(IResponse::class);
+		$client = $this->createMock(IClient::class);
+		$client->expects($this->once())
+			->method('post')
+			->willReturn($response);
+		$class->setClient($client);
+		$class->generateCertificate();
+	}
+
+	public function testGenerateCertificateWhenResponseIsWrong() {
+		$class = new CfsslHandler();
+		$this->expectErrorMessageMatches('/Could not resolve host/');
+		$class->generateCertificate();
+	}
+
+	public function testGenerateCertificateWithInvalidCert() {
+		$class = new CfsslHandler();
+		$this->expectExceptionMessage('Error while creating certificate file');
+		$this->expectExceptionCode(500);
+		$response = $this->createMock(IResponse::class);
+		$response->method('getBody')->willReturn(json_encode([
+			'success' => 'success',
+			'result' => 'invalid result'
+		]));
+		$client = $this->createMock(IClient::class);
+		$client->expects($this->once())
+			->method('post')
+			->willReturn($response);
+		$class->setClient($client);
+		$class->generateCertificate();
+	}
+
+	public function testGenerateCertificateWithValidCert() {
+		$class = new CfsslHandler();
+		$response = $this->createMock(IResponse::class);
+		$cert = file_get_contents(__DIR__ . '/mock/cert.json');
+		$response->method('getBody')->willReturn(json_encode([
+			'success' => 'success',
+			'result' => json_decode($cert, true)
+		]));
+		$client = $this->createMock(IClient::class);
+		$client->expects($this->once())
+			->method('post')
+			->willReturn($response);
+		$class->setClient($client);
+		$class->setPassword('password');
+		$pkcs12 = $class->generateCertificate();
+		openssl_pkcs12_read($pkcs12, $actual, 'password');
+		$this->assertArrayHasKey('cert', $actual);
+		$this->assertArrayHasKey('pkey', $actual);
+	}
+
+	public function testHealthWithInvalidUrl() {
+		$class = new CfsslHandler();
+		$this->expectErrorMessageMatches('/Could not resolve host/');
+		$class->health('invalid_url');
+	}
+
+	public function testHealthWithoutSuccess() {
+		$class = new CfsslHandler();
+		$this->expectExceptionMessage('Error while check cfssl API health!');
+		$client = $this->createMock(IClient::class);
+		$class->setClient($client);
+		$class->health('http://cfssl.coop');
+	}
+
+	public function testHealthWithSuccess() {
+		$class = new CfsslHandler();
+		$response = $this->createMock(IResponse::class);
+		$response->method('getBody')->willReturn(json_encode([
+			'success' => 'success',
+			'result' => true
+		]));
+		$client = $this->createMock(IClient::class);
+		$client->expects($this->once())
+			->method('get')
+			->willReturn($response);
+		$class->setClient($client);
+		$actual = $class->health('http://cfssl.coop');
+		$this->assertNotEmpty($actual);
 	}
 }
