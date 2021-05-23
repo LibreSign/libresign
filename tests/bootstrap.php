88154c85c89363951266a930630c8c07e3f23521
Vitor Mattos <vitor@php.rio>
2021-05-23 17:28:50 -0300

    Clean hooks

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,7 +5,14 @@ if (!defined('PHPUNIT_RUN')) {
 }
 
 require_once __DIR__.'/../../../lib/base.php';
+
 require_once __DIR__.'/../vendor/autoload.php';
+
+if (!class_exists('\PHPUnit\Framework\TestCase')) {
+	require_once('PHPUnit/Autoload.php');
+}
 
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 \OC_App::loadApp('libresign');
+
+OC_Hook::clear();
