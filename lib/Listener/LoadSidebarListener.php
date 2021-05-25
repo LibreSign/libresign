3d4c98aa76377fcf5eb3aeaf48a3d715b41d8599
Vitor Mattos <vitor@php.rio>
2021-05-24 22:16:03 -0300

    script version based on nextcloud version

--- lib/Listener/LoadSidebarListener.php
+++ lib/Listener/LoadSidebarListener.php
@@ -17,6 +17,10 @@ class LoadSidebarListener implements IEventListener {
 			return;
 		}
 
-		Util::addScript(Application::APP_ID, 'libresign-tab');
+		if (version_compare(\OC_Util::getVersionString(), '20', '<=')) {
+			Util::addScript(Application::APP_ID, 'libresign-tab-20');
+		} else {
+			Util::addScript(Application::APP_ID, 'libresign-tab');
+		}
 	}
 }
