b645c0cee02e4ade561f3552eb2a6ca7f39e3421
Vitor Mattos <vitor@php.rio>
2021-05-24 22:36:38 -0300

    Add tab script to Nextcloud 20

---
+++ src/tab-20.js
@@ -0,0 +1,37 @@
+/**
+ * @copyright Copyright (c) 2021 Lyseon Techh <contato@lt.coop.br>
+ *
+ * @author Lyseon Tech <contato@lt.coop.br>
+ * @author Vinicios Gomes <viniciosgomesviana@gmail.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+import LibresignTab from './views/LibresignTab'
+
+window.addEventListener('DOMContentLoaded', () => {
+	if (OCA.Files && OCA.Files.Sidebar) {
+		OCA.Files.Sidebar.registerTab(new OCA.Files.Sidebar.Tab('libresign', LibresignTab, (fileInfo) => {
+			if (!fileInfo || fileInfo.isDirectory()) {
+				return false
+			}
+
+			const mimetype = fileInfo.get('mimetype') || ''
+			return mimetype === 'application/pdf'
+		}))
+	}
+})
