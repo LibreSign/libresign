bf1eb4248cfaacfbf2f640c0e44591a5052f9eea
Vitor Mattos <vitor@php.rio>
2021-05-27 14:20:17 -0300

    cs:fix and remove unecessary file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('PHPUNIT_RUN')) {
-    define('PHPUNIT_RUN', 1);
+	define('PHPUNIT_RUN', 1);
 }
 
 require_once __DIR__.'/../../../lib/base.php';
--- tests/dredd/YamlToJson.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Symfony\Component\Yaml\Yaml;
-
-require_once 'vendor/autoload.php';
-$input = file_get_contents($argv[1]);
-$array = Yaml::parse($input);
-echo json_encode($array, JSON_UNESCAPED_SLASHES);
