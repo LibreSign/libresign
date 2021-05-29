b968bf79d75b9d87f880c685db12cb9336995526
Vitor Mattos <vitor@php.rio>
2021-05-29 12:16:45 -0300

    Create user

--- tests/Unit/Controller/SignatureControllerTest.php
+++ tests/Unit/Controller/SignatureControllerTest.php
@@ -5,11 +5,14 @@ namespace OCA\Libresign\Tests\Unit\Controller;
 use ByJG\ApiTools\OpenApi\OpenApiSchema;
 use PHPUnit\Framework\TestCase;
 use OCA\Libresign\Tests\Unit\ApiTestTrait;
+use OCA\Libresign\Tests\Unit\UserTrait;
 use Symfony\Component\Yaml\Yaml;
 
 final class SignatureControllerTest extends TestCase {
 	use ApiTestTrait;
+	use UserTrait;
 	public function setUp(): void {
+		$this->userSetUp();
 		$data = Yaml::parse(file_get_contents('docs/.vuepress/public/specs/api.yaml'));
 		$data['servers'][] = ['url' => 'http://localhost/apps/libresign/api/0.1'];
 		/** @var OpenApiSchema */
@@ -21,10 +24,11 @@ final class SignatureControllerTest extends TestCase {
 	 * @runInSeparateProcess
 	 */
 	public function testHasRootCertReturnSuccess() {
+		$this->createUser('username', 'password');
 		$request = new \OCA\Libresign\Tests\Unit\ApiRequester();
 		$request
 			->withRequestHeader([
-				'Authorization' => 'Basic ' . base64_encode('admin:admin')
+				'Authorization' => 'Basic ' . base64_encode('username:password')
 			])
 			->withPath('/signature/has-root-cert');
 
