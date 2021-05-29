b17c508a4099ac7af210a52293c3cc945debb0d1
Vitor Mattos <vitor@php.rio>
2021-05-29 10:33:31 -0300

    Implement all necessary to run GET request

--- tests/Unit/ApiRequester.php
+++ tests/Unit/ApiRequester.php
@@ -3,6 +3,9 @@
 namespace OCA\Libresign\Tests\Unit;
 
 use ByJG\ApiTools\AbstractRequester;
+use OC\AppFramework\Http\Request;
+use OCP\IRequest;
+use org\bovigo\vfs\vfsStream;
 use Psr\Http\Message\RequestInterface;
 
 /**
@@ -15,5 +18,53 @@ class ApiRequester extends AbstractRequester
      */
     protected function handleRequest(RequestInterface $request)
     {
+        $this->setupRequest($request);
+    }
+
+    private function setupRequest(RequestInterface $request) {
+        $request = $request->withHeader("User-Agent", "ByJG Swagger Test");
+        $server = [
+            'REQUEST_URI' => $request->getUri()->getPath(),
+            'SCRIPT_NAME' => '/index.php',
+            'REQUEST_METHOD' => $request->getMethod(),
+            'SERVER_PROTOCOL' => $request->getProtocolVersion()
+        ];
+        $_SERVER = array_merge($_SERVER, $server);
+        foreach ($request->getHeaders() as $key => $value) {
+            $name = strtoupper($key);
+            $name = str_replace('-', '_', $name);
+            $server['HTTP_' . $name] = $value[0];
+        }
+        if (isset($server['HTTP_AUTHORIZATION'])) {
+            $auth = base64_decode(explode(' ', $server['HTTP_AUTHORIZATION'])[1]);
+            list($server['PHP_AUTH_USER'], $server['PHP_AUTH_PW']) = explode(':', $auth);
+        }
+        parse_str($request->getUri()->getQuery(), $get);
+        $vars = [
+            'get' => $get,
+            'files' => [],
+            'server' => $server,
+            'env' => $_ENV,
+            'cookies' => ['cookie_test' => true],
+            'method' => $server['REQUEST_METHOD'],
+            'urlParams' => $get,
+        ];
+        $stream = '';
+        $mockRequest = new Request(
+            $vars,
+            \OC::$server->get(\OCP\Security\ISecureRandom::class),
+            \OC::$server->get(\OCP\IConfig::class),
+            \OC::$server->get(\OC\Security\CSRF\CsrfTokenManager::class),
+            $stream
+        );
+        \OC::$server->registerService(IRequest::class, function () use ($mockRequest) {
+            return $mockRequest;
+        });
+        \OC::$CLI = false;
+        $log = \OC::$server->get(\OC\Log::class);
+        $router = new \OC\Route\Router($log);
+        \OC::$server->registerService(\OC\Route\Router::class, function () use ($router) {
+            return $router;
+        });
     }
 }
