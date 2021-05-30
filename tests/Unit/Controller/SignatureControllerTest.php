0558853af71d45876c63e3a210ca382c076bd193
Vitor Mattos <vitor@php.rio>
2021-05-30 08:43:15 -0300

    Refactor

--- tests/Unit/Controller/SignatureControllerTest.php
+++ tests/Unit/Controller/SignatureControllerTest.php
@@ -2,22 +2,14 @@
 
 namespace OCA\Libresign\Tests\Unit\Controller;
 
-use ByJG\ApiTools\OpenApi\OpenApiSchema;
-use PHPUnit\Framework\TestCase;
-use OCA\Libresign\Tests\Unit\ApiTestTrait;
+use OCA\Libresign\Tests\Unit\ApiTestCase;
 use OCA\Libresign\Tests\Unit\UserTrait;
-use Symfony\Component\Yaml\Yaml;
 
-final class SignatureControllerTest extends TestCase {
-	use ApiTestTrait;
+final class SignatureControllerTest extends ApiTestCase {
 	use UserTrait;
 	public function setUp(): void {
+		parent::setUp();
 		$this->userSetUp();
-		$data = Yaml::parse(file_get_contents('docs/.vuepress/public/specs/api.yaml'));
-		$data['servers'][] = ['url' => 'http://localhost/apps/libresign/api/0.1'];
-		/** @var OpenApiSchema */
-		$schema = \ByJG\ApiTools\Base\Schema::getInstance($data);
-		$this->setSchema($schema);
 	}
 
 	/**
