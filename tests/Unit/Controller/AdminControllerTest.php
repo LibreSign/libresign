1ed587b7a950bc49bc589423cd4ff2bb08da7159
Vitor Mattos <vitor@php.rio>
2021-05-31 16:23:28 -0300

    Mock config settings

--- tests/Unit/Controller/AdminControllerTest.php
+++ tests/Unit/Controller/AdminControllerTest.php
@@ -3,6 +3,7 @@
 namespace OCA\Libresign\Tests\Unit\Controller;
 
 use donatj\MockWebServer\Response;
+use OCA\Libresign\Tests\lib\AppConfigOverwrite;
 use OCA\Libresign\Tests\Unit\ApiTestCase;
 use org\bovigo\vfs\vfsStream;
 
@@ -31,6 +32,19 @@ final class AdminControllerTest extends ApiTestCase {
 		self::$server->setResponseOfPath('/api/v1/cfssl/health', new Response(
 			'{"success":true,"result":{"healthy":true},"errors":[],"messages":[]}'
 		));
+		$cfsslConfig = [
+			'commonName' => 'CommonName',
+			'country' => 'Brazil',
+			'organization' => 'Organization',
+			'organizationUnit' => 'organizationUnit',
+			'cfsslUri' => self::$server->getServerRoot() . '/api/v1/cfssl/',
+			'configPath' => 'vfs://home/'
+		];
+		\OC::$server->registerService(\OC\AppConfig::class, function () use ($cfsslConfig) {
+			return new AppConfigOverwrite(\OC::$server->get(\OC\DB\Connection::class), [
+				'libresign' => $cfsslConfig
+			]);
+		});
 
 		// Configure request
 		$this->request
@@ -40,17 +54,15 @@ final class AdminControllerTest extends ApiTestCase {
 				'Content-Type' => 'application/json'
 			])
 			->withPath('/admin/certificate')
-			->withRequestBody([
-				'commonName' => 'CommonName',
-				'country' => 'Brazil',
-				'organization' => 'Organization',
-				'organizationUnit' => 'organizationUnit',
-				'cfsslUri' => self::$server->getServerRoot() . '/api/v1/cfssl/',
-				'configPath' => 'vfs://home/'
-			]);
+			->withRequestBody($cfsslConfig);
 
 		// Make and test request mach with schema
 		$this->assertRequest();
+
+		// Test if settings has been saved
+		foreach ($cfsslConfig as $key => $value) {
+			$this->assertEquals(\OC::$server->get(\OC\AllConfig::class)->getAppValue('libresign', $key), $value);
+		}
 
 		// Test result of endpoint
 		$csrServerJson = file_get_contents('vfs://home/csr_server.json');
