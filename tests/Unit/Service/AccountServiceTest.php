32c90f4c7be9ed651723a4d0df825bede3473b91
Vitor Mattos <vitor@php.rio>
2021-06-07 14:26:34 -0300

    Increase coverage

--- tests/Unit/Service/AccountServiceTest.php
+++ tests/Unit/Service/AccountServiceTest.php
@@ -6,6 +6,7 @@ use OCA\Libresign\Db\FileMapper;
 use OCA\Libresign\Db\FileUser;
 use OCA\Libresign\Db\FileUserMapper;
 use OCA\Libresign\Handler\CfsslHandler;
+use OCA\Libresign\Helper\JSActions;
 use OCA\Libresign\Service\AccountService;
 use OCA\Libresign\Service\FolderService;
 use OCA\Libresign\Tests\Unit\UserTrait;
@@ -13,6 +14,7 @@ use OCA\Settings\Mailer\NewUserMailHelper;
 use OCP\Files\IRootFolder;
 use OCP\IConfig;
 use OCP\IL10N;
+use OCP\IURLGenerator;
 use OCP\IUserManager;
 
 /**
@@ -32,8 +34,12 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 	private $config;
 	/** @var NewUserMailHelper */
 	private $newUserMail;
+	/** @var IURLGenerator */
+	private $urlGenerator;
 	/** @var CfsslHandler */
 	private $cfsslHandler;
+	/** @var AccountService */
+	private $service;
 
 	public function setUp(): void {
 		$this->userSetUp();
@@ -48,7 +54,21 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->fileMapper = $this->createMock(FileMapper::class);
 		$this->config = $this->createMock(IConfig::class);
 		$this->newUserMail = $this->createMock(NewUserMailHelper::class);
+		$this->urlGenerator = $this->createMock(IURLGenerator::class);
 		$this->cfsslHandler = $this->createMock(CfsslHandler::class);
+
+		$this->service = new AccountService(
+			$this->l10n,
+			$this->fileUserMapper,
+			$this->userManagerInstance,
+			$this->folder,
+			$this->root,
+			$this->fileMapper,
+			$this->config,
+			$this->newUserMail,
+			$this->urlGenerator,
+			$this->cfsslHandler
+		);
 	}
 
 	/**
@@ -68,6 +88,7 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			$this->fileMapper,
 			$this->config,
 			$this->newUserMail,
+			$this->urlGenerator,
 			$this->cfsslHandler
 		);
 		$this->expectExceptionMessage($expectedErrorMessage);
@@ -211,6 +232,7 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			$this->fileMapper,
 			$this->config,
 			$this->newUserMail,
+			$this->urlGenerator,
 			$this->cfsslHandler
 		);
 		$this->expectExceptionMessage($expectedErrorMessage);
@@ -306,6 +328,7 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			$this->fileMapper,
 			$this->config,
 			$this->newUserMail,
+			$this->urlGenerator,
 			$this->cfsslHandler
 		);
 		$actual = $this->service->validateCreateToSign([
@@ -321,27 +344,15 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->cfsslHandler
 			->method('__call')
 			->will($this->returnValue($this->cfsslHandler));
-		$this->service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$this->folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
 		$this->expectErrorMessage('Failure on generate certificate');
 		$this->service->generateCertificate('uid', 'password', 'username');
 	}
 
 	public function testGenerateCertificateAndSaveToAFolderAndNotAFile() {
-		$folder = $this->createMock(FolderService::class);
 		$node = $this->createMock(\OCP\Files\Folder::class);
 		$node->method('nodeExists')->will($this->returnValue(true));
 		$node->method('get')->will($this->returnValue($node));
-		$folder->method('getFolder')->will($this->returnValue($node));
+		$this->folder->method('getFolder')->will($this->returnValue($node));
 
 		$backend = $this->createMock(\OC\User\Database::class);
 		$backend->method('implementsActions')
@@ -360,17 +371,6 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->cfsslHandler
 			->method('generateCertificate')
 			->will($this->returnValue('raw content of pfx file'));
-		$this->service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
 		$this->expectErrorMessage('path signature.pfx already exists and is not a file!');
 		$this->expectExceptionCode(400);
 		$this->service->generateCertificate('uid', 'password', 'username');
@@ -381,8 +381,7 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$node->method('nodeExists')->will($this->returnValue(true));
 		$file = $this->createMock(\OCP\Files\File::class);
 		$node->method('get')->will($this->returnValue($file));
-		$folder = $this->createMock(FolderService::class);
-		$folder->method('getFolder')->will($this->returnValue($node));
+		$this->folder->method('getFolder')->will($this->returnValue($node));
 
 		$backend = $this->createMock(\OC\User\Database::class);
 		$backend->method('implementsActions')
@@ -401,17 +400,6 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->cfsslHandler
 			->method('generateCertificate')
 			->will($this->returnValue('raw content of pfx file'));
-		$this->service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
 		$actual = $this->service->generateCertificate('uid', 'password', 'username');
 		$this->assertInstanceOf('\OCP\Files\File', $actual);
 	}
@@ -421,9 +409,7 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$node->method('nodeExists')->will($this->returnValue(false));
 		$file = $this->createMock(\OCP\Files\File::class);
 		$node->method('newFile')->will($this->returnValue($file));
-		$folder = $this->createMock(FolderService::class);
-		$folder->method('getFolder')->will($this->returnValue($node));
-		// $folder->method('newFile')->will($this->returnValue($file));
+		$this->folder->method('getFolder')->will($this->returnValue($node));
 
 		$backend = $this->createMock(\OC\User\Database::class);
 		$backend->method('implementsActions')
@@ -442,33 +428,11 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->cfsslHandler
 			->method('generateCertificate')
 			->will($this->returnValue('raw content of pfx file'));
-		$this->service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
 		$actual = $this->service->generateCertificate('uid', 'password', 'username');
 		$this->assertInstanceOf('\OCP\Files\File', $actual);
 	}
 
 	public function testGetPfxWithInvalidUser() {
-		$this->service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$this->folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
 		$this->expectErrorMessage('Backends provided no user object for invalidUser');
 		$this->service->getPfx('invalidUser');
 	}
@@ -485,24 +449,12 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$userManager->clearBackends();
 		$userManager->registerBackend($backend);
 
-		$folder = $this->createMock(FolderService::class);
 		$node = $this->createMock(\OCP\Files\Folder::class);
 		$node->method('nodeExists')->will($this->returnValue(false));
-		$folder->method('getFolder')->will($this->returnValue($node));
-		$service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
+		$this->folder->method('getFolder')->will($this->returnValue($node));
 		$this->expectErrorMessage('Password to sign not defined. Create a password to sign');
 		$this->expectExceptionCode(400);
-		$service->getPfx('userId');
+		$this->service->getPfx('userId');
 	}
 
 	public function testGetPfxOk() {
@@ -517,23 +469,11 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$userManager->clearBackends();
 		$userManager->registerBackend($backend);
 
-		$folder = $this->createMock(FolderService::class);
 		$node = $this->createMock(\OCP\Files\Folder::class);
 		$node->method('nodeExists')->will($this->returnValue(true));
 		$node->method('get')->will($this->returnValue($node));
-		$folder->method('getFolder')->will($this->returnValue($node));
-		$service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
-		$actual = $service->getPfx('userId');
+		$this->folder->method('getFolder')->will($this->returnValue($node));
+		$actual = $this->service->getPfx('userId');
 		$this->assertInstanceOf('\OCP\Files\Node', $actual);
 	}
 
@@ -548,19 +488,8 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->newUserMail->method('sendMail')->will($this->returnCallback(function () {
 			throw new \Exception("Error Processing Request", 1);
 		}));
-		$service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$this->folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
 		$this->expectErrorMessage('Unable to send the invitation');
-		$service->createToSign('uuid', 'username', 'passwordOfUser', 'passwordToSign');
+		$this->service->createToSign('uuid', 'username', 'passwordOfUser', 'passwordToSign');
 	}
 
 	public function testCreateToSignSuccess() {
@@ -575,8 +504,7 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$node->method('nodeExists')->will($this->returnValue(false));
 		$file = $this->createMock(\OCP\Files\File::class);
 		$node->method('newFile')->will($this->returnValue($file));
-		$folder = $this->createMock(FolderService::class);
-		$folder->method('getFolder')->will($this->returnValue($node));
+		$this->folder->method('getFolder')->will($this->returnValue($node));
 
 		$backend = $this->createMock(\OC\User\Database::class);
 		$backend->method('implementsActions')
@@ -596,24 +524,18 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			->method('generateCertificate')
 			->will($this->returnValue('raw content of pfx file'));
 
-		$service = new AccountService(
-			$this->l10n,
-			$this->fileUserMapper,
-			$this->userManagerInstance,
-			$folder,
-			$this->root,
-			$this->fileMapper,
-			$this->config,
-			$this->newUserMail,
-			$this->cfsslHandler
-		);
-		$actual = $service->createToSign('uuid', 'username', 'passwordOfUser', 'passwordToSign');
+		$actual = $this->service->createToSign('uuid', 'username', 'passwordOfUser', 'passwordToSign');
 		$this->assertNull($actual);
 	}
 
-	public function testGetConfigWithInvalidUuuid() {
-		$this->createUser('username', 'password');
-		$service = new AccountService(
+	/**
+	 * @dataProvider providerTestGetConfigWithInvalidUuuid
+	 */
+	public function testGetConfigWithInvalidUuuid($uuid, $userId, $formatOfPdfOnSign, $expected, $setUp) {
+		if (is_callable($setUp)) {
+			$setUp($this);
+		}
+		$this->service = new AccountService(
 			$this->l10n,
 			$this->fileUserMapper,
 			$this->userManagerInstance,
@@ -622,21 +544,114 @@ final class AccountServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			$this->fileMapper,
 			$this->config,
 			$this->newUserMail,
+			$this->urlGenerator,
 			$this->cfsslHandler
 		);
-		$actual = $service->getConfig('uuid', 'userid', 'filetype');
+		$actual = $this->service->getConfig($uuid, $userId, $formatOfPdfOnSign);
 		$actual = json_encode($actual);
 		$this->assertJsonStringEqualsJsonString(
 			$actual,
-			json_encode([
-				'action' => 200,
-				'errors' => [
-					'This is not your file'
-				],
-				'settings' => [
-					'hasSignatureFile' => false
-				]
-			])
+			json_encode($expected)
 		);
+	}
+
+	public function providerTestGetConfigWithInvalidUuuid() {
+		return [
+			[ // #0
+				null, null, 'filetype',
+				[
+					'settings' => [
+						'hasSignatureFile' => false
+					]
+				], null
+			],
+			[ // #1
+				'uuid', 'userid', 'filetype',
+				[
+					'action' => JSActions::ACTION_DO_NOTHING,
+					'errors' => [
+						'This is not your file'
+					],
+					'settings' => [
+						'hasSignatureFile' => false
+					]
+				], null
+			],
+			[ // #2
+				'uuid', null, 'filetype',
+				[
+					'action' => JSActions::ACTION_CREATE_USER,
+					'settings' => [
+						'hasSignatureFile' => false
+					]
+				], null
+			],
+			[ // #3
+				'uuid', 'username', 'filetype',
+				[
+					'action' => JSActions::ACTION_DO_NOTHING,
+					'errors' => [
+						'This is not your file'
+					],
+					'settings' => [
+						'hasSignatureFile' => false
+					]
+				], function($self) {
+					$self->createUser('username', 'password');
+				}
+			],
+			[ // #4
+				'uuid', null, 'filetype',
+				[
+					'action' => JSActions::ACTION_DO_NOTHING,
+					'errors' => [
+						'Invalid UUID'
+					],
+					'settings' => [
+						'hasSignatureFile' => false
+					]
+				], function($self) {
+					$self->fileUserMapper
+						->method('getByUuid')
+						->will($this->returnCallback(function () {
+							throw new \Exception("Beep, beep, not found!", 1);
+						}));
+				}
+			],
+			[ // #5
+				'uuid', null, 'filetype',
+				[
+					'action' => JSActions::ACTION_REDIRECT,
+					'errors' => [
+						'User already exists. Please login.'
+					],
+					'redirect' => "",
+					'settings' => [
+						'hasSignatureFile' => false
+					]
+				], function($self) {
+					$user = $self->createUser('username', 'password');
+					$user->setEMailAddress('valid@test.coop');
+
+					$fileUser = $self->createMock(FileUser::class);
+					$fileUser
+						->method('__call')
+						->withConsecutive(
+							[$this->equalTo('getUserId')],
+							[$this->equalTo('getEmail')]
+						)
+						->will($this->returnValueMap([
+							['getUserId', [], null],
+							['getEmail', [], 'valid@test.coop']
+						]));
+					$self->fileUserMapper
+						->method('getByUuid')
+						->will($self->returnValue($fileUser));
+					$self->userManagerInstance
+						->method('userExists')
+						->willReturn(true);
+				}
+			],
+		];
 	}
 }
