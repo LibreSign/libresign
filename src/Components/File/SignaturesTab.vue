2efdcf03ba8584442b8cfa4a9f9d8537b889b54b
Vitor Mattos <vitor@php.rio>
2021-06-18 08:52:06 -0300

    Update SignaturesTab.vue

--- src/Components/File/SignaturesTab.vue
+++ src/Components/File/SignaturesTab.vue
@@ -5,7 +5,7 @@
 				<div class="user-name">
 					<div class="icon-sign icon-user" />
 					<span class="name">
-						{{ sign.display_name ? sign.display_name : t('libresign', 'User has no name') }}
+						{{ sign.display_name ? sign.display_name : t('libresign', 'User with no name') }}
 					</span>
 				</div>
 				<div class="content-status">
