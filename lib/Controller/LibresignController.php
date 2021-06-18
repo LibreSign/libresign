3e71a48b3b890c7c38c6d055538fb2f3d415dd2d
Vitor Mattos <vitor@php.rio>
2021-06-18 18:31:02 -0300

    Validation messages

--- lib/Controller/LibresignController.php
+++ lib/Controller/LibresignController.php
@@ -340,6 +340,22 @@ class LibresignController extends Controller {
 			'canRequestSign' => $this->account->canRequestSign($this->userSession->getUser()),
 			'canSign' => $canSign
 		];
+		if ($return['settings']['canSign']) {
+			$return['messages'][
+				[
+					'type' => 'info',
+					'message' => $this->l10n->t('You need to sign this document')
+				]
+			];
+		}
+		if (!$return['settings']['canRequestSign']) {
+			$return['messages'][
+				[
+					'type' => 'info',
+					'message' => $this->l10n->t('You cannot request signature for this document, please contact your administrator')
+				]
+			];
+		}
 		return new JSONResponse($return, $statusCode);
 	}
 
