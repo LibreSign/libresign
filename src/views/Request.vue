d66794f079aa43ae76021ae71db352e55e4a00d8
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-06-23 12:14:33 -0300

    Select the file and request new subscriptions

--- src/views/Request.vue
+++ src/views/Request.vue
@@ -1,74 +1,159 @@
 <template>
-	<Content app-name="libresign">
-		<div class="conntainer-request">
+	<div class="container">
+		<div class="container-request">
 			<header>
 				<h1>{{ t('libresign', 'Request Signatures') }}</h1>
 				<p>{{ t('libresign', 'Choose the file to request signatures.') }}</p>
 			</header>
 			<div class="content-request">
-				<div>
-					<button class="icon-folder" @click="getFile()">
-						{{ t('libresign', 'Share from files') }}
-					</button>
-					<button @click="userAdd">
-						{{ t('libresign', 'Add Users') }}
-					</button>
-				</div>
+				<File
+					v-show="!isEmptyFile"
+					:file="file"
+					status="none"
+					@sidebar="handleSidebar(true)" />
+				<button class="icon icon-folder" @click="getFile()">
+					{{ t('libresign', 'Choose from files') }}
+				</button>
 			</div>
 		</div>
-	</Content>
+		<AppSidebar v-if="sidebar"
+			ref="sidebar"
+			:class="{'app-sidebar--without-background lb-ls-root': 'lb-ls-root'}"
+			:title="file.name"
+			:active="file.name"
+			:subtitle="t('libresign', 'Enter the emails that will receive the request')"
+			:header="false"
+			name="sidebar"
+			icon="icon-rename"
+			@close="handleSidebar(false)">
+			<EmptyContent v-show="canRequest">
+				<template #desc>
+					<p>
+						{{ t('libresign', 'Signatures for this document have already been requested') }}
+					</p>
+				</template>
+			</EmptyContent>
+			<AppSidebarTab
+				v-show="!canRequest"
+				id="request"
+				:name="t('libresign', 'Add Users')"
+				icon="icon-rename">
+				<Users ref="request" :fileinfo="file" @request:signatures="send" />
+			</AppSidebarTab>
+		</AppSidebar>
+	</div>
 </template>
 <script>
-import Content from '@nextcloud/vue/dist/Components/Content'
-import { getFilePickerBuilder } from '@nextcloud/dialogs'
-
-const picker = getFilePickerBuilder(t('libresign', 'Select your file'))
-	.setMultiSelect(false)
-	.setMimeTypeFilter('application/pdf')
-	.setModal(false)
-	.setType(1)
-	.allowDirectories(false)
-	.build()
+import axios from '@nextcloud/axios'
+import AppSidebar from '@nextcloud/vue/dist/Components/AppSidebar'
+import EmptyContent from '@nextcloud/vue/dist/Components/EmptyContent'
+import AppSidebarTab from '@nextcloud/vue/dist/Components/AppSidebarTab'
+import { getFilePickerBuilder, showError, showSuccess } from '@nextcloud/dialogs'
+import Users from '../Components/Request'
+import { generateUrl } from '@nextcloud/router'
+import File from '../Components/File/File.vue'
 
 export default {
 	name: 'Request',
 	components: {
-		Content,
+		AppSidebar,
+		AppSidebarTab,
+		Users,
+		EmptyContent,
+		File,
 	},
 	data() {
 		return {
 			loading: false,
 			file: {},
+			sidebar: false,
+			signers: [],
 		}
 	},
-	methods: {
-		handleUploadFile(event) {
-			const files = event.target.files ?? []
-			for (const file of files) {
-				this.onLocalAttachmentSelected(file, 'file')
-			}
-			event.target.value = ''
+	computed: {
+		isEmptyFile() {
+			return Object.keys(this.file).length === 0
 		},
-		userAdd() {
-			console.info(this.file)
+		canRequest() {
+			return this.signers.length > 0
+		},
+	},
+	watch: {
+		file(newVal, oldVal) {
+			this.handleSidebar(false)
+			this.handleSidebar(true)
+		},
+	},
+	methods: {
+		async getInfo(id) {
+			try {
+				const response = await axios.get(generateUrl(`/apps/libresign/api/0.1/file/validate/file_id/${id}`))
+				this.signers = response.data.signatures
+			} catch (err) {
+				this.signers = []
+			}
+		},
+		async send(users) {
+			try {
+				const response = await axios.post(generateUrl('/apps/libresign/api/0.1/webhook/register'), {
+					file: {
+						fileId: this.file.id,
+					},
+					name: this.file.name.split('.pdf')[0],
+					users,
+				})
+				this.clearRequestList()
+				return showSuccess(response.data.message)
+			} catch (err) {
+				showError(err.response.data.errors)
+			}
+		},
+		clearRequestList() {
+			this.handleSidebar(false)
+			this.file = {}
+			this.$refs.request.clearList()
 		},
 		getFile() {
+			const picker = getFilePickerBuilder(t('libresign', 'Select your file'))
+				.setMultiSelect(false)
+				.setMimeTypeFilter('application/pdf')
+				.setModal(false)
+				.setType(1)
+				.allowDirectories(false)
+				.build()
+
 			picker.pick()
 				.then(path => {
 					OC.dialogs.filelist.forEach(file => {
 						if (file.name === path.split('/')[1]) {
 							this.file = file
+							this.handleSidebar(true)
+							this.getInfo(file.id)
 						}
 					})
 				})
-
+		},
+		changeTab(changeId) {
+			this.tabId = changeId
+		},
+		handleSidebar(status) {
+			this.sidebar = status
 		},
 	},
 }
 </script>
 
 <style lang="scss" scoped>
-.conntainer-request {
+.container{
+	display: flex;
+	flex-direction: row;
+	justify-content: center;
+	align-items: center;
+	width: 100%;
+	height: 100%;
+}
+
+.container-request {
 	display: flex;
 	flex-direction: column;
 	align-items: center;
@@ -76,8 +161,10 @@ export default {
 	width: 100%;
 	max-width: 100%;
 	text-align: center;
+
 	header {
 		margin-bottom: 2.5rem;
+
 		h1 {
 			font-size: 45px;
 			margin-bottom: 1rem;
@@ -87,6 +174,7 @@ export default {
 			font-size: 15px;
 		}
 	}
+
 	.content-request{
 		display: flex;
 		flex-direction: column;
@@ -95,6 +183,6 @@ export default {
 
 button {
 	background-position-x: 8%;
-	padding-left: 35px;
+	padding: 13px 13px 13px 45px;
 }
 </style>
