75a5d5f21a97cce27c2d2b8332d7dc73ab17efb6
Vitor Mattos <vitor@php.rio>
2021-06-28 10:39:10 -0300

    Implement pkcs7

---
+++ lib/Handler/Pkcs7Handler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace OCA\Libresign\Handler;
+
+use OCP\Files\File;
+
+class Pkcs7Handler {
+	public function sign(
+		File $fileToSign,
+		File $certificate,
+		string $passphrase
+	): File {
+		$newName = $fileToSign->getName() . '.p7s';
+		$p7sFile = $fileToSign
+			->getParent()
+			->newFile($newName);
+		openssl_pkcs12_read($certificate->getContent(), $certificateData, $passphrase);
+		$tempntam = tempnam('/temp', 'pkey');
+		file_put_contents($tempntam, $certificateData['pkey']);
+		openssl_pkcs7_sign(
+			$fileToSign->getInternalPath(),
+			$p7sFile->getInternalPath(),
+			'file:/' . $tempntam,
+			$passphrase,
+			[]
+		);
+		return $p7sFile;
+	}
+}
---
+++ tests/Unit/Handler/Pkcs7HandlerTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use OCA\Libresign\Handler\Pkcs7Handler;
+use org\bovigo\vfs\vfsStream;
+
+final class Pkcs7HandlerTest extends \OCA\Libresign\Tests\Unit\TestCase {
+	public function testSignWithSuccess() {
+		vfsStream::setup('home');
+		$fileToSign = $this->createMock(\OCP\Files\File::class);
+		$fileToSign
+			->method('getName')
+			->willReturn('filename.txt');
+		$fileToSign
+			->method('getInternalPath')
+			->willReturn(vfsStream::url('home/filename.txt'));
+		$p7sFile = $this->createMock(\OCP\Files\File::class);
+		$p7sFile
+			->method('getInternalPath')
+			->willReturn(vfsStream::url('home/filename.txt.p7s'));
+		$parentFolder = $this->createMock(\OCP\Files\Folder::class);
+		$parentFolder
+			->method('newFile')
+			->willReturn($p7sFile);
+		$fileToSign
+			->method('getParent')
+			->willReturn($parentFolder);
+
+		$certificate = $this->createMock(\OCP\Files\File::class);
+		$certKeys = json_decode(file_get_contents(__DIR__ . '/../../fixtures/cfssl/newcert-with-success.json'), true);
+		$certKeys = $certKeys['result'];
+		openssl_pkcs12_export($certKeys['certificate'], $certContent, $certKeys['private_key'], 'password');
+		$certificate
+			->method('getContent')
+			->willReturn($certContent);
+		$certificate
+			->method('getInternalPath')
+			->willReturn(vfsStream::url('home/certificate.pfx'));
+
+		$handler = new Pkcs7Handler();
+		$actual = $handler->sign(
+			$fileToSign,
+			$certificate,
+			'password'
+		);
+		$this->assertEquals('p7s', $actual->getExtension());
+		$this->assertEquals('filename.txt.p7s', $actual->getName());
+	}
+}
