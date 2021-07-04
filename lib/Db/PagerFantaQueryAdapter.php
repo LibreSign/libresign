7ad5865cf961a7cd7014635c2568a2bee958d50a
Vitor Mattos <vitor@php.rio>
2021-07-04 12:44:05 -0300

    Create Nextcloud Query Adapter

---
+++ lib/Db/PagerFantaQueryAdapter.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace OCA\Libresign\Db;
+
+use Doctrine\DBAL\Query\QueryBuilder;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use Pagerfanta\Adapter\AdapterInterface;
+use Pagerfanta\Exception\InvalidArgumentException;
+
+/**
+ * Adapter which calculates pagination from a Doctrine DBAL QueryBuilder.
+ */
+class PagerFantaQueryAdapter implements AdapterInterface {
+	private IQueryBuilder $queryBuilder;
+
+	/**
+	 * @var callable
+	 * @phpstan-var callable(QueryBuilder): void
+	 */
+	private $countQueryBuilderModifier;
+
+	/**
+	 * @phpstan-param callable(QueryBuilder): void $countQueryBuilderModifier
+	 *
+	 * @throws InvalidArgumentException if a non-SELECT query is given
+	 */
+	public function __construct(IQueryBuilder $queryBuilder, callable $countQueryBuilderModifier)
+	{
+		$queryBuilder->getType();
+		if (QueryBuilder::SELECT !== $queryBuilder->getType()) {
+			throw new InvalidArgumentException('Only SELECT queries can be paginated.');
+		}
+
+		$this->queryBuilder = clone $queryBuilder;
+		$this->countQueryBuilderModifier = $countQueryBuilderModifier;
+	}
+
+	public function getNbResults(): int
+	{
+		$qb = $this->prepareCountQueryBuilder();
+
+		return (int) $qb->execute()->fetchOne();
+	}
+
+	public function getSlice(int $offset, int $length): iterable
+	{
+		$qb = clone $this->queryBuilder;
+
+		return $qb->setMaxResults($length)
+			->setFirstResult($offset)
+			->execute()
+			->fetchAll();
+	}
+
+	private function prepareCountQueryBuilder(): IQueryBuilder
+	{
+		$qb = clone $this->queryBuilder;
+		$callable = $this->countQueryBuilderModifier;
+
+		$callable($qb);
+
+		return $qb;
+	}
+}
