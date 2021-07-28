d51a5024d587a9c379df82feed9b7f5a3065a128
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-07-28 12:46:33 -0300

    add config to read css files

--- jest.config.js
+++ jest.config.js
@@ -81,6 +81,7 @@ module.exports = {
 	// A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
 	moduleNameMapper: {
 		'^@/(.*)$': '<rootDir>/src/$1',
+		'^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
 	},
 
 	// An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
@@ -188,7 +189,7 @@ module.exports = {
 	// unmockedModulePathPatterns: undefined,
 
 	// Indicates whether each individual test should be reported during the run
-	verbose: false,
+	verbose: true,
 
 	// An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
 	// watchPathIgnorePatterns: [],
