712bea4e70509059e06169aa9fec24eb497c75f0
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-08-02 13:45:34 -0300

    Component Textarea

---
+++ src/Components/Textarea/Textarea.vue
@@ -0,0 +1,16 @@
+<template>
+	<textarea :value="value" @input="$emit('input', $event.target.value)" />
+</template>
+
+<script>
+export default {
+	name: 'Textarea',
+	props: {
+		value: {
+			type: String,
+			required: true,
+			default: '',
+		},
+	},
+}
+</script>
