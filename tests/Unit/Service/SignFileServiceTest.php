7d96a1fe1f00d6eccbdf67281bc312df1a093f19
Vitor Mattos <vitor@php.rio>
2021-08-04 00:20:15 -0300

    fix test

--- tests/Unit/Service/SignFileServiceTest.php
+++ tests/Unit/Service/SignFileServiceTest.php
@@ -627,6 +627,7 @@ final class SignFileServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 
 		$libreSignFile = new \OCA\Libresign\Db\File();
 		$libreSignFile->setUserId('username');
+		$libreSignFile->setUuid('uuid');
 
 		$file = $this->createMock(\OCP\Files\File::class);
 		$file
@@ -644,6 +645,8 @@ final class SignFileServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			->willReturn($file);
 		$this->root->method('getUserFolder')
 			->willReturn($this->root);
+		$this->root->method('newFile')
+			->willReturn($file);
 		$this->pkcs12Handler
 			->method('getPfx')
 			->willReturn($file);
