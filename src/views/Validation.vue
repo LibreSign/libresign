2d7105265f83b45ccc87ab039036609837ef98c9
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-08-09 10:17:48 -0300

    Add media query

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -28,7 +28,7 @@
 								</div>
 								<div class="line-group">
 									<h3>{{ t('libresign', 'Created in:') }}</h3>
-									<span>{{ 'data' }}</span>
+									<span>{{ formatData(document.createdAt) }}</span>
 								</div>
 							</div>
 							<div class="line">
@@ -56,10 +56,13 @@
 									id="sign"
 									:key="item.fullName"
 									class="scroll">
-									<div class="subscriber">
-										<span><b>{{ getName(item) }}</b></span>
-										<span v-if="item.signed" class="data-signed">{{ formatData(item.signed) }} </span>
-										<span v-else>{{ t('libresign', 'No date') }}</span>
+									<div class="subscriber-item">
+										<h3>{{ t('libresign', 'Signed by:') }}</h3>
+										<span>{{ getName(item) }}</span>
+									</div>
+									<div class="subscriber-item">
+										<h3>{{ t('libresign', 'At:') }}</h3>
+										<span>{{ item.signed ? formatData(item.signed) : t('libresign', 'Pending signin') }}</span>
 									</div>
 								</div>
 							</div>
@@ -83,7 +86,8 @@ import iconB from '../../img/file-signature-solid.svg'
 import { generateOcsUrl, generateUrl } from '@nextcloud/router'
 import { showError, showSuccess } from '@nextcloud/dialogs'
 import { translate as t } from '@nextcloud/l10n'
-import { fromUnixTime } from 'date-fns'
+import { fromUnixTime, format } from 'date-fns'
+import { loadState } from '@nextcloud/initial-state'
 
 export default {
 	name: 'Validation',
@@ -124,6 +128,7 @@ export default {
 		if (this.myUuid.length > 0) {
 			this.validate(this.myUuid)
 		}
+		console.info(JSON.parse(loadState('libresign', 'config')))
 	},
 	methods: {
 		validate(id) {
@@ -184,7 +189,7 @@ export default {
 		},
 		formatData(data) {
 			try {
-				return fromUnixTime(data).toLocaleDateString()
+				return format(fromUnixTime(data), 'dd/MM/yyyy HH:mm')
 			} catch {
 				return t('libresign', 'No date')
 			}
@@ -194,6 +199,364 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/styles/validation.scss';
-@import '../assets/styles/loading.scss';
+$text-color: #273849;
+$background: #ECEFFC;
+$title-font: 1.5rem;
+$title-font-mobile: 1.3rem;
+$date-signed-font: .7rem;
+
+.btn-load {
+	background-color: transparent !important;
+	font-size: 0;
+	pointer-events: none;
+	cursor: not-allowed;
+	margin-top: 10px;
+	border: none;
+}
+
+.jumbotron{
+	background-color: $background;
+	padding: 0;
+}
+
+.container{
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	width: 100%;
+
+	.image{
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		img{
+			width: 50%;
+			max-width: 422px;
+		}
+		@media screen and (max-width: 900px) {
+			display: none;
+			width: 0%;
+		}
+
+	}
+	#dataUUID{
+		width: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		@media screen and (max-width: 900px){
+			width: 100%;
+		}
+	}
+	.legal-information{
+		opacity: 0.8;
+		align-self: center;
+		font-size: 1rem;
+		max-height: 100px;
+		overflow: scroll;
+	}
+}
+
+form{
+	background-color: #FFF;
+	color: $text-color;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 20px;
+	margin: 20px;
+	border-radius: 8px;
+	border: 1px solid var(--color-background-dark);
+	max-width: 500px;
+	box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+
+	@media screen and (max-width: 900px) {
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		max-width: 100%;
+	}
+}
+
+h1{
+	font-size: 24px;
+	font-weight: bold;
+	color: $text-color;
+}
+
+h3{
+	color: #337ab7;
+}
+
+input{
+	width: 100%;
+	margin: 20px 0px;
+	background-color: var(--color-background-dark);
+}
+
+button{
+	background-color: #0082c9;
+	color: #FFF;
+	float: right;
+	margin-top: 20px;
+	&:hover{
+		background-color: #286090;
+	}
+}
+
+.infor{
+	display: flex;
+	flex-direction: column;
+	h1{
+		font-size: $title-font;
+	}
+}
+
+.infor-container{
+	margin-right: 20px;
+}
+
+.infor-bg{
+
+	.infor-header{
+		display: flex;
+		flex-direction: column;
+		width: 700px;
+		height: 300px;
+		margin-top: 10px;
+		background-color: #fff;
+		box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+		border-radius: 8px;
+		padding: 20px 60px 20px 20px;
+		min-height: 415px;
+
+		.line{
+			display: flex;
+			flex-direction: row;
+			width: 100%;
+			margin: 15px 0;
+
+			.line-group{
+				display: flex;
+				flex-direction: column;
+				line-height: 10px;
+				width: 100%;
+
+				h3{
+					font-weight: 400;
+					color: #000;
+					min-width: 200px;
+				}
+
+				span{
+					font-size: 16px;
+					color: rgba(0, 0, 0, 0.7);
+				}
+			}
+		}
+		.button{
+			align-self: flex-end;
+		}
+	}
+
+	.infor-content{
+		display: flex;
+		flex-direction: column;
+		overflow: scroll;
+		height: 80%;
+		width: 95%;
+		margin: 0 20px;
+	}
+}
+
+.info-document{
+	color: $text-color;
+	display: flex;
+	flex-direction: column;
+	width: 100%;
+	margin-left: 30px;
+	max-height: 250px;
+	justify-content: center;
+	overflow: scroll;
+
+	p{
+		font-size: 1rem;
+	}
+	a{
+		width: fit-content;
+		align-self: flex-end;
+	}
+	#sign {
+		display: flex;
+	}
+}
+
+.signed {
+	width: 700px;
+	height: 300px;
+	margin-top: 10px;
+	background-color: #FFFFFF;
+	padding: 20px 60px 20px 20px;
+	border-radius: 8px;
+	box-shadow: rgba(17, 12, 46, 0.15) 0px 48px 100px 0px;
+
+	strong {
+		font-size: 22px;
+		margin-bottom: 10px;
+	}
+	button {
+		float: right;
+	}
+}
+
+.scroll {
+	display: flex;
+	flex-direction: row;
+	min-width: 200px;
+	background-color: $background;
+	border-radius: 8px;
+	padding: 0 10px 10px 10px;
+	margin: 5px;
+
+	.subscriber-item{
+		width: 40%;
+		line-height: 12px;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		padding: 10px;
+
+		span{
+			opacity: .8;
+		}
+		h3{
+			color: #000000;
+		}
+	}
+
+	@media (max-width: 600px) {
+		flex-direction: column;
+
+		.subscriber-item{
+			width: 100%;
+		}
+	}
+}
+
+.subscriber {
+	display: flex;
+	flex-direction: column;
+	color: $text-color;
+	background-color: $background;
+	border-radius: 8px;
+	padding: 5px 0px 5px 5px;
+	margin: 5px 5px 0px 0px;
+	min-height: 50px;
+	width: 100%;
+	max-width: 98%;
+
+	.subscriber-item{
+		margin-right: 20px;
+		h3{
+			font-weight: bold;
+		}
+		span{
+			opacity: .8;
+		}
+	}
+
+	.data-signed {
+		font-size: $date-signed-font;
+	}
+	b{
+		display: block;
+		overflow: hidden;
+		text-overflow: ellipsis;
+	}
+}
+
+.header {
+	display: flex;
+	h1{
+		font-size: $title-font;
+	}
+	span{
+		margin-top: 20px;
+	}
+}
+
+.icon{
+	width: 30px;
+	margin-right: 10px;
+}
+
+@media screen and (max-width: 700px) {
+	.infor-container {
+		margin-right: 0px;
+		width: 100%;
+	}
+	.infor-bg {
+		box-shadow: none;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		.infor-header{
+			width: 95%;
+
+			.line-group{
+				line-height: 18px;
+			}
+		}
+
+		.signed{
+			width: 95%;
+			box-shadow: none;
+		}
+	}
+	.container {
+		align-items: flex-start;
+	}
+	.infor {
+		h1 {
+			font-size: $title-font-mobile;
+		}
+	}
+	.header {
+		h1 {
+			font-size: $title-font-mobile;
+		}
+	}
+}
+
+@media (max-width: 400px) {
+	.infor-bg{
+
+		.infor-header{
+			height: 520px;
+		}
+
+		.line{
+			flex-direction: column !important;
+
+			.line-group{
+				span{
+					line-height: 18px;
+				}
+			}
+		}
+	}
+}
+
+#legal-information {
+	margin-right: 0;
+	span{
+		width: 100%;
+		line-height: normal;
+	}
+}
+
 </style>
