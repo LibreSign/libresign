40221bda189f43e324c0919b617e195cfcff9e93
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-08-09 13:34:27 -0300

    Test whether the document preview button renders the correct link

--- src/__test__/unit/Validation.spec.js
+++ src/__test__/unit/Validation.spec.js
@@ -29,7 +29,24 @@ afterEach(() => {
 	mockAxios.reset()
 })
 
-describe('Validation Page', () => {
+describe('Validation Page without API data ', () => {
+	it('Is a Vue Instance', () => {
+		expect(wrapper.vm).toBeTruthy()
+	})
+
+	it('UUID initialized empty', async() => {
+		expect(wrapper.props().uuid).toBe('')
+
+		const input = wrapper.find('input')
+		expect(input.exists()).toBe(true)
+		await input.setValue('')
+		expect(input.element.value).toBe('')
+		expect(wrapper.vm.hasInfo).toBe(false)
+	})
+
+})
+
+describe('Validation Page with API data', () => {
 	const mockDocument = {
 		success: true,
 		name: 'ssd',
@@ -49,132 +66,6 @@ describe('Validation Page', () => {
 			me: false,
 			signatureId: 165,
 			email: '4@sd.com',
-		}, {
-			signed: '1628014488',
-			displayName: 'asd@asd.coop',
-			fullName: null,
-			me: false,
-			signatureId: 147,
-			email: 'asd@asd.coop',
-		}, {
-			signed: '1628019897',
-			displayName: 'algume-amidslacomqweleasdja2teste.coop@teste.coop',
-			fullName: null,
-			me: false,
-			signatureId: 161,
-			email: 'algume-amidslacomqweleasdja2teste.coop@teste.coop',
-		}, {
-			signed: '1628014987',
-		 displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 148,
-			email: '1@b.c',
-		}, {
-			signed: '1628015162',
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 149,
-			email: '2@b.com',
-		}, {
-			signed: '1628016442',
-			displayName: 'd@d.coop',
-			fullName: null,
-			me: false,
-			signatureId: 150,
-			email: 'd@d.coop',
-		}, {
-			signed: '1628022374',
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 157,
-			email: 'asd@sdsd.c',
-		}, {
-			signed: '1628017237',
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 151,
-			email: 'dd@d.coop',
-		}, {
-			signed: '1628017367',
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 145,
-			email: 'adm@adm.coops',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 166,
-			email: 'l@l.c',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 155,
-			email: 'd@sdsd.com',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 156,
-			email: 'sdalksd@sda.c',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 158,
-			email: 'dwww@ww.c',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 160,
-			email: 'd2@d2.d',
-		}, {
-			signed: '1628023125',
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 159,
-			email: 'ealkwee@ds.w',
-		}, {
-			signed: null,
-			displayName: 'nextcloud',
-			fullName: null,
-			me: true,
-			signatureId: 162,
-			email: 'admin@admin.com',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 163,
-			email: 'asdf@d.coop',
-		}, {
-			signed: null,
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 164,
-			email: '3@ds.com',
-		}, {
-			signed: '1628102190',
-			displayName: '',
-			fullName: null,
-			me: false,
-			signatureId: 167,
-			email: 's@s.c',
 		}],
 		settings: {
 			canSign: true,
@@ -183,24 +74,12 @@ describe('Validation Page', () => {
 		},
 	}
 
-	it('Is a Vue Instance', () => {
-		expect(wrapper.vm).toBeTruthy()
-	})
-
-	it('UUID initialized empty', async() => {
-		expect(wrapper.props().uuid).toBe('')
-
-		const input = wrapper.find('input')
-		expect(input.exists()).toBe(true)
-		await input.setValue('')
-		expect(input.element.value).toBe('')
-		expect(wrapper.vm.hasInfo).toBe(false)
-	})
-
-	it('Have data', async() => {
+	beforeEach(async() => {
 		await wrapper.setData({ document: mockDocument })
 		await wrapper.setData({ hasInfo: true })
+	})
 
+	it('Get Legal information', async() => {
 		expect(wrapper.vm.hasInfo).toBe(true)
 
 		expect(mockAxios.get).toHaveBeenCalledWith(
@@ -219,6 +98,19 @@ describe('Validation Page', () => {
 		await wrapper.vm.$nextTick()
 		const legalInformation = wrapper.findAll('span.legal-information')
 		expect(legalInformation.at(0).text()).toBe('This is a message')
-
 	})
+
+	it('Rendering signers list', () => {
+		expect(wrapper.vm.hasInfo).toBe(true)
+
+		const listGroups = wrapper.findAll('div.scroll')
+		expect(listGroups.exists()).toBe(true)
+	})
+
+	it('Button to view Document', () => {
+		const button = wrapper.find('a.button')
+		expect(button.exists()).toBe(true)
+		expect(button.attributes('href')).toBe(mockDocument.file)
+	})
+
 })
