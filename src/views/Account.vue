218c695a721f407faae124b51ae26383e62beee3
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-09-14 08:36:32 -0300

    fix css

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -137,8 +137,10 @@ export default {
 </script>
 <style lang="scss">
 .modal-wrapper--large .modal-container[data-v-3e0b109b]{
-	width: 100%;
+	width: 50%;
+	max-width: 600px;
 	height: 100%;
+	max-height: 560px;
 }
 
 .container-account{
@@ -291,8 +293,11 @@ export default {
 }
 
 .container-modal-customize-signatures{
-	width: 100%;
-	height: 100%;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	width: calc(100% - 20px);
+	height: calc(100% - 40px);
 	margin: 20px;
 
 	header{
@@ -310,6 +315,8 @@ export default {
 	.content{
 		display: flex;
 		flex-direction: column;
+		width: 100%;
+		height: 100%;
 	}
 }
 </style>
