7bcad45ccddbdd305b23c7af71e19fd0b3f121a1
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-09-16 19:37:35 -0300

    Add pfx and error state to module

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -3,39 +3,71 @@ import { showError, showSuccess } from '@nextcloud/dialogs'
 import { generateUrl } from '@nextcloud/router'
 const state = {
 	user: {},
+	pfx: {},
+	error: {},
 }
 
 const getters = {
+	hasPfx: (state) => {
+		return state.pfx.length > 0
+	},
+	getError: state => {
+		return state.error.length > 0
+	},
 
 }
 
-const mutations = {}
+const mutations = {
+	setError: (state, error) => {
+		state.error = error
+	},
+	setPfx: (state, data) => {
+		state.pfx = data
+	},
+}
 
 const actions = {
-	CREATE: async({ commit }, { email, password, signPassword, uuid }) => {
+	CREATE: async({ commit, dispatch }, { email, password, signPassword, uuid }) => {
 		try {
-			const response = await axios.post(generateUrl(`/apps/libresign/api/0.1/create/${uuid}`), {
+			dispatch('CLEAR_ERROR')
+			const response = await axios.post(generateUrl(`/apps/libresign/api/0.1/account/create/${uuid}`), {
 				email,
 				password,
 				signPassword,
 			})
+			console.info(response)
+			if (response.data.sucess === false) {
+				commit('setError', response.data.message)
+			}
 			commit('setPdfData', response.data, { root: true })
+			dispatch('files/SET_FILE_TO_SIGN', response.data, { root: true })
 			showSuccess('libresign', 'User created!')
 		} catch (err) {
 			showError(err.response.data.message)
+			console.info(err.response)
+			commit('setError', err.response.data.message)
 		}
 	},
-	CREATE_PFX: async({ commit }, signPassword) => {
+	CREATE_PFX: async({ commit, dispatch }, { signPassword }) => {
 		try {
-			await axios.post(generateUrl('/apps/libresign/api/0.1/account/signature'), {
+			dispatch('CLEAR_ERROR')
+
+			const response = await axios.post(generateUrl('/apps/libresign/api/0.1/account/signature'), {
 				signPassword,
 			})
 
+			commit('setPfx', response.data.signature)
+
 			commit('setHasPfx', true, { root: true })
-			showSuccess(t('libresign', 'Password dreated!'))
+			showSuccess(t('libresign', 'Password created!'))
 		} catch (err) {
 			showError(err.response.data.message)
+			console.info(err.response)
+			commit('setError', err.response.data.message)
 		}
+	},
+	CLEAR_ERROR: ({ commit }) => {
+		commit('setError', {})
 	},
 }
 
