77deb5f007e78f4021bdf086ef90fe84d5563d4c
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-09-24 15:07:41 -0300

    renamed to feature_controller module

---
+++ src/store/modules/feature_controller.js
@@ -0,0 +1,105 @@
+import axios from '@nextcloud/axios'
+import { generateOcsUrl } from '@nextcloud/router'
+const state = {
+	features: [],
+	enabledFeatures: [],
+}
+
+const getters = {
+	getFeatures: state => {
+		return state.features
+	},
+	getEnabledFeatures: state => {
+		return state.enabledFeatures
+	},
+}
+
+const mutations = {
+	setFeatures: (state, features) => {
+		state.features = features
+	},
+	setEnabledFeatures: (state, feature) => {
+		state.enabledFeatures = feature
+	},
+}
+
+const actions = {
+	GET_CONFIG_FEATURES: async({ commit }) => {
+		const response = await axios.get(
+			generateOcsUrl('/apps/provisioning_api/api/v1', 2) + 'config/apps/libresign/features', {}
+		)
+		const features = response.data.ocs.data.data ? JSON.parse(response.data.ocs.data.data) : response.data.ocs.data.data
+
+		commit('setFeatures', features)
+	},
+	GET_CONFIG_ENABLED_FEATURES: async({ commit }) => {
+		const response = await axios.get(
+			generateOcsUrl('/apps/provisioning_api/api/v1', 2) + 'config/apps/libresign/features_enabled', {}
+		)
+		const enabledFeatures = response.data.ocs.data.data ? JSON.parse(response.data.ocs.data.data) : response.data.ocs.data.data
+
+		commit('setEnabledFeatures', enabledFeatures)
+	},
+	GET_STATES: ({ dispatch }) => {
+		dispatch('GET_CONFIG_FEATURES')
+		dispatch('GET_CONFIG_ENABLED_FEATURES')
+	},
+	ENABLE_FEATURE: async({ state, dispatch, commit }, feature) => {
+		await dispatch('GET_STATES')
+
+		if (!state.features.includes(feature)) {
+			return console.error(t('libresign', 'This feature does not exist.'))
+		}
+
+		if (state.enabledFeatures.includes(feature)) {
+			return console.debug(t('libresign', 'This feature already enabled.'))
+		}
+
+		const newEnabled = [...state.enabledFeatures, feature]
+		const parsed = JSON.stringify(newEnabled)
+
+		await OCP.AppConfig.setValue('libresign', 'features_enabled', parsed)
+
+		setTimeout(() => {
+			dispatch('GET_STATES')
+		}, 2000)
+		console.debug(t('libresign', 'Feature enabled.'))
+	},
+	DISABLE_FEATURE: async({ state, getters, dispatch, commit }, feature) => {
+		dispatch('GET_STATES')
+
+		const enabledState = getters.getEnabledFeatures
+
+		if (!enabledState.includes(feature)) {
+			return console.error(t('libresign', 'This feature does not enabled'))
+		}
+
+		if (enabledState.length <= 1) {
+			OCP.AppConfig.setValue('libresign', 'features_enabled', '')
+			setTimeout(() => {
+				dispatch('GET_STATES')
+			}, 2000)
+			return console.debug(t('libresign', 'Feature disabled.'))
+
+		}
+
+		const newEnabled = enabledState.splice(enabledState.indexOf(feature), 1)
+		const parsed = JSON.stringify(newEnabled)
+
+		OCP.AppConfig.setValue('libresign', 'features_enabled', parsed)
+
+		setTimeout(() => {
+			dispatch('GET_STATES')
+		}, 3000)
+		console.debug(t('libresign', 'Feature disabled.'))
+
+	},
+}
+
+export default {
+	namespaced: true,
+	state,
+	getters,
+	mutations,
+	actions,
+}
