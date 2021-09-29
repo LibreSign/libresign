3ce1417160026f35adf8b9172c21b275bdc29773
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-09-29 17:31:26 -0300

    mock store and services

---
+++ src/services/SignaturesService.js
@@ -0,0 +1,72 @@
+import { generateUrl } from '@nextcloud/router'
+import store from '../store'
+
+export class SignaturesService {
+
+	url(url) {
+		url = `/apps/libresign/api/0.1${url}`
+		return generateUrl(url)
+	}
+
+	async loadSignatures() {
+		return [
+			{
+				type: 'signature',
+				file: {
+					url: 'http://cloud.test.coop/s/ry384r6t384/download/signature.png',
+				},
+				id: 5,
+			}, {
+				type: 'initials',
+				file: {
+					url: 'http://cloud.test.coop/s/ry384r6t384/download/signature.png',
+				},
+				id: 5,
+			},
+		]
+	}
+
+	async newSignature() {}
+
+	async getElement(elementId) {
+		return [
+			{
+				type: 'signature',
+				file: {
+					url: 'http://cloud.test.coop/s/ry384r6t384/download/signature.png',
+				},
+				id: 5,
+				metadata: {
+					text: 'string',
+					dateFormat: 'string',
+				},
+			},
+		]
+	}
+
+	async updateElement(element) {
+		const oldElement = this.getElement(element.id)
+
+		if (oldElement.id === element.id) {
+			if (element.type === 'signature') {
+				store.commit('signature/setSignature', element)
+			} else {
+				store.commit('signature/setInitials', element)
+			}
+		}
+	}
+
+	async newElement(element) {
+		if (element.type === 'signature') {
+			store.commit('signature/setSignature', element)
+		} else {
+			store.commit('signature/setInitials', element)
+		}
+		return { message: 'Success' }
+	}
+
+	async deleteElement(elementId) {
+
+	}
+
+}
--- src/store/modules/signatures.js
+++ src/store/modules/signatures.js
@@ -1,7 +1,10 @@
+import { showError, showSuccess } from '@nextcloud/dialogs'
+import { SignaturesService } from '../../services/SignaturesService'
+
+const apiClient = new SignaturesService()
+
 const state = {
-	signatures: [
-		{ ol: true },
-	],
+	signatures: [],
 	initials: [],
 }
 const getters = {
@@ -13,9 +16,43 @@ const getters = {
 	},
 }
 
-const mutations = {}
+const mutations = {
+	setSignature: (state, signature) => {
+		state.signatures = signature
+	},
+	setInitials: (state, initial) => {
+		state.initials = initial
+	},
+}
 
-const actions = {}
+const actions = {
+	fetchSignatures: async({ commit }) => {
+		const response = await apiClient.loadSignatures()
+		const signature = response.filter(arr => arr.type === 'signature')
+		const initials = response.filter(arr => arr.type === 'initials')
+
+		commit('setSignature', signature)
+		commit('setInitials', initials)
+	},
+	getElement: async({ commit }, { id }) => {
+		const response = await apiClient.newSignature(id)
+
+		if (response.type === 'signature') {
+			commit('signature/setSignature', response)
+		} else {
+			commit('signature/setInitials', response)
+		}
+	},
+	newSignature: async({ commit }, { type, file }) => {
+		try {
+			const response = await apiClient.newElement({ type, file })
+			showSuccess(response.message)
+		} catch (err) {
+			showError(err)
+		}
+	},
+
+}
 
 export default {
 	namespaced: true,
