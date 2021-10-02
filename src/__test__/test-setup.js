2e32f9b513826917980fbcaea5cf34debe2d366c
Vinicios Gomes <viniciusgomesviana@gmail.com>
2021-10-02 14:54:13 -0300

    removed mock @nextcloud/auth

--- src/__test__/test-setup.js
+++ src/__test__/test-setup.js
@@ -5,13 +5,6 @@ jest.mock('@nextcloud/initial-state', () => ({
 	loadState: jest.fn().mockReturnValue('{"settings":{"hasSignatureFile":true}}'),
 }))
 
-jest.mock('@nextcloud/auth', () => ({
-	getCurrentUser: jest.fn().mockReturnValue({
-		displayName: 'admin',
-		uid: 'admin',
-	}),
-}))
-
 global.OC = {
 	coreApps: [
 		'core',
