d041d79c415d5696b289087056b27e4ce15624c6
Vitor Mattos <vitor@php.rio>
2021-10-17 13:16:41 -0300

    Entity and migration to user element

---
+++ lib/Db/UserElement.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace OCA\Libresign\Db;
+
+use OCP\AppFramework\Db\Entity;
+
+/**
+ * @method void setId(int $id)
+ * @method int getId()
+ * @method void setType(string $type)
+ * @method string getType()
+ * @method void setFileId(int $fileId)
+ * @method int getFileId()
+ * @method void setUserId(string $userId)
+ * @method string getUserId()
+ * @method void setCreatedAt(\DateTime $createdAt)
+ * @method \DateTime getCreatedAt()
+ */
+class FileUser extends Entity {
+
+	/** @var integer */
+	protected $id;
+	/** @var string */
+	protected $type;
+	/** @var integer */
+	protected $fileId;
+	/** @var string */
+	protected $userId;
+	/** @var \DateTime */
+	protected $createdAt;
+	public function __construct() {
+		$this->addType('id', 'integer');
+		$this->addType('type', 'string');
+		$this->addType('fileId', 'integer');
+		$this->addType('userId', 'string');
+		$this->addType('createdAt', 'datetime');
+	}
+
+	public function setCreatedAt($createdAt): void {
+		if (!$createdAt instanceof \DateTime) {
+			$createdAt = new \DateTime($createdAt);
+		}
+		$this->createdAt = $createdAt;
+		$this->markFieldUpdated('createdAt');
+	}
+}
