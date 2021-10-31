55e372a59de27a0953d76e73f3322ff759529615
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-10-31 11:40:48 -0300

    🔧 (396): refine eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,10 @@ module.exports = {
 			],
             "resolvePaths": [],
             "tryExtensions": []
-        }]
+        }],
+
+		// production only
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+		'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
     }
 }
