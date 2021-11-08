20e6747d02dbcc41111b67769337d8b9f3d01523
Vitor Mattos <vitor@php.rio>
2021-11-08 00:07:12 -0300

    Translate coordinates to internal notation

--- lib/Service/FileElementService.php
+++ lib/Service/FileElementService.php
@@ -2,6 +2,7 @@
 
 namespace OCA\Libresign\Service;
 
+use OCA\Libresign\Db\File;
 use OCA\Libresign\Db\FileElement;
 use OCA\Libresign\Db\FileElementMapper;
 use OCA\Libresign\Db\FileMapper;
@@ -42,18 +43,14 @@ class FileElementService {
 			$file = $this->fileMapper->getById($properties['fileId']);
 			$fileElement->setFileId($properties['fileId']);
 		}
-		$metadata = $this->signFileService->getFileMetadata($file->getNodeId());
+		$coordinates = $this->translateCoordinatesToInternalNotation($properties, $file);
 		$fileElement->setUserId($properties['uid']);
 		$fileElement->setType($properties['type']);
-		$fileElement->setPage($properties['coordinates']['page'] ?? 1);
-		$fileElement->setUrx($properties['coordinates']['urx'] ?? 0);
-		if (!empty($properties['coordinates']['ury'])) {
-			$fileElement->setUry($properties['coordinates']['ury']);
-		} elseif (!empty($properties['coordinates']['top'])) {
-
-		}
-		$fileElement->setLlx($properties['coordinates']['llx'] ?? 0);
-		$fileElement->setLly($properties['coordinates']['lly'] ?? 0);
+		$fileElement->setPage($coordinates['page']);
+		$fileElement->setUrx($coordinates['urx']);
+		$fileElement->setUry($coordinates['ury']);
+		$fileElement->setLlx($coordinates['llx']);
+		$fileElement->setLly($coordinates['lly']);
 		$fileElement->setMetadata(!empty($properties['metadata']) ? json_encode($properties['metadata']) : null);
 		if (!empty($properties['elementId'])) {
 			$fileElement->setId($properties['elementId']);
@@ -61,6 +58,51 @@ class FileElementService {
 			$fileElement->setCreatedAt($this->timeFactory->getDateTime());
 		}
 		return $fileElement;
+	}
+
+	private function translateCoordinatesToInternalNotation(array $properties, File $file) {
+		$translated['page'] = $properties['coordinates']['page'] ?? 1;
+		$metadata = json_decode($file->getMetadata(), true);
+		$dimension = $metadata['d'][$translated['page'] - 1];
+
+		if (isset($properties['coordinates']['ury'])) {
+			$translated['ury'] = $properties['coordinates']['ury'];
+		} elseif (isset($properties['coordinates']['top'])) {
+			$translated['ury'] = $dimension['h'] - $properties['coordinates']['top'];
+		} else {
+			$translated['ury'] = 0;
+		}
+
+		if (isset($properties['coordinates']['lly'])) {
+			$translated['lly'] = $properties['coordinates']['lly'];
+		} elseif (isset($properties['coordinates']['width'])) {
+			if ($properties['coordinates']['width'] > $translated['ury']) {
+				$translated['ury'] = $properties['coordinates']['width'];
+				$translated['lly'] = 0;
+			} else {
+				$translated['lly'] = $translated['ury'] - $properties['coordinates']['width'];
+			}
+		} else {
+			$translated['lly'] = 0;
+		}
+
+		if (isset($properties['coordinates']['llx'])) {
+			$translated['llx'] = $properties['coordinates']['llx'];
+		} elseif (isset($properties['coordinates']['left'])) {
+			$translated['llx'] = $properties['coordinates']['left'];
+		} else {
+			$translated['llx'] = 0;
+		}
+
+		if (isset($properties['coordinates']['urx'])) {
+			$translated['urx'] = $properties['coordinates']['urx'];
+		} elseif (isset($properties['coordinates']['height'])) {
+			$translated['urx'] = $translated['llx'] + $properties['coordinates']['height'];
+		} else {
+			$translated['urx'] = 0;
+		}
+
+		return $translated;
 	}
 
 	public function deleteVisibleElement($elementId) {
