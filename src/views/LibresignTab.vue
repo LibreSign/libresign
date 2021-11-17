b8b5ae6297adb26473afc997cca3c43cbb7c52b2
Ednaldo Neimeg <neimeg@gmail.com>
2021-11-17 12:23:31 -0300

    Backup

--- src/views/LibresignTab.vue
+++ src/views/LibresignTab.vue
@@ -152,6 +152,9 @@ export default {
 			hasPfx: false,
 			showValidation: false,
 			uuid: '',
+
+			signWithSMS: null,
+			phoneNumberIsRequired: null,
 		}
 	},
 
@@ -261,6 +264,8 @@ export default {
 					this.haveRequest = true
 					this.canRequestSign = true
 					this.signers = response.data.signers
+					this.signWithSMS = true
+					this.phoneNumberIsRequired = true
 				} else {
 					this.signers = []
 				}
