061bde16e3cbedb91143e436133222e66f6737a4
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-11-20 18:26:58 -0300

    ♻️ (396): remove duplicate code

--- src/views/LibresignTab.vue
+++ src/views/LibresignTab.vue
@@ -123,27 +123,10 @@ import axios from '@nextcloud/axios'
 import { generateUrl } from '@nextcloud/router'
 import Sign from '../Components/Sign'
 import Request from '../Components/Request'
-import { forEach, get } from 'lodash-es'
+import { get } from 'lodash-es'
 import { service as signService, SIGN_STATUS } from '../domains/sign'
 import { getAPPURL } from '../helpers/path'
-
-const showErrors = errList => {
-	forEach(errList, err => {
-		showError(err)
-	})
-}
-
-const showResponseError = res => {
-	const errors = get(res, ['data', 'errors'])
-
-	if (errors) {
-		return showErrors(errors)
-	}
-
-	const message = get(res, ['data', 'message'], res?.message)
-
-	return showError(message || 'unknown error')
-}
+import { showResponseError } from '../helpers/errors'
 
 export default {
 	name: 'LibresignTab',
