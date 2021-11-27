8c45adc58e2ae1ce23d7b85fa9bdf8cac992f0a0
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-11-27 14:18:29 -0300

    🚸 add emphasis to pending status

--- src/views/SignDetail/partials/SignerRow.vue
+++ src/views/SignDetail/partials/SignerRow.vue
@@ -64,6 +64,7 @@ export default {
 		v-bind="{ to, 'counter-number': hasElement ? '📎' : undefined }"
 		:title="displayName"
 		:details="signDate"
+		:class="`signer-row signer-row-${status}`"
 		v-on="$listeners">
 		<template #icon>
 			<Avatar is-no-user
@@ -72,8 +73,18 @@ export default {
 				:display-name="displayName" />
 		</template>
 		<template #subtitle>
-			{{ status }}
+			<span class="signer-status">{{ status }}</span>
 		</template>
 		<slot v-if="$slots.actions" slot="actions" name="actions" />
 	</ListItem>
 </template>
+
+<style>
+	.signer-row-signed .signer-status {
+		font-weight: bold;
+	}
+
+	.signer-row-pending .signer-status {
+		color: var(--color-warning, #eca700)
+	}
+</style>
