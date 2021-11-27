4eb2199cadb27ccd0095e124a4fa9bba6d7b8ea5
Vinicius Reis <luiz.vinicius73@gmail.com>
2021-11-27 18:35:53 -0300

    🚚 move PDFViewer component

---
+++ src/views/SignPDF/_partials/PDFViewer.vue
@@ -0,0 +1,27 @@
+<template>
+	<iframe :src="url">
+		{{ t("libresign", "This iframe is not supported in your browser.") }}
+	</iframe>
+</template>
+
+<script>
+export default {
+	name: 'PDFViewer',
+
+	props: {
+		url: {
+			type: String,
+			required: false,
+			default: '',
+		},
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+iframe {
+	overflow: hidden;
+	height: 100%;
+	width: 100%;
+}
+</style>
