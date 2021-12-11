45cce99659fceb475744e68b390c7692d1cd91f5
Vitor Mattos <vitor@php.rio>
2021-12-11 18:20:15 -0300

    Identation

--- appinfo/routes.php
+++ appinfo/routes.php
@@ -5,33 +5,35 @@ return [
 		// API
 		['name' => 'api#preflighted_cors',            'url' => '/api/0.1/{path}', 'verb' => 'OPTIONS', 'requirements' => ['path' => '.+'], ],
 
-		// Deprecated
+		// Deprecated - Account
 		['name' => 'accountDeprecated#me',            'url' => '/api/0.1/webhook/me', 'verb' => 'GET'],
 
-		['name' => 'account#createToSign',            'url' => '/api/0.1/account/create/{uuid}', 'verb' => 'POST'],
-		['name' => 'account#me',                      'url' => '/api/0.1/account/me', 'verb' => 'GET'],
-		['name' => 'account#updateSettings',          'url' => '/api/0.1/account/settings', 'verb' => 'PATCH'],
-		['name' => 'account#signatureGenerate',       'url' => '/api/0.1/account/signature', 'verb' => 'POST'],
-		['name' => 'account#addFiles',                'url' => '/api/0.1/account/files', 'verb' => 'POST'],
-		['name' => 'account#accountFileList',            'url' => '/api/0.1/account/files/approval/list', 'verb' => 'GET'],
-		['name' => 'account#createSignatureElement',  'url' => '/api/0.1/account/signature/elements', 'verb' => 'POST'],
-		['name' => 'account#getSignatureElements',    'url' => '/api/0.1/account/signature/elements', 'verb' => 'GET'],
-		['name' => 'account#getSignatureElement',     'url' => '/api/0.1/account/signature/elements/{elementId}', 'verb' => 'GET'],
-		['name' => 'account#patchSignatureElement',   'url' => '/api/0.1/account/signature/elements/{elementId}', 'verb' => 'PATCH'],
-		['name' => 'account#deleteSignatureElement',  'url' => '/api/0.1/account/signature/elements/{elementId}', 'verb' => 'DELETE'],
+		// Account
+		['name' => 'account#createToSign',              'url' => '/api/0.1/account/create/{uuid}', 'verb' => 'POST'],
+		['name' => 'account#me',                        'url' => '/api/0.1/account/me', 'verb' => 'GET'],
+		['name' => 'account#updateSettings',            'url' => '/api/0.1/account/settings', 'verb' => 'PATCH'],
+		['name' => 'account#signatureGenerate',         'url' => '/api/0.1/account/signature', 'verb' => 'POST'],
+		['name' => 'account#addFiles',                  'url' => '/api/0.1/account/files', 'verb' => 'POST'],
+		['name' => 'account#accountFileList',           'url' => '/api/0.1/account/files/approval/list', 'verb' => 'GET'],
+		['name' => 'account#createSignatureElement',    'url' => '/api/0.1/account/signature/elements', 'verb' => 'POST'],
+		['name' => 'account#getSignatureElements',      'url' => '/api/0.1/account/signature/elements', 'verb' => 'GET'],
+		['name' => 'account#getSignatureElement',       'url' => '/api/0.1/account/signature/elements/{elementId}', 'verb' => 'GET'],
+		['name' => 'account#patchSignatureElement',     'url' => '/api/0.1/account/signature/elements/{elementId}', 'verb' => 'PATCH'],
+		['name' => 'account#deleteSignatureElement',    'url' => '/api/0.1/account/signature/elements/{elementId}', 'verb' => 'DELETE'],
 
-		// Deprecated
+		// Deprecated - Account
 		['name' => 'signFileDeprecated#requestSign',  'url' => '/api/0.1/webhook/register', 'verb' => 'POST'],
 		['name' => 'signFileDeprecated#updateSign',   'url' => '/api/0.1/webhook/register', 'verb' => 'PATCH'],
 		['name' => 'signFileDeprecated#removeSign',   'url' => '/api/0.1/webhook/register/signature', 'verb' => 'DELETE'],
 		['name' => 'signFileDeprecated#sign',         'url' => '/api/0.1/sign', 'verb' => 'POST'],
 
-		['name' => 'signFile#requestSign',            'url' => '/api/0.1/sign/register', 'verb' => 'POST'],
-		['name' => 'signFile#updateSign',             'url' => '/api/0.1/sign/register', 'verb' => 'PATCH'],
-		['name' => 'signFile#signUsingUuid',          'url' => '/api/0.1/sign/uuid/{uuid}', 'verb' => 'POST'],
-		['name' => 'signFile#signUsingFileId',        'url' => '/api/0.1/sign/file_id/{fileId}', 'verb' => 'POST'],
-		['name' => 'signFile#getCodeUsingUuid',      'url' => '/api/0.1/sign/uuid/{uuid}/code', 'verb' => 'POST'],
-		['name' => 'signFile#getCodeUsingFileId',    'url' => '/api/0.1/sign/file_id/{fileId}/code', 'verb' => 'POST'],
+		// Account
+		['name' => 'signFile#requestSign',                     'url' => '/api/0.1/sign/register', 'verb' => 'POST'],
+		['name' => 'signFile#updateSign',                      'url' => '/api/0.1/sign/register', 'verb' => 'PATCH'],
+		['name' => 'signFile#signUsingUuid',                   'url' => '/api/0.1/sign/uuid/{uuid}', 'verb' => 'POST'],
+		['name' => 'signFile#signUsingFileId',                 'url' => '/api/0.1/sign/file_id/{fileId}', 'verb' => 'POST'],
+		['name' => 'signFile#getCodeUsingUuid',                'url' => '/api/0.1/sign/uuid/{uuid}/code', 'verb' => 'POST'],
+		['name' => 'signFile#getCodeUsingFileId',              'url' => '/api/0.1/sign/file_id/{fileId}/code', 'verb' => 'POST'],
 		['name' => 'signFile#deleteAllSignRequestUsingFileId', 'url' => '/api/0.1/sign/file_id/{fileId}', 'verb' => 'DELETE'],
 		['name' => 'signFile#deleteOneSignRequestUsingFileId', 'url' => '/api/0.1/sign/file_id/{fileId}/{fileUserId}', 'verb' => 'DELETE'],
 
@@ -48,7 +50,9 @@ return [
 		// Deprecated
 		['name' => 'settingDeprecated#hasRootCert',   'url' => '/api/0.1/signature/has-root-cert', 'verb' => 'GET'],
 
+		// Settings
 		['name' => 'setting#hasRootCert',             'url' => '/api/0.1/setting/has-root-cert', 'verb' => 'GET'],
+
 		// Admin config
 		['name' => 'admin#generateCertificate',       'url' => '/api/0.1/admin/certificate', 'verb' => 'POST'],
 		['name' => 'admin#loadCertificate',           'url' => '/api/0.1/admin/certificate', 'verb' => 'GET'],
@@ -60,7 +64,7 @@ return [
 		['name' => 'page#resetPassword',              'url' => '/reset-password', 'verb' => 'GET'],
 		// Pages - public
 		['name' => 'page#sign',                       'url' => '/p/sign/{uuid}', 'verb' => 'GET'],
-		['name' => 'page#sign',                  	  'url' => '/p/sign/{uuid}/{path}', 'verb' => 'GET', 'requirements' => ['path' => '.+'], 'postfix' => 'extra'],
+		['name' => 'page#sign',                       'url' => '/p/sign/{uuid}/{path}', 'verb' => 'GET', 'requirements' => ['path' => '.+'], 'postfix' => 'extra'],
 		['name' => 'page#validation',                 'url' => '/p/validation', 'verb' => 'GET'],
 		['name' => 'page#validationFile',             'url' => '/p/validation/{uuid}', 'verb' => 'GET'],
 		['name' => 'page#getPdf',                     'url' => '/p/pdf/{uuid}', 'verb' => 'GET']
