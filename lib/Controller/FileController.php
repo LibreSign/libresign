8bf333078edecc3281d2e8c4364ad40b8d909313
Vitor Mattos <vitor@php.rio>
2022-01-22 21:44:08 -0300

    Refactor

--- lib/Controller/FileController.php
+++ lib/Controller/FileController.php
@@ -4,60 +4,39 @@ namespace OCA\Libresign\Controller;
 
 use OCA\Libresign\AppInfo\Application;
 use OCA\Libresign\Db\File;
-use OCA\Libresign\Db\FileElementMapper;
 use OCA\Libresign\Db\FileMapper;
 use OCA\Libresign\Db\FileUserMapper;
 use OCA\Libresign\Exception\LibresignException;
 use OCA\Libresign\Handler\TCPDILibresign;
 use OCA\Libresign\Helper\JSActions;
-use OCA\Libresign\Helper\Output;
-use OCA\Libresign\Helper\ValidateHelper;
 use OCA\Libresign\Service\AccountService;
-use OCA\Libresign\Service\FileElementService;
-use OCP\Accounts\IAccountManager;
+use OCA\Libresign\Service\FileService;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataDisplayResponse;
 use OCP\AppFramework\Http\FileDisplayResponse;
 use OCP\AppFramework\Http\JSONResponse;
 use OCP\Files\IRootFolder;
-use OCP\IConfig;
 use OCP\IL10N;
 use OCP\IRequest;
-use OCP\IURLGenerator;
-use OCP\IUserManager;
 use OCP\IUserSession;
 use Psr\Log\LoggerInterface;
 
-class LibreSignFileController extends Controller {
+class FileController extends Controller {
 	/** @var FileUserMapper */
 	private $fileUserMapper;
 	/** @var FileMapper */
 	private $fileMapper;
 	/** @var IL10N */
 	private $l10n;
-	/** @var IConfig */
-	private $config;
-	/** @var IAccountManager */
-	private $accountManager;
 	/** @var AccountService */
 	private $accountService;
 	/** @var LoggerInterface */
 	private $logger;
-	/** @var IURLGenerator */
-	private $urlGenerator;
 	/** @var IUserSession */
 	private $userSession;
-	/** @var IUserManager */
-	private $userManager;
-	/** @var FileElementMapper */
-	private $fileElementMapper;
-	/** @var ValidateHelper */
-	private $validateHelper;
-	/** @var Output */
-	private $output;
-	/** @var FileElementService */
-	private $fileElementService;
+	/** @var FileService */
+	private $fileService;
 	/** @var IRootFolder */
 	private $rootFolder;
 
@@ -66,34 +45,20 @@ class LibreSignFileController extends Controller {
 		FileUserMapper $fileUserMapper,
 		FileMapper $fileMapper,
 		IL10N $l10n,
-		IConfig $config,
-		IAccountManager $accountManager,
 		AccountService $accountService,
 		LoggerInterface $logger,
-		IURLGenerator $urlGenerator,
 		IUserSession $userSession,
-		IUserManager $userManager,
-		FileElementMapper $fileElementMapper,
-		FileElementService $fileElementService,
-		ValidateHelper $validateHelper,
-		Output $output,
+		FileService $fileService,
 		IRootFolder $rootFolder
 	) {
 		parent::__construct(Application::APP_ID, $request);
 		$this->fileUserMapper = $fileUserMapper;
 		$this->fileMapper = $fileMapper;
 		$this->l10n = $l10n;
-		$this->config = $config;
-		$this->accountManager = $accountManager;
 		$this->accountService = $accountService;
 		$this->logger = $logger;
-		$this->urlGenerator = $urlGenerator;
 		$this->userSession = $userSession;
-		$this->userManager = $userManager;
-		$this->fileElementMapper = $fileElementMapper;
-		$this->fileElementService = $fileElementService;
-		$this->validateHelper = $validateHelper;
-		$this->output = $output;
+		$this->fileService = $fileService;
 		$this->rootFolder = $rootFolder;
 	}
 
@@ -131,7 +96,7 @@ class LibreSignFileController extends Controller {
 					[$this->fileMapper, 'getBy' . $type],
 					$identifier
 				);
-				$this->output->setFile($file);
+				$this->fileService->setFile($file);
 			} catch (\Throwable $th) {
 				throw new LibresignException($this->l10n->t('Invalid data to validate file'), 404);
 			}
@@ -154,7 +119,7 @@ class LibreSignFileController extends Controller {
 		}
 
 		$return = array_merge($return,
-			$this->output
+			$this->fileService
 				->showSigners()
 				->showSettings()
 				->showVisibleElements()
@@ -245,77 +210,5 @@ class LibreSignFileController extends Controller {
 			$statusCode = $th->getCode() > 0 ? $th->getCode() : Http::STATUS_NOT_FOUND;
 			return new JSONResponse($return, $statusCode);
 		}
-	}
-
-	/**
-	 * @NoAdminRequired
-	 * @NoCSRFRequired
-	 * @return JSONResponse|FileDisplayResponse
-	 */
-	public function postElement(string $uuid, int $fileUserId, int $elementId = null, string $type = '', array $metadata = [], array $coordinates = []): JSONResponse {
-		$visibleElement = [
-			'elementId' => $elementId,
-			'type' => $type,
-			'fileUserId' => $fileUserId,
-			'coordinates' => $coordinates,
-			'metadata' => $metadata,
-			'fileUuid' => $uuid,
-		];
-		try {
-			$this->validateHelper->validateVisibleElement($visibleElement, ValidateHelper::TYPE_VISIBLE_ELEMENT_PDF);
-			$this->validateHelper->validateExistingFile([
-				'uuid' => $uuid,
-				'userManager' => $this->userSession->getUser()
-			]);
-			$fileElement = $this->fileElementService->saveVisibleElement($visibleElement, $uuid);
-			$return = [
-				'fileElementId' => $fileElement->getId(),
-				'success' => true,
-			];
-			$statusCode = Http::STATUS_OK;
-		} catch (\Throwable $th) {
-			$this->logger->error($th->getMessage());
-			$return = [
-				'success' => false,
-				'errors' => [$th->getMessage()]
-			];
-			$statusCode = $th->getCode() > 0 ? $th->getCode() : Http::STATUS_NOT_FOUND;
-		}
-		return new JSONResponse($return, $statusCode);
-	}
-
-	/**
-	 * @NoAdminRequired
-	 * @NoCSRFRequired
-	 * @return JSONResponse|FileDisplayResponse
-	 */
-	public function patchElement(string $uuid, int $fileUserId, int $elementId = null, string $type = '', array $metadata = [], array $coordinates = []) {
-		return $this->postElement($uuid, $fileUserId, $elementId, $type, $metadata, $coordinates);
-	}
-
-	/**
-	 * @NoAdminRequired
-	 * @NoCSRFRequired
-	 * @return JSONResponse|FileDisplayResponse
-	 */
-	public function deletelement(string $uuid, int $elementId): JSONResponse {
-		try {
-			$this->validateHelper->validateExistingFile([
-				'uuid' => $uuid,
-				'userManager' => $this->userSession->getUser()
-			]);
-			$this->validateHelper->validateUserIsOwnerOfPdfVisibleElement($elementId, $this->userSession->getUser()->getUID());
-			$this->fileElementService->deleteVisibleElement($elementId);
-			$return = [];
-			$statusCode = Http::STATUS_OK;
-		} catch (\Throwable $th) {
-			$this->logger->error($th->getMessage());
-			$return = [
-				'success' => false,
-				'errors' => [$th->getMessage()]
-			];
-			$statusCode = $th->getCode() > 0 ? $th->getCode() : Http::STATUS_NOT_FOUND;
-		}
-		return new JSONResponse($return, $statusCode);
 	}
 }
