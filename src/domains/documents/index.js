eef9ba6f35d0ba695c428b95e13e1a9a065e229e
Vinicius Reis <luiz.vinicius73@gmail.com>
2022-02-06 16:50:43 -0300

    🚧 approve page

--- src/domains/documents/index.js
+++ src/domains/documents/index.js
@@ -1,1 +1,14 @@
 export { default as documentsService } from './service'
+
+export const parseDocument = (entry) => {
+	const { file } = entry
+
+	return {
+		uuid: file.uuid,
+		nodeId: file.file.nodeId,
+		file_type: entry.file_type,
+		name: file.name,
+		status: file.status,
+		status_text: file.status_text,
+	}
+}
--- src/views/Documents/AccountValidation.vue
+++ src/views/Documents/AccountValidation.vue
@@ -1,13 +1,67 @@
 <script>
 import Content from '@nextcloud/vue/dist/Components/Content'
+import { documentsService, parseDocument } from '../../domains/documents'
+import { onError } from '../../helpers/errors'
+import ProgressBar from '../../Components/ProgressBar.vue'
+import { map } from 'lodash-es'
+
 export default {
 	name: 'AccountValidation',
-	components: { Content },
+	components: { Content, ProgressBar },
+	data: () => ({
+		documentList: [],
+		loading: true,
+	}),
+	mounted() {
+		this.loadDocuments()
+	},
+	methods: {
+		async loadDocuments() {
+			this.loading = true
+
+			try {
+				const { data } = await documentsService.loadApprovalList()
+				this.documentList = map(data, parseDocument)
+			} catch (err) {
+				onError(err)
+			} finally {
+				this.loading = false
+			}
+		},
+	},
 }
 </script>
 
 <template>
 	<Content class="container-account" app-name="libresign">
-		~
+		<!-- <pre>{{ documentList }}</pre> -->
+		<ProgressBar v-if="loading" infinity />
+
+		<table v-else class="libre-table is-fullwidth">
+			<thead>
+				<tr>
+					<td>
+						{{ t('libresign', 'Type') }}
+					</td>
+					<td>
+						{{ t('libresign', 'Status') }}
+					</td>
+					<td>
+						{{ t('libresign', 'Actions') }}
+					</td>
+				</tr>
+			</thead>
+			<tr v-for="(doc, index) in documentList" :key="`doc-${index}-${doc.nodeId}-${doc.file_type.key}`">
+				<td>
+					{{ doc.file_type.name }}
+				</td>
+				<td>
+					{{ doc.status_text }}
+				</td>
+				<td class="actions">
+					--
+				</td>
+			</tr>
+		</table>
 	</Content>
 </template>
