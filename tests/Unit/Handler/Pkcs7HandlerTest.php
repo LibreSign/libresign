85b6bfeecdf03cad423f86d3ded724769a8672ea
Vitor Mattos <vitor@php.rio>
2022-02-13 12:08:26 -0300

    Run pkcs7 test

--- tests/Unit/Handler/Pkcs7HandlerTest.php
+++ tests/Unit/Handler/Pkcs7HandlerTest.php
@@ -1,30 +1,44 @@
 <?php
 
 use OCA\Libresign\Handler\Pkcs7Handler;
-use org\bovigo\vfs\vfsStream;
+use PHPUnit\Framework\MockObject\MockObject;
 
 final class Pkcs7HandlerTest extends \OCA\Libresign\Tests\Unit\TestCase {
+
+	/**
+	 * @param array $methods
+	 * @return MockObject|Pkcs7Handler
+	 */
+	protected function getInstance(array $methods = []): Pkcs7Handler {
+		if (empty($methods)) {
+			return new Pkcs7Handler();
+		}
+		return $this->getMockBuilder(Pkcs7Handler::class)
+			->setConstructorArgs([])
+			->onlyMethods($methods)
+			->getMock();
+	}
+
 	public function testSignWithSuccess() {
-		$this->markTestSkipped();
-		vfsStream::setup('home');
-		$fileToSign = $this->createMock(\OCP\Files\File::class);
-		$fileToSign
-			->method('getName')
-			->willReturn('filename.txt');
-		$fileToSign
-			->method('getInternalPath')
-			->willReturn(vfsStream::url('home/filename.txt'));
+		$handler = $this->getInstance(['getP7sFile']);
+
 		$p7sFile = $this->createMock(\OCP\Files\File::class);
+		$p7sTempNam = tempnam(sys_get_temp_dir(), 'p7s');
 		$p7sFile
 			->method('getInternalPath')
-			->willReturn(vfsStream::url('home/filename.txt.p7s'));
-		$parentFolder = $this->createMock(\OCP\Files\Folder::class);
-		$parentFolder
-			->method('newFile')
+			->willReturn($p7sTempNam);
+		$handler->expects($this->once())
+			->method('getP7sFile')
 			->willReturn($p7sFile);
+
+		$fileToSign = $this->createMock(\OCP\Files\File::class);
+		$fileToSIgnTempNam = tempnam(sys_get_temp_dir(), 'txt');
+		$content = 'A simple test';
+		file_put_contents($fileToSIgnTempNam, $content);
 		$fileToSign
-			->method('getParent')
-			->willReturn($parentFolder);
+			->method('getInternalPath')
+			->willReturn($fileToSIgnTempNam);
+		$handler->setInputFile($fileToSign);
 
 		$certificate = $this->createMock(\OCP\Files\File::class);
 		$certKeys = json_decode(file_get_contents(__DIR__ . '/../../fixtures/cfssl/newcert-with-success.json'), true);
@@ -33,17 +47,12 @@ final class Pkcs7HandlerTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$certificate
 			->method('getContent')
 			->willReturn($certContent);
-		$certificate
-			->method('getInternalPath')
-			->willReturn(vfsStream::url('home/certificate.pfx'));
+		$handler->setCertificate($certificate);
 
-		$handler = new Pkcs7Handler();
-		$actual = $handler->sign(
-			$fileToSign,
-			$certificate,
-			'password'
-		);
-		$this->assertEquals('p7s', $actual->getExtension());
-		$this->assertEquals('filename.txt.p7s', $actual->getName());
+		$handler->setPassword('password');
+
+		$actual = $handler->sign();
+		$signedFile = $actual->getInternalPath();
+		$this->assertStringContainsString($content, file_get_contents($signedFile));
 	}
 }
