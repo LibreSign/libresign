ab2419b3eb263f7e42c39d787d101eef02e86a72
Vitor Mattos <vitor@php.rio>
2022-03-17 11:03:15 -0300

    Make possible accept different file formats using only one method to validate

--- lib/Service/TFile.php
+++ lib/Service/TFile.php
@@ -2,7 +2,9 @@
 
 namespace OCA\Libresign\Service;
 
+use OCA\Libresign\Exception\LibresignException;
 use OCA\Libresign\Helper\ValidateHelper;
+use OCP\Files\IMimeTypeDetector;
 use OCP\Http\Client\IClientService;
 use OCP\IL10N;
 use Psr\Log\LoggerInterface;
@@ -20,6 +22,10 @@ trait TFile {
 	private $logger;
 	/** @var IL10N */
 	private $l10n;
+	/** @var IMimeTypeDetector */
+	private $mimeTypeDetector;
+	/** @var ?string */
+	private $mimetype = null;
 
 	public function getNodeFromData(array $data): \OCP\Files\Node {
 		if (!$this->folderService->getUserId()) {
@@ -29,13 +35,63 @@ trait TFile {
 			$userFolder = $this->folderService->getFolder($data['file']['fileId']);
 			return $userFolder->getById($data['file']['fileId'])[0];
 		}
+
+		$content = $this->getFileRaw($data);
+
+		$extension = $this->getExtension($content);
+		if ($extension === 'pdf') {
+			$this->validatePdfStringWithFpdi($content);
+		}
+		$data['name'] = $this->sanitizeName($data['name'], $extension);
+
 		$userFolder = $this->folderService->getFolder();
 		$folderName = $this->folderService->getFolderName($data, $data['userManager']);
 		if ($userFolder->nodeExists($folderName)) {
 			throw new \Exception($this->l10n->t('File already exists'));
 		}
 		$folderToFile = $userFolder->newFolder($folderName);
-		return $folderToFile->newFile($data['name'] . '.pdf', $this->getFileRaw($data));
+		return $folderToFile->newFile($data['name'] . '.' . $extension, $content);
+	}
+
+	private function setMimeType(string $mimetype): void {
+		$this->validateHelper->validateMimeTypeAcceptedByMime($mimetype);
+		$this->mimetype = $mimetype;
+	}
+
+	private function getMimeType(string $content): string {
+		if (!$this->mimetype) {
+			$this->setMimeType($this->mimeTypeDetector->detectString($content));
+		}
+		return $this->mimetype;
+	}
+
+	private function getExtension(string $content): string {
+		$mimetype = $this->getMimeType($content);
+		$mappings = $this->mimeTypeDetector->getAllMappings();
+		foreach ($mappings as $ext => $mimetypes) {
+			if ($ext[0] === '_') {
+				// comment
+				continue;
+			}
+			if (in_array($mimetype, $mimetypes)) {
+				return $ext;
+			}
+		}
+		return '';
+	}
+
+	private function sanitizeName(?string $name, string $extension): ?string {
+		//MimeTypeDetector
+		if (!empty($name)) {
+			$extensionWithDot = substr($name, strlen($name) - strlen($extension) - 1);
+			if ($extensionWithDot[0] === '.') {
+				if (ltrim($extensionWithDot, '.') !== $extension) {
+					throw new LibresignException($this->l10n->t('Invalid file type.'));
+				}
+				return rtrim($name, '.' . $extension);
+			}
+		}
+		return $name;
 	}
 
 	/**
@@ -47,19 +103,20 @@ trait TFile {
 				throw new \Exception($this->l10n->t('Invalid URL file'));
 			}
 			$response = $this->client->newClient()->get($data['file']['url']);
-			$contentType = $response->getHeader('Content-Type');
-			if ($contentType !== 'application/pdf') {
-				throw new \Exception($this->l10n->t('The URL should be a PDF.'));
-			}
+			$mimetypeFromHeader = $response->getHeader('Content-Type');
 			$content = $response->getBody();
 			if (!$content) {
 				throw new \Exception($this->l10n->t('Empty file'));
 			}
 			$this->validateHelper->validateBase64($content);
+			$mimeTypeFromContent = $this->getMimeType($content);
+			if ($mimetypeFromHeader !== $mimeTypeFromContent) {
+				throw new \Exception($this->l10n->t('Invalid URL file'));
+			}
+			$this->setMimeType($mimeTypeFromContent);
 		} else {
 			$content = $this->getFileFromBase64($data['file']['base64']);
 		}
-		$this->validatePdfStringWithFpdi($content);
 		return $content;
 	}
 
@@ -67,9 +124,22 @@ trait TFile {
 		$withMime = explode(',', $base64);
 		if (count($withMime) === 2) {
 			$withMime[0] = explode(';', $withMime[0]);
+			$withMime[0][0] = explode(':', $withMime[0][0]);
+			$mimeTypeFromType = $withMime[0][0][1];
+
 			$base64 = $withMime[1];
+
+			$content = base64_decode($base64);
+			$mimeTypeFromContent = $this->getMimeType($content);
+			if ($mimeTypeFromType !== $mimeTypeFromContent) {
+				throw new \Exception($this->l10n->t('Invalid URL file'));
+			}
+			$this->setMimeType($mimeTypeFromContent);
+		} else {
+			$content = base64_decode($base64);
+			$this->getMimeType($content);
 		}
-		return base64_decode($base64);
+		return $content;
 	}
 
 	/**
