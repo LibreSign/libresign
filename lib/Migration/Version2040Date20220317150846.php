58b97aff5ff8d87a695473170f5d8a83b01600ef
Vitor Mattos <vitor@php.rio>
2022-03-17 12:14:23 -0300

    Primary key

---
+++ lib/Migration/Version2040Date20220317150846.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Libresign\Migration;
+
+use Closure;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+class Version2040Date20220317150846 extends SimpleMigrationStep {
+
+	/**
+	 * @param IOutput $output
+	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper */
+		$schema = $schemaClosure();
+		$changed = false;
+
+		$tableFileType = $schema->getTable('libresign_file_type');
+		if (!$tableFileType->hasPrimaryKey()) {
+			$tableFileType->setPrimaryKey(['id']);
+			$changed = true;
+		}
+
+		$tableAccountFile = $schema->getTable('libresign_account_file');
+		if (!$tableAccountFile->hasPrimaryKey()) {
+			$tableAccountFile->setPrimaryKey(['user_id', 'file_type']);
+			$changed = true;
+		}
+
+		if ($changed) {
+			return $schema;
+		}
+		return null;
+	}
+}
