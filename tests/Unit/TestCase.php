30df5bab9ce645941ee213d6029a6b86d4dacd19
Vitor Mattos <vitor@php.rio>
2022-09-05 22:27:21 -0300

    Remove old code

--- tests/Unit/TestCase.php
+++ tests/Unit/TestCase.php
@@ -3,16 +3,10 @@
 namespace OCA\Libresign\Tests\Unit;
 
 use OCA\Libresign\Tests\lib\AppConfigOverwrite;
-use OCA\Libresign\Tests\lib\AppConfigOverwrite20;
 
 class TestCase extends \Test\TestCase {
 	use UserTrait;
 	protected function IsDatabaseAccessAllowed() {
-		// on travis-ci.org we allow database access in any case - otherwise
-		// this will break all apps right away
-		if (true == getenv('TRAVIS')) {
-			return true;
-		}
 		$annotations = \PHPUnit\Util\Test::parseTestMethodAnnotations(get_class($this), $this->getName());
 		if (isset($annotations['class']['group'])) {
 			if (in_array('DB', $annotations['class']['group']) || in_array('SLOWDB', $annotations['class']['group'])) {
@@ -34,11 +28,7 @@ class TestCase extends \Test\TestCase {
 			return;
 		}
 		\OC::$server->registerService(\OC\AppConfig::class, function () use ($config) {
-			if (\OCP\Util::getVersion()[0] <= '20') {
-				return new AppConfigOverwrite20(\OC::$server->get(\OCP\IDBConnection::class), $config);
-			} else {
-				return new AppConfigOverwrite(\OC::$server->get(\OC\DB\Connection::class), $config);
-			}
+			return new AppConfigOverwrite(\OC::$server->get(\OC\DB\Connection::class), $config);
 		});
 	}
 }
