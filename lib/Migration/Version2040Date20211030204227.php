438395f0c77dd293a1932587d5204d8a012fb04f
Vitor Mattos <vitor@php.rio>
2022-09-10 11:04:21 -0300

    Add return types

--- lib/Migration/Version2040Date20211030204227.php
+++ lib/Migration/Version2040Date20211030204227.php
@@ -19,7 +19,6 @@ class Version2040Date20211030204227 extends SimpleMigrationStep {
 	 * @param IOutput $output
 	 * @param Closure $schemaClosure The `\Closure` returns a `ISchemaWrapper`
 	 * @param array $options
-	 * @return null|ISchemaWrapper
 	 */
 	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ISchemaWrapper {
 		$schema = $schemaClosure();
