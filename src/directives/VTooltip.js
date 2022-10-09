f0404a8b0fa361668ee463abe26edb99fd8ad45e
Vitor Mattos <vitor@php.rio>
2022-10-09 07:24:08 -0300

    Remove vtooltip, no more used

--- src/directives/VTooltip.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import VTooltip from '@nextcloud/vue/dist/Directives/Tooltip'
-
-import '@nextcloud/dialogs/styles/toast.scss'
-
-Vue.directive('Tooltip', VTooltip)
-VTooltip.options.autohide = true
