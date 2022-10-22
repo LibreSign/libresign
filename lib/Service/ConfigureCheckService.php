1d4798e1787f853050eaf7fe272f3cff3cb71f5e
Alice Fantoni <fantonicontato@gmail.com>
2022-10-22 11:21:50 -0300

    Bug fix: install
    Signed-off-by: Alice Fantoni <fantonicontato@gmail.com>
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/ConfigureCheckService.php
+++ lib/Service/ConfigureCheckService.php
@@ -96,7 +96,7 @@ class ConfigureCheckService {
 		$jsignpdJarPath = $this->config->getAppValue(Application::APP_ID, 'jsignpdf_jar_path');
 		if ($jsignpdJarPath) {
 			if (file_exists($jsignpdJarPath)) {
-				if (!$this->isJavaOk()){
+				if (!$this->isJavaOk()) {
 					return [
 						(new ConfigureCheckHelper())
 							->setErrorMessage('Necessary Java to run JSignPdf')
