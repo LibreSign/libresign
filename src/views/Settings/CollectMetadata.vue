1cba0309f0b407389f4d8db3a33302b75f86e785
Vitor Mattos <vitor@php.rio>
2023-02-25 14:09:18 -0300

    Collect metadata of signers
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/views/Settings/CollectMetadata.vue
@@ -0,0 +1,50 @@
+<template>
+	<NcSettingsSection :title="title" :description="description">
+		<NcCheckboxRadioSwitch
+			type="switch"
+			:checked.sync="collectMetadataEnabled"
+			@update:checked="saveCollectMetadata()">
+			{{ t('libresign', 'Collect signers metadata when sign a document') }}
+		</NcCheckboxRadioSwitch>
+	</NcSettingsSection>
+</template>
+<script>
+import { translate as t } from '@nextcloud/l10n'
+import NcSettingsSection from '@nextcloud/vue/dist/Components/NcSettingsSection'
+import NcCheckboxRadioSwitch from '@nextcloud/vue/dist/Components/NcCheckboxRadioSwitch'
+import { generateOcsUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios'
+
+export default {
+	name: 'CollectMetadata',
+	components: {
+		NcSettingsSection,
+		NcCheckboxRadioSwitch,
+	},
+	data() {
+		return {
+			title: t('libresign', 'Collect signers metadata'),
+			description: t('libresign', 'Enabling this feature, every when sign a document, LibreSign will store the IP, and user agent of signer.'),
+			collectMetadataEnabled: false,
+		}
+	},
+	created() {
+		this.getData()
+	},
+	methods: {
+		async getData() {
+			const responseCollectMetadata = await axios.get(generateOcsUrl('/apps/provisioning_api/api/v1', 2) + '/config/apps/libresign/collect_metadata', {})
+			this.collectMetadataEnabled = responseCollectMetadata.data.ocs.data.data ? true : false
+		},
+		saveCollectMetadata() {
+			OCP.AppConfig.setValue('libresign', 'collect_metadata', this.collectMetadataEnabled ? 1 : 0)
+		},
+	},
+}
+</script>
+<style scoped>
+.collect-metadata-content{
+	display: flex;
+	flex-direction: column;
+}
+</style>
