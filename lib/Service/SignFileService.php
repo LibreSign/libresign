fa88fae33301d76aba7ea22dd3451727f8d78977
Vitor Mattos <vitor@php.rio>
2023-04-06 11:47:30 -0300

    Rename method to be more clear to identify what the method do
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/SignFileService.php
+++ lib/Service/SignFileService.php
@@ -838,7 +838,7 @@ class SignFileService {
 				'errors' => [$this->l10n->t('Invalid UUID')],
 			]));
 		}
-		$this->trhowIfFileUserNotExists($uuid, $user, $fileUser);
+		$this->trhowIfCantIdentifyUser($uuid, $user, $fileUser);
 		$this->throwIfUserIsNotSigner($user, $fileUser);
 		$this->throwIfAlreadySigned($fileEntity, $fileUser);
 		$this->throwIfInvalidUser($uuid, $user);
@@ -922,7 +922,7 @@ class SignFileService {
 		}
 	}
 
-	private function trhowIfFileUserNotExists(string $uuid, ?IUser $user, ?FileUserEntity $fileUser): void {
+	private function trhowIfCantIdentifyUser(string $uuid, ?IUser $user, ?FileUserEntity $fileUser): void {
 		if ($fileUser instanceof FileUserEntity) {
 			$fileUserId = $fileUser->getUserId();
 			if ($fileUserId) {
