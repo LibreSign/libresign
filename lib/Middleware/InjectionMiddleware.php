66972e9c8ba854fc037436300acbeafd3c05ebbf
Vitor Mattos <vitor@php.rio>
2023-05-06 13:25:24 -0300

    Move logic of require manager to argument
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ lib/Middleware/InjectionMiddleware.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2019 Joas Schilling <coding@schilljs.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Libresign\Middleware;
+
+use OCA\Libresign\Controller\AEnvironmentAwareController;
+use OCA\Libresign\Helper\ValidateHelper;
+use OCA\Libresign\Middleware\Attribute\RequireManager;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http;
+use OCP\AppFramework\Middleware;
+use OCP\IL10N;
+use OCP\IRequest;
+use OCP\IUser;
+use OCP\IUserSession;
+
+class InjectionMiddleware extends Middleware {
+	protected ?string $userId;
+
+	public function __construct(
+		protected IRequest $request,
+		protected IUserSession $userSession,
+		protected ValidateHelper $validateHelper,
+		protected IL10N $l10n,
+		?string $userId,
+	) {
+		$this->request = $request;
+		$this->userId = $userId;
+	}
+
+	/**
+	 * @param Controller $controller
+	 * @param string $methodName
+	 * @throws LobbyException
+	 * @throws NotAModeratorException
+	 * @throws ParticipantNotFoundException
+	 * @throws PermissionsException
+	 * @throws ReadOnlyException
+	 * @throws RoomNotFoundException
+	 */
+	public function beforeController(Controller $controller, string $methodName): void {
+		if (!$controller instanceof AEnvironmentAwareController) {
+			return;
+		}
+
+		$reflectionMethod = new \ReflectionMethod($controller, $methodName);
+
+		$apiVersion = $this->request->getParam('apiVersion');
+		$controller->setAPIVersion((int) substr($apiVersion, 1));
+
+		if (!empty($reflectionMethod->getAttributes(RequireManager::class))) {
+			$this->getLoggedIn();
+		}
+	}
+
+	private function getLoggedIn(): void {
+		$user = $this->userSession->getUser();
+		if (!$user instanceof IUser) {
+			throw new \Exception($this->l10n->t('You are not allowed to request signing'), Http::STATUS_UNPROCESSABLE_ENTITY);
+		}
+		$this->validateHelper->canRequestSign($user);
+	}
+}
