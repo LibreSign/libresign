3e755e931250ef2eceef3c79a1e349877fc0b79d
Enieber Cunha <eniebercunha@gmail.com>
2023-05-09 17:23:33 -0300

    feat: remove input not used
    Signed-off-by: Enieber Cunha <eniebercunha@gmail.com>
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/Settings/IdentifierFactor.vue
+++ src/views/Settings/IdentifierFactor.vue
@@ -6,11 +6,12 @@
 	  	>
  		{{ t('libresign', 'User')}}
 	  </NcCheckboxRadioSwitch>
-		<div class="container-checkbox">	
-			<NcActionCheckbox
-	  		:checked.sync="allowedInviteUser"
+		<div v-if="useUser">
+			<div class="container-checkbox">	
+				<NcActionCheckbox
+	  			:checked.sync="allowedInviteUser"
 	  		>
- 				{{ t('libresign', 'Allow account creation for new users')}}
+				{{ t('libresign', 'Allow account creation for new users')}}
 	  	</NcActionCheckbox>
 		
 			<p>{{ t('libresign', 'Allows sending registration email when the user does not have an account.') }}</p>
@@ -18,13 +19,13 @@
 		
 		<div class="container-select">
 			<label for="selectIdentificationDefault">{{ t('libresign', 'Default signature method')}}</label>
-
-			<NcSelect
-				v-model="selectedDefaultIdentification"
-				:options="options"
-				inputId="selectIdentificationDefault"
-			/>
-		</div>
+				<NcSelect
+					v-model="selectedDefaultIdentification"
+					:options="options"
+					inputId="selectIdentificationDefault"
+				/>
+			</div>
+			</div>
 		<hr/>
 
 		<NcCheckboxRadioSwitch
@@ -32,29 +33,16 @@
 			:checked.sync="useTelegram">
 	  		{{ t('libresign', 'Telegram')}}
 	  </NcCheckboxRadioSwitch>
-		<div class="container-checkbox">	
-			<NcActionCheckbox
-	  		:checked.sync="requiredTelegram"
-	  		>
- 				{{ t('libresign', 'Make this method required')}}
-	  	</NcActionCheckbox>
-		</div>	
-
-		<NcTextField
-	  	:value.sync="botTelegramName"
-			label="Bot"
-			trailing-button-icon="close"
-			@trailing-button-click="clearText"
-			:label-visible="true"
-		/>
-	  <NcTextField
-	  	:value.sync="botTelegramApi"
-			label="API Token"
-			trailing-button-icon="close"
-			@trailing-button-click="clearText"
-		:label-visible="true"
-		/>
-	
+		<div v-if="useTelegram">
+			<div class="container-checkbox">	
+				<NcActionCheckbox
+	  			:checked.sync="requiredTelegram"
+	  			>
+ 					{{ t('libresign', 'Make this method required')}}
+	  		</NcActionCheckbox>
+			</div>
+		</div>
+>
 		<hr/>
 
 		<NcCheckboxRadioSwitch
@@ -62,52 +50,34 @@
 			:checked.sync="useSMS">
 	  		{{ t('libresign', 'SMS')}}
 	  </NcCheckboxRadioSwitch>
-	  
-	  <div class="container-checkbox">	
-			<NcActionCheckbox
-	  		:checked.sync="requiredSMS"
-	  		>
+	 	<div v-if="useSMS">
+	  	<div class="container-checkbox">	
+				<NcActionCheckbox
+	  			:checked.sync="requiredSMS"
+	  			>
  				{{ t('libresign', 'Make this method required')}}
 	  	</NcActionCheckbox>
-		</div>
+			</div>
 
-	  <NcTextField
-	  	:value.sync="smsApiToken"
-			label="API Token"
-			trailing-button-icon="close"
-			@trailing-button-click="clearText"
-		:label-visible="true"
-		/>
-		<hr/>
+		</div>
+	  <hr/>
 
 		<NcCheckboxRadioSwitch
 			type="switch"
 			:checked.sync="useSignal">
 	  		{{ t('libresign', 'Signal')}}
 	  </NcCheckboxRadioSwitch>
-	  <div class="container-checkbox">	
-			<NcActionCheckbox
-	  		:checked.sync="requiredSignal"
-	  		>
- 			{{ t('libresign', 'Make this method required')}}
-	  	</NcActionCheckbox>
-		</div>
-		<div class="container-checkbox">	
-			<NcActionCheckbox
-	  		:checked.sync="identificationSignal"
-	  		>
- 				{{ t('libresign', 'allow this method like identifiation')}}
-	  	</NcActionCheckbox>
-		</div>
-		<NcTextField
-	  	:value.sync="signalApiToken"
-			label="API Token"
-			trailing-button-icon="close"
-			@trailing-button-click="clearText"
-		:label-visible="true"
-		/>
 
-</NcSettingsSection>
+	  <div v-if="useSignal">
+	  	<div class="container-checkbox">	
+				<NcActionCheckbox
+	  			:checked.sync="requiredSignal"
+	  			>
+ 					{{ t('libresign', 'Make this method required')}}
+	  		</NcActionCheckbox>
+			</div>
+		</div>
+	</NcSettingsSection>
 </template>
 <script>
 import { translate as t } from '@nextcloud/l10n'
