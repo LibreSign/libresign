8d2d650abe5a4fbf70a63ffbff9395bf015916d5
Vitor Mattos <vitor@php.rio>
2023-05-20 17:02:49 -0300

    Add initial state to admin settings
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -3,13 +3,24 @@
 namespace OCA\Libresign\Settings;
 
 use OCA\Libresign\AppInfo\Application;
+use OCA\Libresign\Service\IdentifyMethodService;
 use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
 use OCP\Settings\ISettings;
 use OCP\Util;
 
 class Admin implements ISettings {
+	public function __construct(
+		private IInitialState $initialState,
+		private IdentifyMethodService $identifyMethodService
+	) {
+	}
 	public function getForm(): TemplateResponse {
 		Util::addScript(Application::APP_ID, 'libresign-settings');
+		$this->initialState->provideInitialState(
+			'identify_methods',
+			$this->identifyMethodService->getAdminInitialState()
+		);
 		return new TemplateResponse(Application::APP_ID, 'admin_settings');
 	}
 
