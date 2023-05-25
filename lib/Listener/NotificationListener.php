badeff1585c5ea01ed90123a6ce9302ae047cc2f
Vitor Mattos <vitor@php.rio>
2023-05-25 12:30:42 -0300

    Add nextcloud event to notify when the identification method is account
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ lib/Listener/NotificationListener.php
@@ -0,0 +1,78 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright Copyright (c) 2023 Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace OCA\Libresign\Listener;
+
+use OCA\Libresign\AppInfo\Application as AppInfoApplication;
+use OCA\Libresign\Db\FileUser;
+use OCA\Libresign\Events\SendSignNotificationEvent;
+use OCA\Libresign\Service\IdentifyMethod\AbstractIdentifyMethod;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\EventDispatcher\Event;
+use OCP\EventDispatcher\IEventListener;
+use OCP\Notification\IManager;
+
+/**
+ * @template-implements IEventListener<Event>
+ */
+class NotificationListener implements IEventListener {
+	public function __construct(
+		private IManager $notificationManager,
+		private ITimeFactory $timeFactory
+	) {
+	}
+
+	public function handle(Event $event): void {
+		if ($event instanceof SendSignNotificationEvent) {
+			$this->sendNewSignNotification(
+				$event->getFileUser(),
+				$event->getIdentifyMethod(),
+				$event->isNew()
+			);
+		}
+	}
+
+	private function sendNewSignNotification(
+		FileUser $fileUser,
+		AbstractIdentifyMethod $identifyMethod,
+		bool $isNew
+	): void {
+		$notification = $this->notificationManager->createNotification();
+		$notification
+			->setApp(AppInfoApplication::APP_ID)
+			->setObject('identifyMethod', (string) $identifyMethod->getEntity()->getId())
+			->setDateTime((new \DateTime())->setTimestamp($this->timeFactory->now()->getTimestamp()))
+			->setUser($identifyMethod->getEntity()->getIdentifierValue());
+		if ($isNew) {
+			$notification->setSubject('new_sign_request', [
+				'fileUser' => $fileUser->getId(),
+			]);
+		} else {
+			$notification->setSubject('update_sign_request', [
+				'fileUser' => $fileUser->getId(),
+			]);
+		}
+		$this->notificationManager->notify($notification);
+	}
+}
