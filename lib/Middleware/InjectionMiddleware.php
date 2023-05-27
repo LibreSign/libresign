53de112b03313ea2e6192e387fd871a28f01db76
Vitor Mattos <vitor@php.rio>
2023-05-27 18:34:42 -0300

    Add argument RequireSigner
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Middleware/InjectionMiddleware.php
+++ lib/Middleware/InjectionMiddleware.php
@@ -24,28 +24,43 @@ declare(strict_types=1);
 namespace OCA\Libresign\Middleware;
 
 use OC\AppFramework\Http as AppFrameworkHttp;
+use OCA\Libresign\AppInfo\Application;
 use OCA\Libresign\Controller\AEnvironmentAwareController;
+use OCA\Libresign\Controller\AEnvironmentPageAwareController;
+use OCA\Libresign\Db\FileMapper;
+use OCA\Libresign\Db\FileUserMapper;
 use OCA\Libresign\Exception\LibresignException;
+use OCA\Libresign\Exception\PageException;
 use OCA\Libresign\Helper\ValidateHelper;
 use OCA\Libresign\Middleware\Attribute\RequireManager;
+use OCA\Libresign\Middleware\Attribute\RequireSigner;
+use OCA\Libresign\Service\IdentifyMethodService;
 use OCP\AppFramework\Controller;
 use OCP\AppFramework\Http;
+use OCP\AppFramework\Http\ContentSecurityPolicy;
 use OCP\AppFramework\Http\JSONResponse;
 use OCP\AppFramework\Http\Response;
+use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Middleware;
+use OCP\AppFramework\Services\IInitialState;
 use OCP\IL10N;
 use OCP\IRequest;
 use OCP\IUser;
 use OCP\IUserSession;
+use OCP\Util;
 
 class InjectionMiddleware extends Middleware {
 	protected ?string $userId;
 
 	public function __construct(
-		protected IRequest $request,
-		protected IUserSession $userSession,
-		protected ValidateHelper $validateHelper,
-		protected IL10N $l10n,
+		private IRequest $request,
+		private IUserSession $userSession,
+		private ValidateHelper $validateHelper,
+		private FileUserMapper $fileUserMapper,
+		private FileMapper $fileMapper,
+		private IInitialState $initialState,
+		private IdentifyMethodService $identifyMethodService,
+		private IL10N $l10n,
 		?string $userId,
 	) {
 		$this->request = $request;
@@ -58,17 +73,25 @@ class InjectionMiddleware extends Middleware {
 	 * @throws \Exception
 	 */
 	public function beforeController(Controller $controller, string $methodName) {
-		if (!$controller instanceof AEnvironmentAwareController) {
-			return;
+		switch (true) {
+			case $controller instanceof AEnvironmentAwareController:
+				$apiVersion = $this->request->getParam('apiVersion');
+				$controller->setAPIVersion((int) substr($apiVersion, 1));
+				break;
+			case $controller instanceof AEnvironmentPageAwareController:
+				break;
+			default:
+				return;
 		}
 
 		$reflectionMethod = new \ReflectionMethod($controller, $methodName);
 
-		$apiVersion = $this->request->getParam('apiVersion');
-		$controller->setAPIVersion((int) substr($apiVersion, 1));
-
 		if (!empty($reflectionMethod->getAttributes(RequireManager::class))) {
 			$this->getLoggedIn();
+		}
+
+		if (!empty($reflectionMethod->getAttributes(RequireSigner::class))) {
+			$this->requireSigner();
 		}
 	}
 
@@ -80,6 +103,17 @@ class InjectionMiddleware extends Middleware {
 		$this->validateHelper->canRequestSign($user);
 	}
 
+	private function requireSigner(): void {
+		$uuid = $this->request->getParam('uuid');
+
+		try {
+			$user = $this->userSession->getUser();
+			$this->validateHelper->validateSigner($uuid, $user);
+		} catch (LibresignException $e) {
+			throw new PageException($e->getMessage());
+		}
+	}
+
 	/**
 	 * @param Controller $controller
 	 * @param string $methodName
@@ -88,15 +122,26 @@ class InjectionMiddleware extends Middleware {
 	 * @return Response
 	 */
 	public function afterException($controller, $methodName, \Exception $exception): Response {
-		if ($exception instanceof LibresignException) {
-			return new JSONResponse(
-				[
-					'message' => $exception->getMessage(),
-				],
-				$exception->getCode() === 0
-					? AppFrameworkHttp::STATUS_UNPROCESSABLE_ENTITY
-					: $exception->getCode()
-			);
+		switch (true) {
+			case $exception instanceof LibresignException:
+				return new JSONResponse(
+					[
+						'message' => $exception->getMessage(),
+					],
+					$exception->getCode() === 0
+						? AppFrameworkHttp::STATUS_UNPROCESSABLE_ENTITY
+						: $exception->getCode()
+				);
+			case $exception instanceof PageException:
+				$this->initialState->provideInitialState('config', json_decode($exception->getMessage(), true));
+
+				Util::addScript(Application::APP_ID, 'libresign-external');
+				$response = new TemplateResponse(Application::APP_ID, 'external', [], TemplateResponse::RENDER_AS_BASE);
+
+				$policy = new ContentSecurityPolicy();
+				$policy->addAllowedFrameDomain('\'self\'');
+				$response->setContentSecurityPolicy($policy);
+				return $response;
 		}
 
 		throw $exception;
