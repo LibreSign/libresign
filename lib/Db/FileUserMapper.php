f8874f35a093ed0b7c3d40e86dcee7fd03631f51
Vitor Mattos <vitor@php.rio>
2023-05-27 18:22:19 -0300

    Fix list of documents associated to an account
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Db/FileUserMapper.php
+++ lib/Db/FileUserMapper.php
@@ -4,6 +4,7 @@ namespace OCA\Libresign\Db;
 
 use OCA\Libresign\Helper\Pagination;
 use OCA\Libresign\Service\IdentifyMethod\IIdentifyMethod;
+use OCA\Libresign\Service\IdentifyMethodService;
 use OCP\AppFramework\Db\DoesNotExistException;
 use OCP\AppFramework\Db\QBMapper;
 use OCP\DB\QueryBuilder\IQueryBuilder;
@@ -273,7 +274,7 @@ class FileUserMapper extends QBMapper {
 		int $page = null,
 		int $length = null
 	): array {
-		$pagination = $this->getFilesAssociatedFilesWithMeStmt($user->getUID());
+		$pagination = $this->getFilesAssociatedFilesWithMeStmt($user->getUID(), $user->getEMailAddress());
 		$pagination->setMaxPerPage($length);
 		$pagination->setCurrentPage($page);
 		$currentPageResults = $pagination->getCurrentPageResults();
@@ -307,21 +308,24 @@ class FileUserMapper extends QBMapper {
 		$qb->select('im.*')
 			->from('libresign_identify_method', 'im')
 			->where(
-				$qb->expr()->in('im.file_user_id', $qb->createNamedParameter($fileUserIds, IQueryBuilder::PARAM_INT_ARRAY))
+				$qb->expr()->in('im.file_user_id', $qb->createParameter('fileUserIds'))
 			)
-			->orderBy('im.default', 'DESC')
+			->orderBy('im.mandatory', 'DESC')
 			->addOrderBy('im.identified_at_date', 'ASC');
 
-		$cursor = $qb->executeQuery();
 		$return = [];
-		while ($row = $cursor->fetch()) {
-			$identifyMethod = new IdentifyMethod();
-			$return[$row['file_user_id']][$row['method']] = $identifyMethod->fromRow($row);
+		foreach (array_chunk($fileUserIds, 1000) as $fileUserIdsChunk) {
+			$qb->setParameter('fileUserIds', $fileUserIdsChunk, IQueryBuilder::PARAM_INT_ARRAY);
+			$cursor = $qb->executeQuery();
+			while ($row = $cursor->fetch()) {
+				$identifyMethod = new IdentifyMethod();
+				$return[$row['file_user_id']][$row['method']] = $identifyMethod->fromRow($row);
+			}
 		}
 		return $return;
 	}
 
-	private function getFilesAssociatedFilesWithMeStmt(string $userId): Pagination {
+	private function getFilesAssociatedFilesWithMeStmt(string $userId, string $email): Pagination {
 		$qb = $this->db->getQueryBuilder();
 		$qb->select(
 			'f.id',
@@ -336,16 +340,19 @@ class FileUserMapper extends QBMapper {
 			->selectAlias($qb->func()->max('fu.signed'), 'status_date')
 			->from('libresign_file', 'f')
 			->leftJoin('f', 'libresign_file_user', 'fu', 'fu.file_id = f.id')
-			->leftJoin('f', 'libresign_identify_method', 'im', $qb->expr()->andX(
-				$qb->expr()->eq('fu.id', 'im.file_user_id'),
-				$qb->expr()->eq('im.method', $qb->createNamedParameter('account')),
-				$qb->expr()->eq('im.identifier_key', $qb->createNamedParameter('uid'))
-			))
-			->leftJoin('f', 'users', 'u', 'im.identifier_value = u.uid')
+			->leftJoin('f', 'libresign_identify_method', 'im', $qb->expr()->eq('fu.id', 'im.file_user_id'))
+			->join('f', 'users', 'u', 'f.user_id = u.uid')
 			->where(
 				$qb->expr()->orX(
 					$qb->expr()->eq('f.user_id', $qb->createNamedParameter($userId)),
-					$qb->expr()->eq('im.identifier_value', $qb->createNamedParameter($userId))
+					$qb->expr()->andX(
+						$qb->expr()->eq('im.identifier_key', $qb->createNamedParameter(IdentifyMethodService::IDENTIFY_ACCOUNT)),
+						$qb->expr()->eq('im.identifier_value', $qb->createNamedParameter($userId))
+					),
+					$qb->expr()->andX(
+						$qb->expr()->eq('im.identifier_key', $qb->createNamedParameter(IdentifyMethodService::IDENTIFY_EMAIL)),
+						$qb->expr()->eq('im.identifier_value', $qb->createNamedParameter($email))
+					)
 				)
 			)
 			->groupBy(
@@ -359,15 +366,18 @@ class FileUserMapper extends QBMapper {
 				'u.displayname'
 			);
 
-		$countQueryBuilderModifier = function (IQueryBuilder &$qb) use ($userId): void {
-			$count = $qb->getConnection()->getQueryBuilder();
-			$count->selectAlias($count->func()->count(), 'total_results')
+		$countQueryBuilderModifier = function (IQueryBuilder &$qb) use ($userId, $email): void {
+			$query = $qb->getConnection()->getQueryBuilder();
+			$subQuery = $qb->getConnection()->getQueryBuilder();
+			$subQuery->resetQueryParts(['select', 'groupBy']);
+			$subQuery->select('f.id')
+				->groupBy('f.id');
+
+			$query->setParameters($subQuery->getParameters());
+			$query->selectAlias($query->func()->count(), 'total_results')
 				->from('libresign_file', 'f')
-				->where(
-					$count->expr()->eq('f.user_id', $count->createNamedParameter($userId)),
-				)
-				->setMaxResults(1);
-			$qb = $count;
+				->where($query->expr()->in('f.id', $query->createFunction($subQuery->getSQL())));
+			$qb = $query;
 		};
 
 		$pagination = new Pagination($qb, $countQueryBuilderModifier);
@@ -389,9 +399,7 @@ class FileUserMapper extends QBMapper {
 					$identifyMethodsOfSigner = $identifyMethods[$signer->getId()] ?? [];
 					$data = [
 						'email' => array_reduce($identifyMethodsOfSigner, function (string $carry, IdentifyMethod $identifyMethod): string {
-							if ($identifyMethod->getIdentifierKey() === 'uid') {
-								return $identifyMethod->getIdentifierValue();
-							} elseif ($identifyMethod->getIdentifierKey() === 'email') {
+							if ($identifyMethod->getIdentifierKey() === IdentifyMethodService::IDENTIFY_EMAIL) {
 								return $identifyMethod->getIdentifierValue();
 							}
 							return $carry;
@@ -403,7 +411,7 @@ class FileUserMapper extends QBMapper {
 							->format('Y-m-d H:i:s'),
 						'sign_date' => null,
 						'uid' => array_reduce($identifyMethodsOfSigner, function (string $carry, IdentifyMethod $identifyMethod): string {
-							if ($identifyMethod->getIdentifierKey() === 'uid') {
+							if ($identifyMethod->getIdentifierKey() === IdentifyMethodService::IDENTIFY_ACCOUNT) {
 								return $identifyMethod->getIdentifierValue();
 							}
 							return $carry;
@@ -413,8 +421,15 @@ class FileUserMapper extends QBMapper {
 							if (!$user->getEMailAddress()) {
 								return false;
 							}
-							if ($identifyMethod->getIdentifierKey() === 'uid') {
+							if ($identifyMethod->getIdentifierKey() === IdentifyMethodService::IDENTIFY_ACCOUNT) {
 								if ($user->getUID() === $identifyMethod->getIdentifierValue()) {
+									return true;
+								}
+							} elseif ($identifyMethod->getIdentifierKey() === IdentifyMethodService::IDENTIFY_EMAIL) {
+								if (!$user->getEMailAddress()) {
+									return false;
+								}
+								if ($user->getEMailAddress() === $identifyMethod->getIdentifierValue()) {
 									return true;
 								}
 							}
@@ -423,10 +438,10 @@ class FileUserMapper extends QBMapper {
 						'identifyMethods' => array_map(function (IdentifyMethod $identifyMethod) use ($signer): array {
 							return [
 								'method' => $identifyMethod->getMethod(),
-								'default' => $identifyMethod->getDefault(),
+								'mandatory' => $identifyMethod->getMandatory(),
 								'identifiedAtDate' => $identifyMethod->getIdentifiedAtDate()
 							];
-						}, $identifyMethodsOfSigner),
+						}, array_values($identifyMethodsOfSigner)),
 					];
 
 					if ($data['me']) {
