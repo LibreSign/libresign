2e1a3384740b618540b1bb5bf1ee237ff91f04db
Vitor Mattos <vitor@php.rio>
2023-09-17 16:10:05 -0300

    Fix frontend to configure root certifacete data
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/helpers/certification.js
+++ src/helpers/certification.js
@@ -1,46 +1,50 @@
 import { Option } from '@marionebl/option'
 import { translate as t } from '@nextcloud/l10n'
 
+/**
+ * Return custom options details from ID
+ *
+ * @param {string} id identification of custom option
+ */
 export function selectCustonOption(id) {
-  return Option.from(options.find(item => item.id === id))
+	return Option.from(options.find(item => item.id === id))
 }
 
 export const options = [
-  {
+	{
 		id: 'C',
 		label: 'Country',
 		min: 2,
 		max: 2,
 		value: '',
-		minHelper: t('libresign', 'Two-letter ISO 3166 country code'),
-		defaultHelper: t('libresign', 'Two-letter ISO 3166 country code'),
+		helperText: t('libresign', 'Two-letter ISO 3166 country code'),
 	},
 	{
 		id: 'ST',
 		label: 'State',
 		min: 1,
 		value: '',
-		defaultHelper: t('libresign', 'Full name of states or provinces'),
+		helperText: t('libresign', 'Full name of states or provinces'),
 	},
 	{
 		id: 'L',
 		label: 'Locality',
 		min: 1,
 		value: '',
-		defaultHelper: t('libresign', 'Name of a locality or place, such as a city, county, or other geographic region'),
+		helperText: t('libresign', 'Name of a locality or place, such as a city, county, or other geographic region'),
 	},
 	{
 		id: 'O',
 		label: 'Organization',
 		min: 1,
 		value: '',
-		defaultHelper: t('libresign', 'Name of an organization'),
+		helperText: t('libresign', 'Name of an organization'),
 	},
 	{
 		id: 'OU',
 		label: 'OrganizationalUnit',
 		min: 1,
 		value: '',
-		defaultHelper: t('libresign', 'Name of an organizational unit'),
+		helperText: t('libresign', 'Name of an organizational unit'),
 	},
 ]
