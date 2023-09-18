aba88839695d192a79992cb0be9a6cb972926fd6
Vitor Mattos <vitor@php.rio>
2023-09-18 12:43:14 -0300

    Fix cammelize
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- appinfo/routes/routesFileController.php
+++ appinfo/routes/routesFileController.php
@@ -6,7 +6,7 @@ $requirements = [
 
 return [
 	'ocs' => [
-		['name' => 'File#Save',           'url' => '/api/{apiVersion}/file', 'verb' => 'POST', 'requirements' => $requirements],
+		['name' => 'File#save',           'url' => '/api/{apiVersion}/file', 'verb' => 'POST', 'requirements' => $requirements],
 		['name' => 'File#list',           'url' => '/api/{apiVersion}/file/list', 'verb' => 'GET', 'requirements' => $requirements],
 		['name' => 'File#validateUuid',   'url' => '/api/{apiVersion}/file/validate/uuid/{uuid}', 'verb' => 'GET', 'requirements' => $requirements],
 		['name' => 'File#validateFileId', 'url' => '/api/{apiVersion}/file/validate/file_id/{fileId}', 'verb' => 'GET', 'requirements' => $requirements],
