7f394a2fbf961319a550330b7e7e3a7579b56e90
Vitor Mattos <vitor@php.rio>
2023-09-26 12:51:56 -0300

    Create component to request signature
    WIP
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Components/Request/RequestSignature.vue
@@ -0,0 +1,31 @@
+<template>
+	<div>
+		<NcButton v-if="canAddSigner">
+			{{ t('libresign', 'Add signer') }}
+		</NcButton>
+		<Signers :signers="signers">
+		</Signers>
+	</div>
+</template>
+<script>
+import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
+import Signers from '../Signers/Signers.vue'
+export default {
+	name: 'RequestSignature',
+	components: {
+		NcButton,
+		Signers,
+	},
+	props: {
+		signers: {
+			type: Array,
+			required: true,
+		},
+	},
+	data() {
+		return {
+			canAddSigner: true,
+		}
+	}
+}
+</script>
