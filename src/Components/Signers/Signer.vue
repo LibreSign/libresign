b74814a1c10c0a4320bdbb5b0aa805c031687419
Vitor Mattos <vitor@php.rio>
2023-09-26 12:51:33 -0300

    Create components to list signers
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Components/Signers/Signer.vue
@@ -0,0 +1,66 @@
+<template>
+	<div>
+		<NcListItem
+			:title="name"
+			:forceDisplayActions="true">
+			<template #icon>
+				<NcAvatar :size="44" display-name="displayName" />
+			</template>
+			<template #subtitle>
+				<Bullet v-for="method in identifyMethodsNames" :key="method" :name="method" />
+			</template>
+			<template #actions>
+				<NcActionButton
+					ariaLabel="Delete"
+					@click="deleteItem">
+					<template #icon>
+						<Delete :size="20" />
+					</template>
+				</NcActionButton>
+			</template>
+		</NcListItem>
+	</div>
+</template>
+<script>
+import NcListItem from '@nextcloud/vue/dist/Components/NcListItem.js'
+import NcAvatar from '@nextcloud/vue/dist/Components/NcAvatar.js'
+import NcActionButton from '@nextcloud/vue/dist/Components/NcActionButton'
+import Delete from 'vue-material-design-icons/Delete'
+import Bullet from '../Bullet/Bullet.vue'
+
+export default {
+	name: 'Signer',
+	components: {
+		NcListItem,
+		NcAvatar,
+		NcActionButton,
+		Delete,
+		Bullet,
+	},
+	props: {
+		displayName: {
+			type: String,
+			required: true,
+		},
+		fileUserId: {
+			type: Number,
+			required: true,
+
+		},
+		identifyMethods: {
+			type: Array,
+			required: true,
+		},
+	},
+	computed: {
+		identifyMethodsNames() {
+			return this.identifyMethods.map(method => Object.keys(method)[0])
+		}
+	},
+	methods: {
+		deleteItem() {
+			
+		}
+	},
+}
+</script>
---
+++ src/Components/Signers/Signers.vue
@@ -0,0 +1,25 @@
+<template>
+	<ul>
+		<Signer v-for="signer in signers"
+			:key="signer.id"
+			:name="signer.name"
+			:fileUserId="signer.fileUserId"
+			:identifyMethods=signer.identify>
+		</Signer>
+	</ul>
+</template>
+<script>
+import Signer from './Signer.vue'
+export default {
+	name: 'Signers',
+	components: {
+		Signer,
+	},
+	props: {
+		signers: {
+			type: Array,
+			required: true,
+		},
+	},
+}
+</script>
