fe39457cd892545bc71046fa8939d7fde501c350
Vitor Mattos <vitor@php.rio>
2023-09-26 12:50:38 -0300

    Create bullet command
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/Components/Bullet/Bullet.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="bullet__wrapper">
+		<span class="bullet__name">
+			{{ name }}
+		</span>
+	</div>
+</template>
+<script>
+export default {
+	name: 'Bullet',
+	props: {
+		name: {
+			type: String,
+			required: true,
+		},
+	},
+}
+</script>
+<style lang="scss" scoped>
+.bullet {
+	&__wrapper {
+		display: inline-block;
+		vertical-align: middle;
+		min-width: 0;
+		max-width: 100%;
+	}
+	&__name {
+		border-radius: 10px;
+		display: inline-flex;
+		max-width: 100%;
+		background-color: var(--color-background-dark);
+		padding-right: 8px;
+		padding-left: 8px;
+	}
+}
+</style>
