2ce9ca833f264c62bc8bfcd84bb98f11c3339c85
Vitor Mattos <vitor@php.rio>
2023-10-24 20:43:53 -0300

    Format request date
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Components/File/AppFilesTab.vue
+++ src/Components/File/AppFilesTab.vue
@@ -1,6 +1,5 @@
 <template>
 	<div>
-		<h2>{{ name }}</h2>
 		<h3 v-if="subTitle">
 			{{ subTitle }}
 		</h3>
@@ -27,16 +26,17 @@ export default {
 			signers: [],
 			name: '',
 			requestedBy: {},
+			requestDate: '',
 		}
 	},
 	computed: {
 		subTitle() {
-			if ((this.file?.requested_by?.uid ?? '').length === 0) {
+			if ((this.requestedBy?.uid ?? '').length === 0 || this.requestDate.length === 0) {
 				return ''
 			}
 			return t('libresign', 'Requested by {name}, at {date}', {
-				name: this.file.requested_by.uid,
-				date: Moment(Date.parse(this.file.request_date)).format('LL LTS'),
+				name: this.requestedBy.uid,
+				date: Moment(parseInt(this.requestDate)).format('LL LTS'),
 			})
 		},
 	},
@@ -52,12 +52,14 @@ export default {
 			}
 			this.name = fileInfo.name
 			this.requestedBy = {}
+			this.requestDate = ''
 			try {
 				const response = await axios.get(generateOcsUrl(`/apps/libresign/api/v1/file/validate/file_id/${fileInfo.id}`))
 				this.signers = response.data.signers
 				this.name = response.data.name
 				this.file.uuid = response.data.uuid
 				this.requestedBy = response.data.requested_by
+				this.requestDate = response.data.request_date
 			} catch (e) {
 			}
 		},
