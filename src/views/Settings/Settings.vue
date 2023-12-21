8aa66795af5b6e5de897e0902f19467fd379b3b3
Vitor Mattos <vitor@php.rio>
2023-12-21 18:22:50 -0300

    Administration settings to handle expiration rules
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -29,6 +29,7 @@
 		<RootCertificateCfssl />
 		<RootCertificateOpenSsl />
 		<IdentifierFactor />
+		<ExpirationRules />
 		<Validation />
 		<AllowedGroups />
 		<LegalInformation />
@@ -45,6 +46,7 @@ import DownloadBinaries from './DownloadBinaries.vue'
 import ConfigureCheck from './ConfigureCheck.vue'
 import RootCertificateCfssl from './RootCertificateCfssl.vue'
 import RootCertificateOpenSsl from './RootCertificateOpenSsl.vue'
+import ExpirationRules from './ExpirationRules.vue'
 import Validation from './Validation.vue'
 import AllowedGroups from './AllowedGroups.vue'
 import LegalInformation from './LegalInformation.vue'
@@ -63,6 +65,7 @@ export default {
 		RootCertificateCfssl,
 		RootCertificateOpenSsl,
 		IdentifierFactor,
+		ExpirationRules,
 		Validation,
 		AllowedGroups,
 		LegalInformation,
