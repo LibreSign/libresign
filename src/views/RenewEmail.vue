e4de5fd643d7491b08543038f5f7e3346636b9c8
Vitor Mattos <vitor@php.rio>
2023-12-30 19:17:37 -0300

    Add empty template to Renew Email
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/views/RenewEmail.vue
@@ -0,0 +1,18 @@
+<template>
+	<NcContent />
+</template>
+
+<script>
+import NcContent from '@nextcloud/vue/dist/Components/NcContent.js'
+
+export default {
+	name: 'RenewEmail',
+
+	components: {
+		NcContent,
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
