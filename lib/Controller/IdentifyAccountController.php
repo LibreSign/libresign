079b3b6fdfa3ad77f3724a63e2884f0c7674f707
Vitor Mattos <vitor@php.rio>
2024-01-06 17:52:16 -0300

    Respect the admin setting to allow create account
    When the setting "allow create account" is true, need to be possible
    fill an email to request to sign and create account with this email
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Controller/IdentifyAccountController.php
+++ lib/Controller/IdentifyAccountController.php
@@ -25,6 +25,7 @@ declare(strict_types=1);
 namespace OCA\Libresign\Controller;
 
 use OCA\Libresign\AppInfo\Application;
+use OCA\Libresign\Service\IdentifyMethod\Account;
 use OCP\AppFramework\Http\Attribute\NoAdminRequired;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\Collaboration\Collaborators\ISearch;
@@ -34,20 +35,22 @@ use OCP\IUserSession;
 use OCP\Share\IShare;
 
 class IdentifyAccountController extends AEnvironmentAwareController {
+	private array $shareTypes = [
+		IShare::TYPE_USER,
+	];
 	public function __construct(
 		IRequest $request,
 		private ISearch $collaboratorSearch,
 		private IUserSession $userSession,
 		private IURLGenerator $urlGenerator,
+		private Account $identifyAccountMethod,
 	) {
 		parent::__construct(Application::APP_ID, $request);
 	}
 
 	#[NoAdminRequired]
 	public function search(string $search = '', int $page = 1, int $limit = 25): DataResponse {
-		$shareTypes = [
-			IShare::TYPE_USER,
-		];
+		$shareTypes = $this->getShareTypes();
 		$lookup = false;
 
 		// only search for string larger than a given threshold
@@ -64,17 +67,41 @@ class IdentifyAccountController extends AEnvironmentAwareController {
 		return new DataResponse($return);
 	}
 
+	private function getShareTypes(): array {
+		if (count($this->shareTypes) > 1) {
+			return $this->shareTypes;
+		}
+		$settings = $this->identifyAccountMethod->getSettings();
+		if ($settings['can_create_account']) {
+			$this->shareTypes[] = IShare::TYPE_EMAIL;
+		}
+		return $this->shareTypes;
+	}
+
 	private function formatForNcSelect(array $list): array {
-		$list = array_merge($list['exact']['users'], $list['users']);
-		foreach ($list as $key => $item) {
-			$list[$key] = [
+		$return = [];
+		foreach ($list['exact'] as $item) {
+			$return = array_merge($return, $item);
+		}
+		unset($list['exact']);
+		foreach ($list as $item) {
+			$return = array_merge($return, $item);
+		}
+
+		foreach ($return as $key => $item) {
+			$return[$key] = [
 				'id' => $item['value']['shareWith'],
-				'isNoUser' => false,
+				'isNoUser' => $item['value']['shareType'] !== IShare::TYPE_USER ?? false,
 				'displayName' => $item['label'],
 				'subname' => $item['shareWithDisplayNameUnique'],
 			];
+			if ($item['value']['shareType'] === IShare::TYPE_EMAIL) {
+				$return[$key]['icon'] = 'icon-mail';
+			} elseif ($item['value']['shareType'] === IShare::TYPE_USER) {
+				$return[$key]['icon'] = 'icon-user';
+			}
 		}
-		return $list;
+		return $return;
 	}
 
 	private function addHerself(array $return, string $search): array {
@@ -91,6 +118,8 @@ class IdentifyAccountController extends AEnvironmentAwareController {
 			'isNoUser' => false,
 			'displayName' => $user->getDisplayName(),
 			'subname' => $user->getEMailAddress(),
+			'type' => IShare::TYPE_USER,
+			'icon' => 'icon-user',
 		];
 		return $return;
 	}
