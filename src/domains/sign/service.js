80819c25f4baec2deda65e803143b2dd9d2c8b92
Enieber Cunha <7907068+enieber@users.noreply.github.com>
2024-01-17 00:13:42 -0300

    Display PDF document with lib (#1996)
    Signed-off-by: Enieber Cunha <eniebercunha@gmail.com>
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    Co-authored-by: Vitor Mattos <vitor@php.rio>

--- src/domains/sign/service.js
+++ src/domains/sign/service.js
@@ -11,17 +11,6 @@ import {
  */
 const buildService = (http) => {
 	return ({
-
-		/**
-		 * @param   {string}  uuid uuid
-		 *
-		 * @return  {*}
-		 */
-		async validateByUUID(uuid) {
-			const { data } = await http.get(generateOcsUrl(`/apps/libresign/api/v1/file/validate/uuid/${uuid}`))
-
-			return data
-		},
 		async signDocument({ fileId, password, elements, code }) {
 			const url = String(fileId).length >= 10
 				? generateOcsUrl(`/apps/libresign/api/v1/sign/uuid/${fileId}`)
@@ -36,68 +25,6 @@ const buildService = (http) => {
 			const { data } = await http.post(url, payload)
 
 			return data
-		},
-		/**
-		 * @param   {string}  fileID fileID
-		 * @param   {string}  email email
-		 *
-		 * @return  {*}
-		 */
-		async notifySigner(fileID, email) {
-			const body = {
-				fileId: fileID,
-				signers: [
-					{
-						email,
-					},
-				],
-			}
-
-			const { data } = await http.post(generateOcsUrl('/apps/libresign/api/v1/notify/signers'), body)
-
-			return data
-		},
-		/**
-		 * @param   {string}  fileID fileID
-		 * @param   {string}  signerId signerId
-		 *
-		 * @return  {*}
-		 */
-		async removeSigner(fileID, signerId) {
-			const { data } = await http.delete(generateOcsUrl(`/apps/libresign/api/v1/sign/file_id/${fileID}/${signerId}`))
-
-			return data
-		},
-		/**
-		 * update sign document register
-		 *
-		 * @param   {string}  fileId fileId
-		 * @param   {Record<string, unknown>}  content content
-		 *
-		 * @return  {Promise<unknown>}
-		 */
-		async updateRegister(fileId, content = {}) {
-			const url = generateOcsUrl('/apps/libresign/api/v1/request-signature')
-
-			const body = {
-				file: { fileId },
-				...content,
-			}
-
-			const { data } = await http.patch(url, body)
-
-			return data
-		},
-		/**
-		 * change document sign status
-		 *
-		 * @param   {string}  fileId fileId
-		 * @param   {number}  status  new status
-		 *
-		 * @return  {Promise<unknown>}
-		 */
-		changeRegisterStatus(fileId, status) {
-			return this.updateRegister(fileId, { status })
 		},
 		/**
 		 * request sign code
