20d4ff62acb19a31a0eaf5598c2925902c499dd2
Vitor Mattos <vitor@php.rio>
2024-01-17 09:36:48 -0300

    Display PDF document with lib (#1996) (#2104)
    Signed-off-by: Enieber Cunha <eniebercunha@gmail.com>
    Signed-off-by: Vitor Mattos <vitor@php.rio>
    Co-authored-by: Enieber Cunha <7907068+enieber@users.noreply.github.com>

---
+++ src/Components/PdfEditor/PdfEditor.vue
@@ -0,0 +1,73 @@
+<template>
+	<VuePdfEditor ref="vuePdfEditor"
+		width="100%"
+		height="100%"
+		:show-choose-file-btn="false"
+		:show-customize-editor="false"
+		:show-line-size-select="false"
+		:show-font-size-select="false"
+		:show-font-select="false"
+		:show-save-btn="false"
+		:save-to-upload="false"
+		:init-file-src="fileSrc"
+		:init-image-scale="1"
+		:seal-image-show="false"
+		@pdf-editor:end-init="endInit">
+		<template #custom="{ object, pagesScale }">
+			<Signature :x="object.x"
+				:y="object.y"
+				:fix-size="false"
+				:display-name="object.signer.displayName"
+				:width="object.width"
+				:height="object.height"
+				:origin-width="object.originWidth"
+				:origin-height="object.originHeight"
+				:page-scale="pagesScale"
+				@onUpdate="$refs.vuePdfEditor.updateObject(object.id, $event)"
+				@onDelete="onDeleteSigner(object)" />
+		</template>
+	</VuePdfEditor>
+</template>
+
+<script>
+import VuePdfEditor from '@libresign/vue-pdf-editor'
+import Signature from './Signature.vue'
+
+export default {
+	name: 'PdfEditor',
+	components: {
+		VuePdfEditor,
+		Signature,
+	},
+	props: {
+		fileSrc: {
+			type: String,
+			default: '',
+			require: true,
+		},
+	},
+	methods: {
+		endInit(event) {
+			this.$emit('pdf-editor:end-init', { ...event })
+		},
+		onDeleteSigner(object) {
+			this.$emit('pdf-editor:on-delete-signer', object)
+			this.$refs.vuePdfEditor.deleteObject(object.id)
+		},
+		addSigner(signer) {
+			const object = {
+				id: this.$refs.vuePdfEditor.genID(),
+				type: 'custom',
+				signer,
+				width: signer.element.coordinates.width,
+				height: signer.element.coordinates.height,
+				originWidth: signer.element.coordinates.width,
+				originHeight: signer.element.coordinates.height,
+				x: signer.element.coordinates.llx,
+				y: signer.element.coordinates.ury,
+			}
+			this.$refs.vuePdfEditor.addObject(object)
+		},
+	},
+}
+</script>
