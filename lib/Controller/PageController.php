66e428fffd7cb55f1341cd11539c26a51bf754d7
Vitor Mattos <vitor@php.rio>
2024-01-20 17:22:24 -0300

    Send signature_method to frontend as initial state
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -36,6 +36,7 @@ use OCA\Libresign\Service\FileService;
 use OCA\Libresign\Service\IdentifyMethodService;
 use OCA\Libresign\Service\RequestSignatureService;
 use OCA\Libresign\Service\SignFileService;
+use OCA\Libresign\Service\SignMethodService;
 use OCP\AppFramework\Db\DoesNotExistException;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\Attribute\AnonRateLimit;
@@ -65,6 +66,7 @@ class PageController extends AEnvironmentPageAwareController {
 		protected RequestSignatureService $requestSignatureService,
 		protected IL10N $l10n,
 		private IdentifyMethodService $identifyMethodService,
+		private SignMethodService $signMethodService,
 		private IAppConfig $appConfig,
 		private FileService $fileService,
 		private ValidateHelper $validateHelper,
@@ -97,6 +99,7 @@ class PageController extends AEnvironmentPageAwareController {
 
 		$this->initialState->provideInitialState('file_info', $this->fileService->formatFile());
 		$this->initialState->provideInitialState('identify_methods', $this->identifyMethodService->getIdentifyMethodsSettings());
+		$this->initialState->provideInitialState('signature_method', $this->signMethodService->getCurrent());
 		$this->initialState->provideInitialState('legal_information', $this->appConfig->getAppValue('legal_information'));
 
 		Util::addScript(Application::APP_ID, 'libresign-main');
