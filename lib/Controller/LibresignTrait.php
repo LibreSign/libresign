3c81d0053637f678b7f8ec7cedca90cd1cea3caa
Vitor Mattos <vitor@php.rio>
2024-01-22 21:26:52 -0300

    Improvements to validate UUID
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Controller/LibresignTrait.php
+++ lib/Controller/LibresignTrait.php
@@ -68,6 +68,7 @@ trait LibresignTrait {
 	 * @throws LibresignException
 	 */
 	public function validateSignRequestUuid(string $uuid): void {
+		$this->validateUuidFormat($uuid);
 		$this->loadEntitiesFromUuid($uuid);
 		$this->signFileService->validateSigner($uuid, $this->userSession->getUser());
 		$this->nextcloudFile = $this->signFileService->getNextcloudFile(
@@ -78,7 +79,20 @@ trait LibresignTrait {
 	/**
 	 * @throws LibresignException
 	 */
+	private function validateUuidFormat(string $uuid): void {
+		if (!$uuid || !preg_match('/^[a-f\d]{8}(-[a-f\d]{4}){4}[a-f\d]{8}$/i', $uuid)) {
+			throw new LibresignException(json_encode([
+				'action' => JSActions::ACTION_DO_NOTHING,
+				'errors' => [$this->l10n->t('Invalid UUID')],
+			]), AppFrameworkHttp::STATUS_NOT_FOUND);
+		}
+	}
+
+	/**
+	 * @throws LibresignException
+	 */
 	public function validateRenewSigner(string $uuid): void {
+		$this->validateUuidFormat($uuid);
 		$this->loadEntitiesFromUuid($uuid);
 		$this->signFileService->validateRenewSigner($uuid, $this->userSession->getUser());
 		$this->nextcloudFile = $this->signFileService->getNextcloudFile(
@@ -90,6 +104,7 @@ trait LibresignTrait {
 	 * @throws LibresignException
 	 */
 	public function loadNextcloudFileFromSignRequestUuid(string $uuid): void {
+		$this->validateUuidFormat($uuid);
 		$this->loadEntitiesFromUuid($uuid);
 		$this->nextcloudFile = $this->signFileService->getNextcloudFile(
 			$this->fileEntity->getNodeId(),
