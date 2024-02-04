5ff6699094ec97969eef66f680a0b78b475d47e9
Vitor Mattos <vitor@php.rio>
2024-02-03 22:25:34 -0300

    Remove unused code and prevent error when unautenticated user
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Controller/PageController.php
+++ lib/Controller/PageController.php
@@ -376,14 +376,13 @@ class PageController extends AEnvironmentPageAwareController {
 	public function validationFile(string $uuid): TemplateResponse {
 		$this->throwIfValidationPageNotAccessible();
 		try {
-			$fileEntity = $this->signFileService->getFileByUuid($uuid);
-			$this->signFileService->getAccountFileById($fileEntity->getId());
+			$this->signFileService->getFileByUuid($uuid);
 		} catch (DoesNotExistException $e) {
 			$this->initialState->provideInitialState('action', JSActions::ACTION_DO_NOTHING);
 			$this->initialState->provideInitialState('errors', [$this->l10n->t('Invalid UUID')]);
 		}
 		$this->initialState->provideInitialState('config',
-			$this->accountService->getConfig($this->userSession->getUser())
+			$this->accountService->getConfig($this->userSession?->getUser())
 		);
 
 		$this->initialState->provideInitialState('legal_information', $this->appConfig->getAppValue('legal_information'));
