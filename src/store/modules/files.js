8bc7e6f9a0db39b04fdcacfe17db71463c994deb
Vitor Mattos <vitor@php.rio>
2024-02-07 22:40:22 -0300

    Remove unsecessary methods from store
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/store/modules/files.js
+++ src/store/modules/files.js
@@ -1,7 +1,3 @@
-import axios from '@nextcloud/axios'
-import { showError } from '@nextcloud/dialogs'
-import { generateOcsUrl } from '@nextcloud/router'
-
 const state = {
 	file: {},
 	files: [],
@@ -24,24 +20,8 @@ const mutations = {
 }
 
 const actions = {
-	FETCH_FILE_TO_SIGN: async () => {
-	},
-	SET_FILE: ({ commit }, file) => {
-		commit('setFile', file)
-	},
-	SET_FILES: ({ commit }, files) => {
-		commit('setFiles', files)
-	},
 	SET_FILE_TO_SIGN: ({ commit }, data) => {
 		commit('setFileToSign', data)
-	},
-	GET_ALL_FILES: async ({ dispatch }) => {
-		try {
-			const response = await axios.get(generateOcsUrl('/apps/libresign/api/v1/file/list'))
-			dispatch('SET_FILES', response.data.data)
-		} catch (err) {
-			showError('An error occurred while fetching the files')
-		}
 	},
 }
 
