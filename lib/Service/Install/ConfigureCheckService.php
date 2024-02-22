424f9f4bb104942580e4473238df3340aa14fdfc
Vitor Mattos <vitor@php.rio>
2024-02-22 01:34:21 -0300

    Start to implement jeidison/pdf-signe
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/Install/ConfigureCheckService.php
+++ lib/Service/Install/ConfigureCheckService.php
@@ -70,6 +70,10 @@ class ConfigureCheckService {
 	 * @return ConfigureCheckHelper[]
 	 */
 	public function checkJSignPdf(): array {
+		$signatureEngine = $this->appConfig->getAppValue('signature_engine', 'jsignpdf');
+		if ($signatureEngine !== 'jsignpdf') {
+			return [];
+		}
 		$jsignpdJarPath = $this->appConfig->getAppValue('jsignpdf_jar_path');
 		if ($jsignpdJarPath) {
 			if (file_exists($jsignpdJarPath)) {
@@ -182,6 +186,10 @@ class ConfigureCheckService {
 	 * @return ConfigureCheckHelper[]
 	 */
 	private function checkJava(): array {
+		$signatureEngine = $this->appConfig->getAppValue('signature_engine', 'jsignpdf');
+		if ($signatureEngine !== 'jsignpdf') {
+			return [];
+		}
 		$javaPath = $this->appConfig->getAppValue('java_path');
 		if ($javaPath) {
 			if (file_exists($javaPath)) {
