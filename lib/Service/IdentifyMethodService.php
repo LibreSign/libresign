5be7265d5b5d5d868a07c0a33ce4470a77add529
Vitor Mattos <vitor@php.rio>
2024-03-25 16:22:00 -0300

    Fix CI
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/IdentifyMethodService.php
+++ lib/Service/IdentifyMethodService.php
@@ -193,6 +193,9 @@ class IdentifyMethodService {
 				}
 				$signatureMethods = $identifyMethod->getSignatureMethods();
 				foreach ($signatureMethods as $signatureMethod) {
+					if (!$signatureMethod->isEnabled()) {
+						continue;
+					}
 					$signatureMethod->setEntity($identifyMethod->getEntity());
 					$return[$signatureMethod->getName()] = $signatureMethod->toArray();
 				}
