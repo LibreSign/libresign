5edc046b263614b1073526a6213ddb786633d4cb
Vitor Mattos <vitor@php.rio>
2024-04-05 11:48:14 -0300

    chore: Change metadata field to be a json and not a text
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ lib/Migration/Version8000Date20240405142042.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @copyright Copyright (c) 2024 Vitor Mattos <vitor@php.rio>
+ *
+ * @author Vitor Mattos <vitor@php.rio>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\Libresign\Migration;
+
+use Closure;
+use Doctrine\DBAL\Types\JsonType;
+use OCP\DB\ISchemaWrapper;
+use OCP\Migration\IOutput;
+use OCP\Migration\SimpleMigrationStep;
+
+/**
+ * FIXME Auto-generated migration step: Please modify to your needs!
+ */
+class Version8000Date20240405142042 extends SimpleMigrationStep {
+	/**
+	 * @param IOutput $output
+	 * @param Closure(): ISchemaWrapper $schemaClosure
+	 * @param array $options
+	 * @return null|ISchemaWrapper
+	 */
+	public function changeSchema(IOutput $output, Closure $schemaClosure, array $options): ?ISchemaWrapper {
+		/** @var ISchemaWrapper $schema */
+		$schema = $schemaClosure();
+		$table = $schema->getTable('libresign_file');
+
+		if ($table->hasColumn('metadata')) {
+			$options = $table->getColumn('metadata');
+			if (!$options->getType() instanceof JsonType) {
+				$table->modifyColumn('metadata', [
+					'Type' => new JsonType(),
+				]);
+				return $schema;
+			}
+		}
+
+		return null;
+	}
+}
