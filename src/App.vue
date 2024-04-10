b1cad60101d6ae3f285877afbc87d5858f24a9ff
Vitor Mattos <vitor@php.rio>
2024-04-10 19:21:25 -0300

    feat: Reduce usage of iframe
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/App.vue
+++ src/App.vue
@@ -64,7 +64,7 @@ export default {
 			return this.$route.path === '/'
 		},
 		isSignExternalPage() {
-			return this.$route.name === 'SignPDF'
+			return this.$route.name === 'SignPDFExternal'
 				|| this.$route.name === 'DefaultPageError'
 		},
 	},
