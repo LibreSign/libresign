257f3e88326a77ac7f8f5bd0f25375c965f7f83f
Vitor Mattos <vitor@php.rio>
2024-05-30 14:29:28 -0300

    fix: cast string to int
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/IdentifyMethod/AbstractIdentifyMethod.php
+++ lib/Service/IdentifyMethod/AbstractIdentifyMethod.php
@@ -207,7 +207,7 @@ abstract class AbstractIdentifyMethod implements IIdentifyMethod {
 		$signRequest = $this->identifyService->getSignRequestMapper()->getById($this->getEntity()->getSignRequestId());
 		$startTime = $this->identifyService->getSessionService()->getSignStartTime();
 		$createdAt = $signRequest->getCreatedAt();
-		$lastAttempt = $this->getEntity()->getLastAttemptDate()?->format('U');
+		$lastAttempt = (int) $this->getEntity()->getLastAttemptDate()?->format('U');
 		$lastActionDate = max(
 			$startTime,
 			$createdAt,
