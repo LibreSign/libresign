90dc96480f7900a388d5b5b214e9b60876c541d6
Vitor Mattos <vitor@php.rio>
2024-05-30 20:13:05 -0300

    fix: return type
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -2273,6 +2273,7 @@ export type operations = {
                 /** Format: int64 */
                 action: number;
                 errors: string[];
+                redirect?: string;
               };
             };
           };
