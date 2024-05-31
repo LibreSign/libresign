5c1a5e6d447032af7bb6a3563a8590207c0c704b
Vitor Mattos <vitor@php.rio>
2024-05-31 04:49:33 -0300

    chore: update documentation
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Controller/DevelopController.php
+++ lib/Controller/DevelopController.php
@@ -31,6 +31,10 @@ class DevelopController extends Controller {
 	/**
 	 * Get a demo PDF file to be used by test purpose
 	 *
+	 * To use this endpoint is necessary to enable the debug mode in your instance. To do this, run the command:
+	 *
+	 * `occ config:system:set debug --value true --type boolean`
+	 *
 	 * @return FileDisplayResponse<Http::STATUS_OK, array{Content-Type: 'application/pdf',Content-Disposition: 'inline; filename="file.pdf"'}>|DataResponse<Http::STATUS_NOT_FOUND, array<empty>, array{}>
 	 *
 	 * 200: PDF returned
