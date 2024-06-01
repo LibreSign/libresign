1df009b1d02d9c40947691a378aed15b5ad30188
Vitor Mattos <vitor@php.rio>
2024-06-01 16:37:52 -0300

    chore: start to implement unit tests
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- tests/Unit/Service/Install/SignFilesTest.php
+++ tests/Unit/Service/Install/SignFilesTest.php
@@ -8,11 +8,14 @@ declare(strict_types=1);
 
 namespace OCA\Libresign\Tests\Unit\Service;
 
+use bovigo\vfs\vfsStream;
 use OC\IntegrityCheck\Helpers\FileAccessHelper;
 use OCA\Libresign\Service\Install\SignFiles;
 use OCP\App\IAppManager;
 use OCP\Files\AppData\IAppDataFactory;
 use OCP\IConfig;
+use phpseclib\Crypt\RSA;
+use phpseclib\File\X509;
 use PHPUnit\Framework\MockObject\MockObject;
 
 final class SignFilesTest extends \OCA\Libresign\Tests\Unit\TestCase {
@@ -43,6 +46,28 @@ final class SignFilesTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			->getMock();
 	}
 
+	private function getNewCert(): array {
+		$privateKey = openssl_pkey_new([
+			'private_key_bits' => 2048,
+			'private_key_type' => OPENSSL_KEYTYPE_RSA,
+		]);
+
+		$csrNames = ['commonName' => 'Jhon Doe'];
+
+		$csr = openssl_csr_new($csrNames, $privateKey, ['digest_alg' => 'sha256']);
+		$x509 = openssl_csr_sign($csr, null, $privateKey, $days = 365, ['digest_alg' => 'sha256']);
+
+		openssl_x509_export($x509, $rootCertificate);
+		openssl_pkey_export($privateKey, $rootPrivateKey);
+
+		$privateKey = openssl_pkey_new([
+			'private_key_bits' => 2048,
+			'private_key_type' => OPENSSL_KEYTYPE_RSA,
+		]);
+		return [
+			'privateKey' => $privateKey,
+			'certificate' => $rootCertificate,
+		];
 	}
 
 	/**
@@ -67,6 +92,40 @@ final class SignFilesTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			[['dependencies' => ['architecture' => []]], true, []],
 			[['dependencies' => ['architecture' => ['x86_64']]], false, ['x86_64']],
 			[['dependencies' => ['architecture' => ['x86_64', 'aarch64']]], false, ['x86_64', 'aarch64']],
+		];
+	}
+
+	/**
+	 * @dataProvider dataWriteAppSignature
+	 */
+	public function testWriteAppSignature(string $architecture): void {
+		$this->appManager->method('getAppInfo')
+			->willReturn(['dependencies' => ['architecture' => [$architecture]]]);
+
+		$certificate = $this->getNewCert('123456');
+		$rsa = new RSA();
+		$rsa->loadKey($certificate['privateKey']);
+		$x509 = new X509();
+		$x509->loadX509($certificate['certificate']);
+		$x509->setPrivateKey($rsa);
+
+
+		vfsStream::setup('home');
+
+		$this->config->method('getSystemValue')
+			->willReturn(vfsStream::url('home/data'));
+
+		$signFiles = $this->getInstance(['getInternalPathOfFolder']);
+		$signFiles->expects($this->any())
+			->method('getInternalPathOfFolder')
+			->willReturn('libresign');
+		mkdir('vfs://home/data/libresign', 0755, true);
+		$signFiles->writeAppSignature($x509, $rsa, $architecture);
+	}
+
+	public static function dataWriteAppSignature(): array {
+		return [
+			['x86_64'],
 		];
 	}
 }
