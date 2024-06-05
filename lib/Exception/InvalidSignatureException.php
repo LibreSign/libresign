1cabde4fa6b31dca1c922fa8e9e54c9c2d17929b
Vitor Mattos <vitor@php.rio>
2024-06-05 17:05:42 -0300

    fix: SPDX
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Exception/InvalidSignatureException.php
+++ lib/Exception/InvalidSignatureException.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 /**
- * SPDX-FileCopyrightText: 2020-2024 LibreCode coop and contributors
+ * SPDX-FileCopyrightText: 2024 LibreCode coop and contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
