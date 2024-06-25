3f887a026a673e8745a22ec0e0130bacec9e646a
Vitor Mattos <vitor@php.rio>
2024-06-25 13:05:47 -0300

    fix: neutralize deleted users
    Prevent to show files of deleted accounts when is created a new account
    with the same user id of deleted account.
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ lib/BackgroundJob/UserDeleted.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2024-2024 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Libresign\BackgroundJob;
+
+use OCA\Libresign\Db\FileMapper;
+use OCA\Libresign\Db\IdentifyMethodMapper;
+use OCA\Libresign\Db\UserElementMapper;
+use OCP\AppFramework\Utility\ITimeFactory;
+use OCP\BackgroundJob\QueuedJob;
+
+use Psr\Log\LoggerInterface;
+
+class UserDeleted extends QueuedJob {
+	public function __construct(
+		protected FileMapper $fileMapper,
+		protected IdentifyMethodMapper $identifyMethodMapper,
+		protected UserElementMapper $userElementMapper,
+		protected ITimeFactory $time,
+		protected LoggerInterface $logger
+	) {
+		parent::__construct($time);
+	}
+
+	/**
+	 * @param array $argument
+	 */
+	public function run($argument): void {
+		$userId = $argument['user_id'];
+		$displayName = $argument['display_name'];
+		$this->logger->info('Neutralizing data for deleted user {user}', [
+			'user' => $userId
+		]);
+		$this->fileMapper->neutralizeDeletedUser($userId, $displayName);
+		$this->identifyMethodMapper->neutralizeDeletedUser($userId, $displayName);
+		$this->userElementMapper->neutralizeDeletedUser($userId, $displayName);
+	}
+}
