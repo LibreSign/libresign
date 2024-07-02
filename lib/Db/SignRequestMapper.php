882a0f5196eebf2ba5e04f636aaea62193eb79e2
Vitor Mattos <vitor@php.rio>
2024-07-02 14:00:50 -0300

    chore: implement limit and ofset
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Db/SignRequestMapper.php
+++ lib/Db/SignRequestMapper.php
@@ -348,6 +348,8 @@ class SignRequestMapper extends QBMapper {
 		array $filter,
 	): array {
 		$filter['email'] = $user->getEMailAddress();
+		$filter['length'] = $length;
+		$filter['page'] = $page;
 		$pagination = $this->getFilesAssociatedFilesWithMeStmt($user->getUID(), $filter);
 		$pagination->setMaxPerPage($length);
 		$pagination->setCurrentPage($page);
@@ -484,6 +486,10 @@ class SignRequestMapper extends QBMapper {
 				$qb->andWhere(
 					$qb->expr()->eq('f.node_id', $qb->createNamedParameter($filter['nodeId'], IQueryBuilder::PARAM_INT))
 				);
+			}
+			if (isset($filter['length']) && isset($filter['page'])) {
+				$qb->setFirstResult($filter['length'] * ($filter['page'] - 1));
+				$qb->setMaxResults($filter['length']);
 			}
 		}
 		return $qb;
