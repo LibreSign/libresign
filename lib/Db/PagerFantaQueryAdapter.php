20867e7a73568a6f129aeaccaa7b2f8bb923e729
Vitor Mattos <vitor@php.rio>
2024-07-02 17:09:33 -0300

    fix: pending changes after backport
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Db/PagerFantaQueryAdapter.php
+++ lib/Db/PagerFantaQueryAdapter.php
@@ -2,24 +2,8 @@
 
 declare(strict_types=1);
 /**
- * @copyright Copyright (c) 2023 Vitor Mattos <vitor@php.rio>
- *
- * @author Vitor Mattos <vitor@php.rio>
- *
- * @license GNU AGPL version 3 or any later version
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-FileCopyrightText: 2020-2024 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 namespace OCA\Libresign\Db;
@@ -34,24 +18,17 @@ use ReflectionClass;
  * Adapter which calculates pagination from a Doctrine DBAL QueryBuilder.
  */
 class PagerFantaQueryAdapter implements AdapterInterface {
-	private IQueryBuilder $queryBuilder;
-
-	private $countQueryBuilderModifier;
-
 	/**
-	 * @phpstan-param callable(QueryBuilder): void $countQueryBuilderModifier
-	 *
 	 * @throws InvalidArgumentException if a non-SELECT query is given
 	 */
-	public function __construct(IQueryBuilder $queryBuilder, callable $countQueryBuilderModifier) {
+	public function __construct(
+		private IQueryBuilder $queryBuilder,
+	) {
 		if (QueryBuilder::SELECT !== $queryBuilder->getType()) {
 			// @codeCoverageIgnoreStart
 			throw new InvalidArgumentException('Only SELECT queries can be paginated.');
 			// @codeCoverageIgnoreEnd
 		}
-
-		$this->queryBuilder = clone $queryBuilder;
-		$this->countQueryBuilderModifier = $countQueryBuilderModifier;
 	}
 
 	public function getNbResults(): int {
@@ -67,12 +44,16 @@ class PagerFantaQueryAdapter implements AdapterInterface {
 		$qb = $reflectionProperty->getValue($this->queryBuilder);
 		$originalQueryBuilder = clone $qb;
 
-		$callable = $this->countQueryBuilderModifier;
-		$total = $callable($this->queryBuilder);
+		$this->queryBuilder->resetQueryPart('select')
+			->resetQueryPart('groupBy')
+			->select($this->queryBuilder->func()->count())
+			->setFirstResult(0)
+			->setMaxResults(null);
+		$total = $this->queryBuilder->executeQuery()->fetchOne();
 
 		$reflectionProperty->setValue($this->queryBuilder, $originalQueryBuilder);
 
-		return $total;
+		return abs((int) $total);
 	}
 
 	/**
