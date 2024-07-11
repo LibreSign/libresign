2e94ee25ff965f52b7ccd8865ca3f23b978b28dc
Vitor Mattos <vitor@php.rio>
2024-07-11 01:11:24 -0300

    chore: apply cs fixer
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once './vendor/autoload.php';
+require_once './vendor-bin/coding-standard/vendor/autoload.php';
 
 use Nextcloud\CodingStandard\Config;
 use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
