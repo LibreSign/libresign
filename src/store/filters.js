044e86c8de682fc29a539072b30909c283d35ce9
Vitor Mattos <vitor@php.rio>
2024-09-20 18:41:00 -0300

    feat: rewrite file list
    Signed-off-by: Vitor Mattos <vitor@php.rio>

---
+++ src/store/filters.js
@@ -0,0 +1,27 @@
+/**
+ * SPDX-FileCopyrightText: 2020-2024 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+import { defineStore } from 'pinia'
+import logger from '../helpers/logger.js'
+
+export const useFiltersStore = defineStore('filter', {
+	state: () => ({
+		chips: {},
+	}),
+
+	getters: {
+		activeChips(state) {
+			return Object.values(state.chips).flat()
+		},
+	},
+
+	actions: {
+		onFilterUpdateChips(event) {
+			this.chips = { ...this.chips, [event.id]: [...event.detail] }
+
+			logger.debug('File list filter chips updated', { chips: event.detail })
+		},
+	},
+})
