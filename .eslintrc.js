932276f0daac9c1127cdaa8f7d04ef688dc3537b
Vitor Mattos <vitor@php.rio>
2024-09-23 14:34:21 -0300

    chore: convert indent size
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,13 @@
 module.exports = {
-  globals: {
-    appName: true,
-  },
-  extends: [
-    '@nextcloud',
-  ],
-  rules: {
-    // production only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-  }
+	globals: {
+		appName: true,
+	},
+	extends: [
+		'@nextcloud',
+	],
+	rules: {
+		// production only
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+		'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+	}
 }
