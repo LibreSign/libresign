f32e23fcf234a7ae0d97025351f7a80f04ac9f1c
Vitor Mattos <vitor@php.rio>
2024-09-23 19:11:23 -0300

    chore: start to implement FileEntry component
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/store/filters.js
+++ src/store/filters.js
@@ -1,9 +1,10 @@
 /**
- * SPDX-FileCopyrightText: 2020-2024 LibreCode coop and contributors
+ * SPDX-FileCopyrightText: 2024 LibreCode coop and contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 import { defineStore } from 'pinia'
+
 import logger from '../helpers/logger.js'
 
 export const useFiltersStore = defineStore('filter', {
