9d4206cf61514f6ec2417820808bc572351966b2
Vitor Mattos <vitor@php.rio>
2024-09-23 14:34:21 -0300

    chore: convert indent size
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  globals: {
-    appName: true,
-  },
-  extends: [
-    '@nextcloud',
-  ],
-  rules: {
-    // production only
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-    'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
-  },
-  overrides: [
-    {
-      files: ['src/types/openapi/*.ts'],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-        quotes: 'off',
-        'no-multiple-empty-lines': 'off',
-        'no-use-before-define': 'off',
-      },
-    },
-  ],
+	globals: {
+		appName: true,
+	},
+	extends: [
+		'@nextcloud',
+	],
+	rules: {
+		// production only
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+		'vue/no-unused-components': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+	},
+	overrides: [
+		{
+			files: ['src/types/openapi/*.ts'],
+			rules: {
+				'@typescript-eslint/no-explicit-any': 'off',
+				quotes: 'off',
+				'no-multiple-empty-lines': 'off',
+				'no-use-before-define': 'off',
+			},
+		},
+	],
 }
