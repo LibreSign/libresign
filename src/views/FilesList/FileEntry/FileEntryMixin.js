f3d906f100c74f1809bf05676233c65c81ac3614
Vitor Mattos <vitor@php.rio>
2024-10-31 13:50:31 -0300

    fix: SPDX
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/FilesList/FileEntry/FileEntryMixin.js
+++ src/views/FilesList/FileEntry/FileEntryMixin.js
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2021 LibreCode coop and LibreCode contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 export default {
 	props: {
 		source: {
--- src/views/FilesList/FileEntry/FileEntryPreview.vue
+++ src/views/FilesList/FileEntry/FileEntryPreview.vue
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 LibreCode coop and LibreCode contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 <template>
 	<span class="files-list__row-icon">
 		<!-- Decorative images, should not be aria documented -->
