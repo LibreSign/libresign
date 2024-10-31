0b4b535b0b1eb09d6637d09a3251c21e6a97fa8b
Vitor Mattos <vitor@php.rio>
2024-10-31 13:50:31 -0300

    fix: SPDX
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/FilesList/FileListFilters.vue
+++ src/views/FilesList/FileListFilters.vue
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2024 LibreCode coop and LibreCode contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 <template>
 	<div class="file-list-filters">
 		<div class="file-list-filters__filter">
