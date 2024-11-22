3c62307de52a4ab36cfa68e39e81f8d392740c36
Vitor Mattos <vitor@php.rio>
2024-11-22 14:19:05 -0300

    fix: show actions at signer list
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/Components/Signers/Signer.vue
+++ src/Components/Signers/Signer.vue
@@ -8,9 +8,7 @@
 		<template #subname>
 			<Bullet v-for="method in identifyMethodsNames" :key="method" :name="method" />
 		</template>
-		<NcActions>
-			<slot v-if="$slots.actions" slot="actions" name="actions" />
-		</NcActions>
+		<slot slot="actions" name="actions" />
 		<template #indicator>
 			<CheckboxBlankCircle :size="16"
 				:fill-color="statusColor"
@@ -25,7 +23,6 @@ import { emit } from '@nextcloud/event-bus'
 import { loadState } from '@nextcloud/initial-state'
 import Moment from '@nextcloud/moment'
 
-import NcActions from '@nextcloud/vue/dist/Components/NcActions.js'
 import NcAvatar from '@nextcloud/vue/dist/Components/NcAvatar.js'
 import NcListItem from '@nextcloud/vue/dist/Components/NcListItem.js'
 
@@ -36,7 +33,6 @@ import { useFilesStore } from '../../store/files.js'
 export default {
 	name: 'Signer',
 	components: {
-		NcActions,
 		NcListItem,
 		NcAvatar,
 		CheckboxBlankCircle,
