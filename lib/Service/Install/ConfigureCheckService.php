c70f82f75c22450db6a611130acea5db49ad1863
Vitor Mattos <vitor@php.rio>
2024-12-19 14:48:29 -0300

    chore: implement logs
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/Install/ConfigureCheckService.php
+++ lib/Service/Install/ConfigureCheckService.php
@@ -30,6 +30,7 @@ use OCA\Libresign\Handler\CertificateEngine\Handler as CertificateEngine;
 use OCA\Libresign\Handler\JSignPdfHandler;
 use OCA\Libresign\Helper\ConfigureCheckHelper;
 use OCP\AppFramework\Services\IAppConfig;
+use Psr\Log\LoggerInterface;
 
 class ConfigureCheckService {
 	private string $architecture;
@@ -41,6 +42,7 @@ class ConfigureCheckService {
 		private JSignPdfHandler $jSignPdfHandler,
 		private CertificateEngine $certificateEngine,
 		private SignSetupService $signSetupService,
+		private LoggerInterface $logger,
 	) {
 		$this->architecture = php_uname('m');
 	}
@@ -259,6 +261,7 @@ class ConfigureCheckService {
 				];
 			}
 		}
+		$this->logger->error('Invalid hash of binaries files', ['result' => $result]);
 		return [
 			'Invalid hash of binaries files.',
 			'Run occ libresign:install --all',
