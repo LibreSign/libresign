84e7038b1cc847479c35047276d393edbc5c6d16
Vitor Mattos <vitor@php.rio>
2024-12-28 18:01:51 -0300

    feat: rewrite validation page
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -1,67 +1,115 @@
 <template>
 	<div class="container">
-		<div class="image">
-			<img :src="image" draggable="false">
+		<div class="logo">
+			<img :src="logo" draggable="false">
 		</div>
 		<div id="dataUUID">
 			<form v-show="!hasInfo" @submit="(e) => e.preventDefault()">
-				<h1>{{ title }}</h1>
-				<NcTextField v-model="myUuid" :label="legend" />
+				<h1>{{ t('libresign', 'Validate signature') }}</h1>
+				<NcTextField v-model="uuidToValidate" :label="t('libresign', 'Enter the ID or UUID of the document to validate.')" />
 				<NcButton type="primary"
-					@click.prevent="clickedValidate = true;validate(myUuid)">
+					:disabled="loading"
+					@click.prevent="clickedValidate = true;validate(uuidToValidate)">
 					<template #icon>
-						<NcLoadingIcon v-if="hasLoading" :size="20" />
+						<NcLoadingIcon v-if="loading" :size="20" />
 					</template>
-					{{ buttonTitle }}
+					{{ t('libresign', 'Validation') }}
 				</NcButton>
 			</form>
 			<div v-if="hasInfo" class="infor-container">
-				<div class="infor-bg">
-					<div class="infor">
-						<div class="header">
-							<img class="icon" :src="infoIcon">
-							<h1>{{ infoDocument }}</h1>
-						</div>
-						<div class="info-document">
-							<NcNoteCard v-if="isAfterSigned" type="success">
-								{{ t('libresign', 'Congratulations you have digitally signed a document using LibreSign') }}
-							</NcNoteCard>
-							<p>
-								<b>{{ document.name }}</b>
-							</p>
-							<NcRichText class="legal-information"
-								:text="legalInformation"
-								:use-markdown="true" />
+				<div class="section">
+					<div class="header">
+						<NcIconSvgWrapper :path="mdiInformationSlabCircle" :size="30" />
+						<h1>{{ t('libresign', 'Document informations') }}</h1>
+					</div>
+					<div class="info-document">
+						<NcNoteCard v-if="isAfterSigned" type="success">
+							{{ t('libresign', 'Congratulations you have digitally signed a document using LibreSign') }}
+						</NcNoteCard>
+						<p>
+							<b>{{ document.name }}</b>
+						</p>
+						<NcRichText class="legal-information"
+							:text="legalInformation"
+							:use-markdown="true" />
 
-							<NcButton type="primary"
-								@click="viewDocument()">
-								<template #icon>
-									<NcLoadingIcon v-if="hasLoading" :size="20" />
-								</template>
-								{{ t('libresign', 'View') }}
-							</NcButton>
-						</div>
+						<NcButton type="primary"
+							@click="viewDocument()">
+							<template #icon>
+								<NcLoadingIcon v-if="loading" :size="20" />
+							</template>
+							{{ t('libresign', 'View') }}
+						</NcButton>
 					</div>
 				</div>
-				<div class="infor-bg signed">
+				<div class="section">
 					<div class="header">
-						<img class="icon" :src="signatureIcon">
+						<NcIconSvgWrapper :path="mdiSignatureFreehand" :size="30" />
 						<h1>{{ t('libresign', 'Signatories:') }}</h1>
 					</div>
-					<div class="infor-content">
-						<div v-for="item in document.signers"
-							id="sign"
-							:key="item.displayName"
-							class="scroll">
-							<div class="subscriber">
-								<span><b>{{ getName(item) }}</b></span>
-								<span v-if="item.signed" class="data-signed">
-									{{ item.signed }}
-								</span>
-								<span v-else>{{ noDateMessage }}</span>
-							</div>
-						</div>
-					</div>
+					<ul>
+						<span v-for="(signer, index) in document.signers"
+							:key="index">
+							<NcListItem :name="getName(signer)"
+								:active="signer.opened"
+								@click="toggleDetail(signer)">
+								<template #icon>
+									<NcAvatar disable-menu
+										:is-no-user="!signer.userId"
+										:size="44"
+										:user="signer.userId ? signer.userId : getName(signer)"
+										:display-name="getName(signer)" />
+								</template>
+								<template #subname>
+									<strong>{{ t('LibreSign', 'Date signed:') }}</strong>
+									<span v-if="signer.signed" class="data-signed">
+										{{ signer.signed }}
+									</span>
+									<span v-else>{{ t('libresign', 'No date') }}</span>
+								</template>
+								<template #extra-actions>
+									<NcButton type="tertiary"
+										@click="toggleDetail(signer)">
+										<template #icon>
+											<NcIconSvgWrapper v-if="signer.opened"
+												:path="mdiUnfoldLessHorizontal"
+												:size="20" />
+											<NcIconSvgWrapper v-else
+												:path="mdiUnfoldMoreHorizontal"
+												:size="20" />
+										</template>
+									</NcButton>
+								</template>
+							</NcListItem>
+							<NcListItem v-if="signer.opened"
+								class="extra"
+								compact
+								:name="t('libresign', 'Requested on:')">
+								<template #name>
+									<strong>{{ t('libresign', 'Requested on:')}}</strong>
+									{{ formatDate(signer.request_sign_date) }}
+								</template>
+							</NcListItem>
+							<NcListItem v-if="signer.opened && signer.remote_address"
+								class="extra"
+								compact
+								:name="t('libresign', 'Remote address:')">
+								<template #name>
+									<strong>{{ t('libresign', 'Remote address:')}}</strong>
+									{{ signer.remote_address }}
+								</template>
+							</NcListItem>
+							<NcListItem v-if="signer.opened && signer.user_agent"
+								class="extra"
+								compact
+								:name="t('libresign', 'User agent:')">
+								<template #name>
+									<strong>{{ t('libresign', 'User agent:')}}</strong>
+									{{ signer.user_agent }}
+								</template>
+							</NcListItem>
+						</span>
+					</ul>
 				</div>
 				<NcButton v-if="clickedValidate"
 					type="primary"
@@ -74,24 +122,32 @@
 </template>
 
 <script>
+import {
+	mdiInformationSlabCircle,
+	mdiSignatureFreehand,
+	mdiUnfoldMoreHorizontal,
+	mdiUnfoldLessHorizontal,
+} from '@mdi/js'
 import JSConfetti from 'js-confetti'
 
 import axios from '@nextcloud/axios'
 import { showError, showSuccess } from '@nextcloud/dialogs'
 import { loadState } from '@nextcloud/initial-state'
 import { translate as t } from '@nextcloud/l10n'
+import Moment from '@nextcloud/moment'
 import { generateUrl, generateOcsUrl } from '@nextcloud/router'
 
+import NcAvatar from '@nextcloud/vue/dist/Components/NcAvatar.js'
 import NcButton from '@nextcloud/vue/dist/Components/NcButton.js'
+import NcIconSvgWrapper from '@nextcloud/vue/dist/Components/NcIconSvgWrapper.js'
+import NcListItem from '@nextcloud/vue/dist/Components/NcListItem.js'
 import NcLoadingIcon from '@nextcloud/vue/dist/Components/NcLoadingIcon.js'
 import NcNoteCard from '@nextcloud/vue/dist/Components/NcNoteCard.js'
 // eslint-disable-next-line import/no-named-as-default
 import NcRichText from '@nextcloud/vue/dist/Components/NcRichText.js'
 import NcTextField from '@nextcloud/vue/dist/Components/NcTextField.js'
 
-import iconB from '../../img/file-signature-solid.svg'
-import iconA from '../../img/info-circle-solid.svg'
-import BackgroundImage from '../../img/logo-gray.svg'
+import logoGray from '../../img/logo-gray.svg'
 import logger from '../logger.js'
 
 export default {
@@ -102,26 +158,24 @@ export default {
 		NcRichText,
 		NcButton,
 		NcLoadingIcon,
+		NcListItem,
+		NcAvatar,
+		NcIconSvgWrapper,
 		NcNoteCard,
 	},
 
 	data() {
-		const fileInfo = loadState('libresign', 'file_info', {})
 		return {
-			image: BackgroundImage,
-			infoDocument: t('libresign', 'Document informations'),
-			infoIcon: iconA,
-			signatureIcon: iconB,
-			title: t('libresign', 'Validate signature'),
-			legend: t('libresign', 'Enter the ID or UUID of the document to validate.'),
-			buttonTitle: t('libresign', 'Validation'),
-			noDateMessage: t('libresign', 'No date'),
-			myUuid: this.$route.params?.uuid ?? '',
-			hasInfo: Object.keys(fileInfo).length > 0,
-			hasLoading: false,
-			document: fileInfo,
-			documentUuid: '',
-			legalInformation: '',
+			logo: logoGray,
+			mdiUnfoldMoreHorizontal,
+			mdiInformationSlabCircle,
+			mdiUnfoldLessHorizontal,
+			mdiSignatureFreehand,
+			uuidToValidate: this.$route.params?.uuid ?? '',
+			hasInfo: false,
+			loading: false,
+			document: {},
+			legalInformation: loadState('libresign', 'legal_information', ''),
 			clickedValidate: false,
 		}
 	},
@@ -136,60 +190,73 @@ export default {
 		},
 	},
 	created() {
-		this.getData()
-		if (this.myUuid.length > 0) {
-			this.validate(this.myUuid)
+		if (this.uuidToValidate.length > 0) {
+			this.validate(this.uuidToValidate)
+		} else {
+			this.$set(this, 'document', loadState('libresign', 'file_info', {}))
+			this.hasInfo = Object.keys(this.document).length > 0
+			if (this.hasInfo) {
+				this.document.signers.forEach(signer => {
+					this.$set(signer, 'opened', false)
+				})
+			}
 		}
 	},
 	methods: {
+		formatDate(date) {
+			return Moment(Date.parse(date)).format('LL LTS')
+		},
+		toggleDetail(signer) {
+			this.$set(signer, 'opened', !signer.opened)
+		},
 		validate(id) {
 			if (id === this.document?.uuid) {
 				showSuccess(t('libresign', 'This document is valid'))
 				this.hasInfo = true
 				return
 			}
-			if (id.length >= 8) {
+			if (id.length === 36) {
 				this.validateByUUID(id)
 			} else {
 				this.validateByNodeID(id)
 			}
 		},
 		async validateByUUID(uuid) {
-			this.hasLoading = true
+			this.loading = true
 			try {
 				const response = await axios.get(generateOcsUrl(`/apps/libresign/api/v1/file/validate/uuid/${uuid}`))
 				showSuccess(t('libresign', 'This document is valid'))
-				this.document = response.data.ocs.data
+				this.$set(this, 'document', response.data.ocs.data)
+				this.document.signers.forEach(signer => {
+					this.$set(signer, 'opened', false)
+				})
 				this.hasInfo = true
-				this.hasLoading = false
+				this.loading = false
 				if (this.isAfterSigned) {
 					const jsConfetti = new JSConfetti()
 					jsConfetti.addConfetti()
 				}
 			} catch (err) {
-				this.hasLoading = false
+				this.loading = false
 				showError(err.response.data.ocs.data.errors[0])
 			}
 		},
 		async validateByNodeID(nodeId) {
-			this.hasLoading = true
+			this.loading = true
 			try {
 				const response = await axios.get(generateOcsUrl(`/apps/libresign/api/v1/file/validate/file_id/${nodeId}`))
 				showSuccess(t('libresign', 'This document is valid'))
-				this.document = response.data.ocs.data
+				this.$set(this, 'document', response.data.ocs.data)
 				this.hasInfo = true
-				this.hasLoading = false
+				this.loading = false
 				if (this.isAfterSigned) {
 					const jsConfetti = new JSConfetti()
 					jsConfetti.addConfetti()
 				}
 			} catch (err) {
-				this.hasLoading = false
+				this.loading = false
 				showError(err.response.data.ocs.data.errors[0])
 			}
-		},
-		async getData() {
-			this.legalInformation = loadState('libresign', 'legal_information', '')
 		},
 		getName(user) {
 			if (user.displayName) {
@@ -228,25 +295,14 @@ export default {
 				}
 			}
 			this.hasInfo = !this.hasInfo
-			this.myUuid = this.$route.params.uuid
+			this.uuidToValidate = this.$route.params.uuid
 		},
 	},
 }
 </script>
 
 <style lang="scss" scoped>
-$title-font: 1.5rem;
-$title-font-mobile: 1.3rem;
-$date-signed-font: .7rem;
-
-.jumbotron{
-	padding: 0;
-	margin: unset;
-	width: unset;
-	height: 100%;
-}
-
-.container{
+.container {
 	display: flex;
 	align-items: center;
 	justify-content: center;
@@ -254,12 +310,12 @@ $date-signed-font: .7rem;
 	width: 100%;
 	height: 100%;
 
-	.image{
+	.logo {
 		width: 100%;
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		img{
+		img {
 			width: 50%;
 			max-width: 422px;
 		}
@@ -269,7 +325,7 @@ $date-signed-font: .7rem;
 		}
 
 	}
-	#dataUUID{
+	#dataUUID {
 		width: 100%;
 		display: flex;
 		align-items: center;
@@ -277,178 +333,99 @@ $date-signed-font: .7rem;
 		@media screen and (max-width: 900px){
 			width: 100%;
 		}
+		h1 {
+			font-size: 24px;
+			font-weight: bold;
+			color: var(--color-main-text);
+		}
+		form {
+			background-color: var(--color-main-background);
+			color: var(--color-main-text);
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+			justify-content: center;
+			padding: 20px;
+			margin: 20px;
+			border-radius: 8px;
+			max-width: 500px;
+			width: 100%;
+			box-shadow: 0 0 6px 0 var(--color-box-shadow);
+
+			@media screen and (max-width: 900px) {
+				width: 100%;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				max-width: 100%;
+			}
+		}
+		button {
+			float: right;
+			margin-top: 20px;
+			align-self: flex-end;
+		}
+		.infor-container {
+			width: 100%;
+			margin-right: 20px;
+			.section {
+				background-color: var(--color-main-background);
+				padding: 20px;
+				border-radius: 8px;
+				box-shadow: 0 0 6px 0 var(--color-box-shadow);
+				margin-bottom: 10px;
+
+				.header {
+					display: flex;
+					margin-bottom: 2rem;
+				}
+				h1 {
+					font-size: 1.5rem;
+				}
+
+				.info-document {
+					color: var(--color-main-text);
+					display: flex;
+					flex-direction: column;
+					overflow: scroll;
+					.legal-information {
+						opacity: 0.8;
+						align-self: center;
+						font-size: 1rem;
+						overflow: scroll;
+					}
+
+					p {
+						font-size: 1rem;
+					}
+				}
+
+				.extra {
+					margin-left: 44px;
+					padding-right: 44px;
+					:deep(.list-item-content__name) {
+						white-space: unset;
+					}
+					:deep(.list-item__anchor) {
+						height: unset;
+					}
+				}
+			}
+		}
 	}
-	.legal-information{
-		opacity: 0.8;
-		align-self: center;
-		font-size: 1rem;
-		overflow: scroll;
-	}
-}
-
-form{
-	background-color: var(--color-main-background);
-	color: var(--color-main-text);
-	display: flex;
-	flex-direction: column;
-	align-items: center;
-	justify-content: center;
-	padding: 20px;
-	margin: 20px;
-	border-radius: 8px;
-	max-width: 500px;
-	width: 100%;
-	box-shadow: 0 0 6px 0 var(--color-box-shadow);
-
-	@media screen and (max-width: 900px) {
-		width: 100%;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		max-width: 100%;
-	}
-}
-
-h1{
-	font-size: 24px;
-	font-weight: bold;
-	color: var(--color-main-text);
-}
-
-button{
-	float: right;
-	margin-top: 20px;
-}
-
-.infor{
-	display: flex;
-	flex-direction: column;
-	h1{
-		font-size: $title-font;
-	}
-}
-
-.infor-container{
-	width: 100%;
-	margin-right: 20px;
-}
-
-.infor-bg{
-	background-color: var(--color-main-background);
-	padding: 20px 60px 20px 20px;
-	border-radius: 8px;
-	box-shadow: 0 0 6px 0 var(--color-box-shadow);
-
-	.infor-content{
-		display: flex;
-		flex-direction: row;
-		flex-wrap: wrap;
-		overflow: scroll;
-		height: 80%;
-		width: 98%;
-	}
-}
-
-.info-document{
-	color: var(--color-main-text);
-	display: flex;
-	flex-direction: column;
-	width: 100%;
-	margin-left: 30px;
-	justify-content: center;
-	overflow: scroll;
-
-	p{
-		font-size: 1rem;
-	}
-	button{
-		align-self: flex-end;
-	}
-	#sign {
-		display: flex;
-	}
-}
-
-.signed {
-	margin-top: 10px;
-	padding-right: 2px;
-	strong {
-		font-size: 22px;
-		margin-bottom: 10px;
-	}
-	button {
-		float: right;
-	}
-}
-
-.scroll {
-	max-height: 200px;
-	display: flex;
-	flex-wrap: wrap;
-	min-width: 200px;
-	max-width: 200px;
-}
-
-.subscriber {
-	display: flex;
-	flex-direction: column;
-	color: var(--color-main-text);
-	background-color: rgba(var(--color-info-rgb), 0.1);
-	border-radius: 8px;
-	padding: 5px 0px 5px 5px;
-	margin: 5px 5px 0px 0px;
-	min-height: 50px;
-	max-height: 60px;
-	padding-left: 10px;
-	width: 100%;
-	max-width: 98%;
-
-	.data-signed {
-		font-size: $date-signed-font;
-	}
-	b{
-		display: block;
-		overflow: hidden;
-		text-overflow: ellipsis;
-	}
-}
-
-.header {
-	display: flex;
-	margin-bottom: 2rem;
-	h1{
-		font-size: $title-font;
-	}
-}
-
-.icon{
-	width: 30px;
-	margin-right: 10px;
-	filter: var(--background-invert-if-dark);
 }
 
 @media screen and (max-width: 700px) {
-	.signed {
-		width: 100%;
-	}
-	.infor-container {
-		margin-right: 0px;
-		width: 100%;
-	}
-	.infor-bg {
-		box-shadow: none;
-	}
 	.container {
 		align-items: flex-start;
-	}
-	.infor {
 		h1 {
-			font-size: $title-font-mobile;
+			font-size: 1.3rem;
 		}
-	}
-	.header {
-		h1 {
-			font-size: $title-font-mobile;
+		.infor-container {
+			margin-right: 0px;
+			.section {
+				box-shadow: none;
+			}
 		}
 	}
 }
