003c8413904b852b7b6ec8d38ed7e0bb5262e38a
Vitor Mattos <vitor@php.rio>
2025-01-03 14:31:32 -0300

    refactor: change methods structure
    A step to be possible choose what kind of metadata I want
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/PdfParserService.php
+++ lib/Service/PdfParserService.php
@@ -30,10 +30,26 @@ use OCP\ITempManager;
 use Psr\Log\LoggerInterface;
 
 class PdfParserService {
+	private string $content = '';
 	public function __construct(
 		private ITempManager $tempManager,
 		private LoggerInterface $logger,
 	) {
+	}
+
+	public function setFile(File|string $file): self {
+		try {
+			if ($file instanceof File) {
+				$this->content = $file->getContent();
+			} else {
+				$this->content = $file;
+			}
+		} catch (\Throwable $th) {
+		}
+		if (!$this->content) {
+			throw new LibresignException('Empty file.');
+		}
+		return $this;
 	}
 
 	/**
@@ -41,23 +57,12 @@ class PdfParserService {
 	 * @throws LibresignException
 	 * @psalm-return array{p: int, d?: non-empty-list<array{w: mixed, h: mixed}>}
 	 */
-	public function getMetadata(File $node): array {
+	public function toArray(): array {
 		try {
-			$content = $node->getContent();
-		} catch (\Throwable $th) {
-		}
-		if (!$content) {
-			throw new LibresignException('Empty file.');
-		}
-
-		/**
-		 * Generate temporary file to prevent error when get path of
-		 * shared file
-		 */
-		$tempFile = $this->tempManager->getTemporaryFile('.pdf');
-		file_put_contents($tempFile, $content);
-		try {
-			$output = $this->parsePdfOnlyWithPhp($tempFile);
+			$output = $this->parsePdfOnlyWithPhp();
+		} catch (LibresignException $e) {
+			$this->logger->error('Impossible get metadata from this file: ' . $th->getMessage());
+			throw new LibresignException('Impossible get metadata from this file.');
 		} catch (\Throwable $th) {
 			if ($th->getMessage() === 'Secured pdf file are currently not supported.') {
 				throw new LibresignException('Secured pdf file are currently not supported.');
@@ -68,9 +73,9 @@ class PdfParserService {
 		return $output;
 	}
 
-	private function parsePdfOnlyWithPhp(string $filename): array {
+	private function parsePdfOnlyWithPhp(): array {
 		$parser = new \Smalot\PdfParser\Parser();
-		$pdf = $parser->parseFile($filename);
+		$pdf = $parser->parseContent($this->content);
 
 		$pages = $pdf->getPages();
 		$output = [
@@ -87,8 +92,7 @@ class PdfParserService {
 				'h' => $details['MediaBox'][3]
 			];
 			if (!is_numeric($widthAndHeight['w']) || !is_numeric($widthAndHeight['h'])) {
-				$this->logger->error('Impossible get metadata from this file: ' . $filename . '. Error to get page width and height.');
-				throw new LibresignException('Impossible get metadata from this file: ' . $filename . '. Error to get page width and height. If possible, open an issue at github.com/libresign/libresign with the file that you used.');
+				throw new LibresignException('Error to get page width and height. If possible, open an issue at github.com/libresign/libresign with the file that you used.');
 			}
 			$output['d'][] = $widthAndHeight;
 		}
