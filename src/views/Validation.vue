f40cbe1b33fc5fa7167ce06bdec81aa85f5c0416
Vitor Mattos <vitor@php.rio>
2025-01-03 16:18:16 -0300

    fix: revert previous commit
    This is necessary when the validation page is opened after sign a
    document
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -311,6 +311,8 @@ export default {
 			this.document.signers.forEach(signer => {
 				this.$set(signer, 'opened', false)
 			})
+		} else if (this.uuidToValidate.length > 0) {
+			this.validate(this.uuidToValidate)
 		}
 	},
 	methods: {
