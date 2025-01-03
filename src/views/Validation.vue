cd5515798329318c30810c69daa7b9bd7a4ce584
Vitor Mattos <vitor@php.rio>
2025-01-03 16:08:39 -0300

    fix: remove request to validation api when open validation page
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -300,8 +300,6 @@ export default {
 			this.document.signers.forEach(signer => {
 				this.$set(signer, 'opened', false)
 			})
-		} else if (this.uuidToValidate.length > 0) {
-			this.validate(this.uuidToValidate)
 		}
 	},
 	methods: {
