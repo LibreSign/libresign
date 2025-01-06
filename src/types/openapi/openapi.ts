42f8153cd2dbc67136e63fabcb9b5b6634ba1ad5
Vitor Mattos <vitor@php.rio>
2025-01-06 11:05:58 -0300

    feat: add subject (Common Name) to API response
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -1096,6 +1096,7 @@ export type components = {
         Signer: {
             description: string | null;
             displayName: string;
+            subject?: string;
             request_sign_date: string;
             /** Format: int64 */
             valid_from?: number;
