19296b535a13cf72630de2adb211bd5dd8c0efa2
Vitor Mattos <vitor@php.rio>
2025-01-11 17:27:28 -0300

    fix: unit test
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- tests/Unit/Service/MailServiceTest.php
+++ tests/Unit/Service/MailServiceTest.php
@@ -109,7 +109,7 @@ final class MailServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			});
 		$this->appConfig
 			->method('getValueBool')
-			->will($this->returnValue('1'));
+			->will($this->returnValue(true));
 		$actual = $this->service->notifyUnsignedUser($signRequest, 'a@b.coop');
 		$this->assertNull($actual);
 	}
