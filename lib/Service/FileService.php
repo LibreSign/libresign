e24186263334a5f962186db300ec6b56563860a3
Vitor Mattos <vitor@php.rio>
2025-03-25 17:35:37 -0300

    chore: return the validity of certificate as ATOM
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/FileService.php
+++ lib/Service/FileService.php
@@ -348,7 +348,9 @@ class FileService {
 			$this->fileData->signers[$index]['visibleElements'] = $this->getVisibleElements($signer->getId());
 			$this->fileData->signers[$index]['request_sign_date'] = $signer->getCreatedAt()->format(DateTimeInterface::ATOM);
 			if (empty($this->fileData->signers[$index]['signed'])) {
-				$this->fileData->signers[$index]['signed'] = $signer->getSigned()->format(DateTimeInterface::ATOM);
+				if ($signer->getSigned()) {
+					$this->fileData->signers[$index]['signed'] = $signer->getSigned()->format(DateTimeInterface::ATOM);
+				}
 			}
 			$metadata = $signer->getMetadata();
 			if (!empty($metadata['remote-address'])) {
@@ -361,7 +363,9 @@ class FileService {
 				$this->fileData->signers[$index]['notify'] = $metadata['notify'];
 			}
 			if ($signer->getSigned() && empty($this->fileData->signers[$index]['signed'])) {
-				$this->fileData->signers[$index]['signed'] = $signer->getSigned()->format(DateTimeInterface::ATOM);
+				if ($signer->getSigned()) {
+					$this->fileData->signers[$index]['signed'] = $signer->getSigned()->format(DateTimeInterface::ATOM);
+				}
 			}
 			// @todo refactor this code
 			if ($this->me || $this->identifyMethodId) {
