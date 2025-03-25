8fb730422f558bb539b567d13a413557de7aec9f
Vitor Mattos <vitor@php.rio>
2025-03-25 17:41:47 -0300

    fix: always return signed field
    When is null wasnt signed, when have a date, was signed at the date
    
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Service/FileService.php
+++ lib/Service/FileService.php
@@ -350,6 +350,8 @@ class FileService {
 			if (empty($this->fileData->signers[$index]['signed'])) {
 				if ($signer->getSigned()) {
 					$this->fileData->signers[$index]['signed'] = $signer->getSigned()->format(DateTimeInterface::ATOM);
+				} else {
+					$this->fileData->signers[$index]['signed'] = null;
 				}
 			}
 			$metadata = $signer->getMetadata();
@@ -365,6 +367,8 @@ class FileService {
 			if ($signer->getSigned() && empty($this->fileData->signers[$index]['signed'])) {
 				if ($signer->getSigned()) {
 					$this->fileData->signers[$index]['signed'] = $signer->getSigned()->format(DateTimeInterface::ATOM);
+				} else {
+					$this->fileData->signers[$index]['signed'] = null;
 				}
 			}
 			// @todo refactor this code
