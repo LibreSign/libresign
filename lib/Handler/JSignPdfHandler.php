3e4054f66372baf7ea1ed58fb22ce457e789ff3a
Vitor Mattos <vitor@php.rio>
2025-03-26 20:57:42 -0300

    fix: changes after refactor
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Handler/JSignPdfHandler.php
+++ lib/Handler/JSignPdfHandler.php
@@ -151,11 +151,10 @@ class JSignPdfHandler extends SignEngineHandler {
 	}
 
 	public function getSignatureText(): string {
-		$signatureText = parent::getSignatureText();
 		$params = $this->getSignatureParams();
 		$params['SignerName'] = '${signer}';
 		$params['SignatureDate'] = '${timestamp}';
-		$this->signatureTextService->parse(context: $params);
+		$signatureText = $this->signatureTextService->parse(context: $params);
 
 		$signatureText = '"' . str_replace(
 			['"', '$'],
