a476a5a2acbf0e74b72384499641377a7261cf9b
Vitor Mattos <vitor@php.rio>
2025-04-08 17:19:43 -0300

    chore: reduce indent level to be more readdable and sort items
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Settings/Admin.php
+++ lib/Settings/Admin.php
@@ -32,90 +32,30 @@ class Admin implements ISettings {
 	}
 	public function getForm(): TemplateResponse {
 		Util::addScript(Application::APP_ID, 'libresign-settings');
-		$this->initialState->provideInitialState(
-			'identify_methods',
-			$this->identifyMethodService->getIdentifyMethodsSettings()
-		);
-		$this->initialState->provideInitialState(
-			'certificate_engine',
-			$this->certificateEngineFactory->getEngine()->getName()
-		);
-		$this->initialState->provideInitialState(
-			'config_path',
-			$this->appConfig->getValueString(Application::APP_ID, 'config_path')
-		);
 		try {
 			$signatureParsed = $this->signatureTextService->parse();
-			$this->initialState->provideInitialState(
-				'signature_text_parsed',
-				$signatureParsed['parsed'],
-			);
+			$this->initialState->provideInitialState('signature_text_parsed', $signatureParsed['parsed']);
 		} catch (LibresignException $e) {
-			$this->initialState->provideInitialState(
-				'signature_text_parsed',
-				'',
-			);
-			$this->initialState->provideInitialState(
-				'signature_text_template_error',
-				$e->getMessage(),
-			);
+			$this->initialState->provideInitialState('signature_text_parsed', '');
+			$this->initialState->provideInitialState('signature_text_template_error', $e->getMessage());
 		}
-		$this->initialState->provideInitialState(
-			'signature_text_template',
-			$this->signatureTextService->getTemplate(),
-		);
-		$this->initialState->provideInitialState(
-			'template_font_size',
-			$this->signatureTextService->getTemplateFontSize(),
-		);
-		$this->initialState->provideInitialState(
-			'signature_font_size',
-			$this->signatureTextService->getSignatureFontSize(),
-		);
-		$this->initialState->provideInitialState(
-			'default_signature_width',
-			SignatureTextService::DEFAULT_SIGNATURE_WIDTH,
-		);
-		$this->initialState->provideInitialState(
-			'default_signature_height',
-			SignatureTextService::DEFAULT_SIGNATURE_HEIGHT,
-		);
-		$this->initialState->provideInitialState(
-			'signature_width',
-			$this->signatureTextService->getFullSignatureWidth(),
-		);
-		$this->initialState->provideInitialState(
-			'signature_height',
-			$this->signatureTextService->getFullSignatureHeight(),
-		);
-		$this->initialState->provideInitialState(
-			'signature_preview_zoom_level',
-			$this->appConfig->getValueFloat(Application::APP_ID, 'signature_preview_zoom_level', 100)
-		);
-		$this->initialState->provideInitialState(
-			'default_signature_text_template',
-			$this->signatureTextService->getDefaultTemplate(),
-		);
-		$this->initialState->provideInitialState(
-			'default_template_font_size',
-			$this->signatureTextService->getDefaultTemplateFontSize(),
-		);
-		$this->initialState->provideInitialState(
-			'default_signature_font_size',
-			SignatureTextService::SIGNATURE_DEFAULT_FONT_SIZE,
-		);
-		$this->initialState->provideInitialState(
-			'signature_available_variables',
-			$this->signatureTextService->getAvailableVariables(),
-		);
-		$this->initialState->provideInitialState(
-			'signature_render_mode',
-			$this->signatureTextService->getRenderMode(),
-		);
-		$this->initialState->provideInitialState(
-			'signature_background_type',
-			$this->signatureBackgroundService->getSignatureBackgroundType(),
-		);
+		$this->initialState->provideInitialState('certificate_engine', $this->certificateEngineFactory->getEngine()->getName());
+		$this->initialState->provideInitialState('config_path', $this->appConfig->getValueString(Application::APP_ID, 'config_path'));
+		$this->initialState->provideInitialState('default_signature_font_size', SignatureTextService::SIGNATURE_DEFAULT_FONT_SIZE);
+		$this->initialState->provideInitialState('default_signature_height', SignatureTextService::DEFAULT_SIGNATURE_HEIGHT);
+		$this->initialState->provideInitialState('default_signature_text_template', $this->signatureTextService->getDefaultTemplate());
+		$this->initialState->provideInitialState('default_signature_width', SignatureTextService::DEFAULT_SIGNATURE_WIDTH);
+		$this->initialState->provideInitialState('default_template_font_size', $this->signatureTextService->getDefaultTemplateFontSize());
+		$this->initialState->provideInitialState('identify_methods', $this->identifyMethodService->getIdentifyMethodsSettings());
+		$this->initialState->provideInitialState('signature_available_variables', $this->signatureTextService->getAvailableVariables());
+		$this->initialState->provideInitialState('signature_background_type', $this->signatureBackgroundService->getSignatureBackgroundType());
+		$this->initialState->provideInitialState('signature_font_size', $this->signatureTextService->getSignatureFontSize());
+		$this->initialState->provideInitialState('signature_height', $this->signatureTextService->getFullSignatureHeight());
+		$this->initialState->provideInitialState('signature_preview_zoom_level', $this->appConfig->getValueFloat(Application::APP_ID, 'signature_preview_zoom_level', 100));
+		$this->initialState->provideInitialState('signature_render_mode', $this->signatureTextService->getRenderMode());
+		$this->initialState->provideInitialState('signature_text_template', $this->signatureTextService->getTemplate());
+		$this->initialState->provideInitialState('signature_width', $this->signatureTextService->getFullSignatureWidth());
+		$this->initialState->provideInitialState('template_font_size', $this->signatureTextService->getTemplateFontSize());
 		return new TemplateResponse(Application::APP_ID, 'admin_settings');
 	}
 
