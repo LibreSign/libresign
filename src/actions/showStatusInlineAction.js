644a5727a8241c0e9bec24c4811e81013202ebdd
David Lima <antdavidlima@gmail.com>
2025-05-21 09:23:32 -0300

    chore(files-app-integration): fix problems to pass failing checks
    Change code style, add copyright text and fix end-point response type to
    pass previously failing tasks
    
    Signed-off-by: David Lima <antdavidlima@gmail.com>

--- src/actions/showStatusInlineAction.js
+++ src/actions/showStatusInlineAction.js
@@ -1,3 +1,7 @@
+/**
+ * SPDX-FileCopyrightText: 2020-2024 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
 import SignatureSvg from '@mdi/svg/svg/signature.svg'
 
 import { FileAction, registerFileAction } from '@nextcloud/files'
