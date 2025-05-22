fa232fb7eb88bffd762464202be8abf63d72fce6
David Lima <antdavidlima@gmail.com>
2025-05-21 21:58:57 -0300

    feat(files-app-integration): show status on signed and original files
    Add the property signedNodeId to the /file/list end-point to show an
    icon on the original and signed file, according to the signing status
    
    Signed-off-by: David Lima <antdavidlima@gmail.com>

--- lib/Db/SignRequestMapper.php
+++ lib/Db/SignRequestMapper.php
@@ -433,6 +433,7 @@ class SignRequestMapper extends QBMapper {
 			$qb->select(
 				'f.id',
 				'f.node_id',
+				'f.signed_node_id',
 				'f.user_id',
 				'f.uuid',
 				'f.name',
@@ -544,6 +545,7 @@ class SignRequestMapper extends QBMapper {
 		$row['status'] = (int)$row['status'];
 		$row['statusText'] = $this->fileMapper->getTextOfStatus($row['status']);
 		$row['nodeId'] = (int)$row['node_id'];
+		$row['signedNodeId'] = (int)$row['signed_node_id'];
 		$row['requested_by'] = [
 			'userId' => $row['user_id'],
 			'displayName' => $this->userManager->get($row['user_id'])?->getDisplayName(),
@@ -554,6 +556,7 @@ class SignRequestMapper extends QBMapper {
 		unset(
 			$row['user_id'],
 			$row['node_id'],
+			$row['signed_node_id'],
 		);
 		return $row;
 	}
