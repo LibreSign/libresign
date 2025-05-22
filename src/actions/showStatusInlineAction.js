012431eb3a9c69559cbbfe4ab66826ba6b4af61f
David Lima <antdavidlima@gmail.com>
2025-05-21 21:23:52 -0300

    feat(files-app-integration): use dynamic status icons and labels
    Change the status icon and label based on the response from the API
    
    Signed-off-by: David Lima <antdavidlima@gmail.com>

--- src/actions/showStatusInlineAction.js
+++ src/actions/showStatusInlineAction.js
@@ -2,17 +2,49 @@
  * SPDX-FileCopyrightText: 2020-2024 LibreCode coop and contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
-import SignatureSvg from '@mdi/svg/svg/signature.svg'
-
+import axios from '@nextcloud/axios'
 import { FileAction, registerFileAction } from '@nextcloud/files'
 import { loadState } from '@nextcloud/initial-state'
+import { generateOcsUrl } from '@nextcloud/router'
+
+import { SIGN_STATUS } from '../domains/sign/enum.js'
+import { fileStatus } from '../helpers/fileStatus.js'
+
+let nodeIds = []
+const nodeStatusById = new Map()
 
 const action = new FileAction({
 	id: 'show-status-inline',
 	displayName: () => '',
-	title: () => 'Status',
+	title: (nodes) => {
+		const nodeId = nodes[0].fileid
+
+		if (nodeStatusById.has(nodeId)) {
+			const node = nodeStatusById.get(nodeId)
+
+			if ([SIGN_STATUS.PARTIAL_SIGNED, SIGN_STATUS.SIGNED].includes(node.status)) {
+				return fileStatus.find(status => status.id === node.status).label
+			}
+		}
+
+		nodeIds.push(nodeId)
+
+		return ''
+	},
 	exec: async () => null,
-	iconSvgInline: () => SignatureSvg,
+	iconSvgInline: (nodes) => {
+		const nodeId = nodes[0].fileid
+
+		if (nodeStatusById.has(nodeId)) {
+			const node = nodeStatusById.get(nodeId)
+
+			if ([SIGN_STATUS.PARTIAL_SIGNED, SIGN_STATUS.SIGNED].includes(node.status)) {
+				return fileStatus.find(status => status.id === node.status).icon
+			}
+		}
+
+		return ''
+	},
 	inline: () => true,
 	enabled: (nodes) => {
 		return loadState('libresign', 'certificate_ok')
@@ -23,4 +55,14 @@ const action = new FileAction({
 	order: -1,
 })
 
-registerFileAction(action)
+axios.get(generateOcsUrl('/apps/libresign/api/v1/file/list'), {
+	params: {
+		nodeIds,
+	}
+}).then(({ data }) => {
+	data.ocs.data.data.forEach(node => {
+		nodeStatusById.set(node.nodeId, { status: node.status })
+	})
+
+	registerFileAction(action)
+}).finally(() => { nodeIds = [] })
