b7cfd99350c3fce4d53de4288a23acb093df3797
Samuelson Brito <samuelsonma@gmail.com>
2025-05-25 01:13:08 -0400

    feat: notify admins only once per LibreSign version after upgrade
    Signed-off-by: Samuelson Brito <samuelsonma@gmail.com>

--- lib/Migration/NotifyAdminsAfterUpgrade.php
+++ lib/Migration/NotifyAdminsAfterUpgrade.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace OCA\Libresign\Migration;
 
 use OCA\Libresign\AppInfo\Application as AppInfoApplication;
+use OCP\App\IAppManager;
+use OCP\IConfig;
+use OCP\IL10N;
 use OCP\IUserManager;
 use OCP\Migration\IOutput;
 use OCP\Migration\IRepairStep;
@@ -14,6 +17,8 @@ class NotifyAdminsAfterUpgrade implements IRepairStep {
 	public function __construct(
 		private IUserManager $userManager,
 		private NotificationManager $notificationManager,
+		private IConfig $config,
+		private IAppManager $appManager,
 	) {
 	}
 
@@ -22,6 +27,14 @@ class NotifyAdminsAfterUpgrade implements IRepairStep {
 	}
 
 	public function run(IOutput $output): void {
+
+		$currentVersion = $currentVersion = $this->appManager->getAppVersion(AppInfoApplication::APP_ID);
+    	$lastNotifiedVersion = $this->config->getAppValue(AppInfoApplication::APP_ID, 'last_notified_version', '');
+
+		if ($currentVersion === $lastNotifiedVersion) {
+			return;
+		}
+
 		$admins = $this->userManager->search('', 1, 0, ['admin']);
 		foreach ($admins as $admin) {
 			$notification = $this->notificationManager->createNotification();
@@ -31,9 +44,12 @@ class NotifyAdminsAfterUpgrade implements IRepairStep {
 				->setDateTime(new \DateTime())
 				->setObject('upgrade', '1')
 				->setSubject('libresign_upgrade', [
-					'message' => 'LibreSign has been updated! Consider supporting the project: https://libresign.coop'
+					'message' => 'LibreSign has been updated to version ' . $currentVersion . '! Consider supporting the project: https://libresign.coop'
 				]);
 			$this->notificationManager->notify($notification);
 		}
+
+		$this->config->setAppValue(AppInfoApplication::APP_ID, 'last_notified_version', $currentVersion);
 	}
+
 }
