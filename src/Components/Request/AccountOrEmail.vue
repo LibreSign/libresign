5f4bf0508872b8b876efc08b62b2e7e0ff96f8f9
dehaarte <jahdehaarte@gmail.com>
2025-06-11 19:57:25 -0400

    fix: implemented debounce on signer search field
    Signed-off-by: dehaarte <jahdehaarte@gmail.com>

--- src/Components/Request/AccountOrEmail.vue
+++ src/Components/Request/AccountOrEmail.vue
@@ -29,6 +29,7 @@
 	</div>
 </template>
 <script>
+import debounce from 'debounce'
 import AlertCircle from 'vue-material-design-icons/AlertCircleOutline.vue'
 
 import axios from '@nextcloud/axios'
@@ -98,7 +99,7 @@ export default {
 		}
 	},
 	methods: {
-		async asyncFind(search, lookup = false) {
+		async _asyncFind(search, lookup = false) {
 			search = search.trim()
 			this.loading = true
 
@@ -117,6 +118,9 @@ export default {
 			this.options = response.data.ocs.data
 			this.loading = false
 		},
+		asyncFind: debounce(function(search, lookup = false) {
+			this._asyncFind(search,lookup)
+		}, 500),
 	},
 }
 </script>
