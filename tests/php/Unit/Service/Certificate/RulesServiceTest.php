a5a38912a7fed53e35c378a17c47964283774928
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-07-22 17:04:09 -0300

    fix: run cs:fix
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Service/Certificate/RulesServiceTest.php
+++ tests/php/Unit/Service/Certificate/RulesServiceTest.php
@@ -8,7 +8,6 @@ declare(strict_types=1);
 
 namespace OCA\Libresign\Tests\Unit\Service;
 
-use OCA\Activity\Tests\Mock\Provider;
 use OCA\Libresign\AppInfo\Application;
 use OCA\Libresign\Service\Certificate\RulesService;
 use OCP\IL10N;
