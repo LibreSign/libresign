3d5c7427f235c2c8b725b9c09378f40cd657c676
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-07-22 17:03:20 -0300

    fix: consider to return empty array when is invalid field name
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/Certificate/RulesService.php
+++ lib/Service/Certificate/RulesService.php
@@ -50,6 +50,9 @@ class RulesService {
 	public function getRule(string $fieldName): array {
 		if(!isset($this->rules[$fieldName]['helperText'])){
 			$this->rules[$fieldName]['helperText'] = $this->getHelperText($fieldName);
+			if (empty($this->rules[$fieldName]['helperText'])) {
+				unset($this->rules[$fieldName]['helperText']);
+			}
 		}
 		return $this->rules[$fieldName];
 	}
