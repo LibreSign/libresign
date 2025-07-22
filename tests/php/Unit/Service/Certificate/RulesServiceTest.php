8acaddfdfbd32df4fec25f8fd622a207112fa6a7
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-07-22 17:00:05 -0300

    chore: cover with more tests
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ tests/php/Unit/Service/Certificate/RulesServiceTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Libresign\Tests\Unit\Service;
+
+use OCA\Libresign\AppInfo\Application;
+use OCA\Libresign\Service\Certificate\RulesService;
+use OCP\IL10N;
+use OCP\L10N\IFactory as IL10NFactory;
+use PHPUnit\Framework\Attributes\DataProvider;
+
+final class RulesServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
+	private IL10N $l10n;
+
+	public function setUp(): void {
+		$this->l10n = \OCP\Server::get(IL10NFactory::class)->get(Application::APP_ID);
+	}
+
+	public function getService(): RulesService {
+		return new RulesService(
+			$this->l10n,
+		);
+	}
+
+	#[DataProvider('providerGetHelperTextWithValidFieldName')]
+	public function testGetHelperTextWithValidFieldName(string $fieldName, string $expected): void {
+		$service = $this->getService();
+		$helperText = $service->getHelperText($fieldName);
+		$this->assertNotEmpty($helperText);
+		$this->assertStringContainsString($expected, $helperText);
+	}
+
+	public static function providerGetHelperTextWithValidFieldName(): array {
+		return [
+			['CN', 'Common Name'],
+			['C', 'country code'],
+			['ST', 'states'],
+			['L', 'locality'],
+			['O', 'organization'],
+			['OU', 'organizational unit'],
+		];
+	}
+}
