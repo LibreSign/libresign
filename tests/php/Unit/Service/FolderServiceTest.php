d98e0f97f311c9c59495a27c52cc77324d52665a
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-08-16 15:35:49 -0300

    chore: cover class with more tests
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Service/FolderServiceTest.php
+++ tests/php/Unit/Service/FolderServiceTest.php
@@ -19,6 +19,8 @@ use OCP\Files\SimpleFS\ISimpleFile;
 use OCP\Files\SimpleFS\ISimpleFolder;
 use OCP\IGroupManager;
 use OCP\IL10N;
+use OCP\IUser;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\MockObject\MockObject;
 
 final class FakeFolder implements ISimpleFolder {
@@ -72,7 +74,7 @@ final class FolderServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$this->l10n = $this->createMock(IL10N::class);
 	}
 
-	private function getFolderService(?string $userId = '171'): FolderSErvice {
+	private function getInstance(?string $userId = '171'): FolderService {
 		$service = new FolderService(
 			$this->root,
 			$this->appDataFactory,
@@ -84,47 +86,192 @@ final class FolderServiceTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		return $service;
 	}
 
-	public function testGetFolderAsUnauthenticatedWhenUserIdIsInvalid():void {
+	public function testGetContainerFolderAsUnauthenticatedWhenUserIdIsInvalid():void {
 		$folder = $this->createMock(\OCP\Files\Folder::class);
-		$folder->method('newFolder')->willReturn($folder);
 		$fakeFolder = new FakeFolder();
 		$fakeFolder->folder = $folder;
 		$appData = $this->createMock(IAppData::class);
 		$appData->method('getFolder')->willReturn($fakeFolder);
 		$this->appDataFactory->method('get')->willReturn($appData);
 
-		$service = $this->getFolderService(null);
-		$actual = $service->getFolder();
+		$service = $this->getInstance(userId: null);
+		$actual = $this->invokePrivate($service, 'getContainerFolder');
 		$this->assertEquals($folder, $actual);
 	}
 
-	public function testGetFileWithInvalidNodeId():void {
-		$folder = $this->createMock(\OCP\Files\Folder::class);
-		$folder->method('isUpdateable')->willReturn(true);
-		$this->root->method('getUserFolder')->willReturn($folder);
+	#[DataProvider('providerGetFolderName')]
+	public function testGetFolderName(array $data, string $uid, ?string $expectExact, ?string $expectRegex): void {
+		$user = $this->createMock(IUser::class);
+		$user->method('getUID')->willReturn($uid);
 
-		$service = $this->getFolderService();
-		$this->expectExceptionMessage('Invalid node');
-		$service->getFileById(171);
+		$service = $this->getInstance();
+		$actual = $service->getFolderName($data, $user);
+
+		if ($expectExact !== null) {
+			self::assertSame($expectExact, $actual);
+		} else {
+			self::assertMatchesRegularExpression($expectRegex, $actual);
+		}
 	}
 
-	public function testGetFolderWithValidNodeId():void {
-		$folder = $this->createMock(\OCP\Files\Folder::class);
-		$folder->method('isUpdateable')->willReturn(true);
-		$folder->method('newFolder')->willReturn($folder);
-		$this->root->method('getUserFolder')
-			->willReturn($folder);
+	public static function providerGetFolderName(): array {
+		$defaultDateRegex = '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{6}';
 
-		$fakeFolder = new FakeFolder();
-		$fakeFolder->folder = $folder;
-		$appData = $this->createMock(IAppData::class);
-		$appData->method('getFolder')
-			->willReturn($fakeFolder);
-		$this->appDataFactory->method('get')
-			->willReturn($appData);
-
-		$service = $this->getFolderService('1');
-		$actual = $service->getFolder();
-		$this->assertInstanceOf(\OCP\Files\Folder::class, $actual);
+		return [
+			'explicit-folderName' => [
+				'data' => [
+					'name' => 'MyDoc',
+					'settings' => [
+						'folderName' => 'predefinido/abc',
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => 'predefinido/abc',
+				'expectRegex' => null,
+			],
+			'defaults-with-name' => [
+				'data' => [
+					'name' => 'MyDoc',
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^' . $defaultDateRegex . '_MyDoc_u1$/',
+			],
+			'defaults-without-name' => [
+				'data' => [
+					'settings' => [],
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^' . $defaultDateRegex . '_u1$/',
+			],
+			'custom-order-no-date' => [
+				'data' => [
+					'name' => 'Doc',
+					'settings' => [
+						'separator' => '-',
+						'folderPatterns' => [
+							['name' => 'name'],
+							['name' => 'userId'],
+						],
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^Doc-u1$/',
+			],
+			'custom-date-format' => [
+				'data' => [
+					'name' => 'Doc',
+					'settings' => [
+						'separator' => '.',
+						'folderPatterns' => [
+							['name' => 'date', 'setting' => 'Ymd'],
+							['name' => 'name'],
+							['name' => 'userId'],
+						],
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^[0-9]{8}\.Doc\.u1$/',
+			],
+			'only-date' => [
+				'data' => [
+					'settings' => [
+						'separator' => '/',
+						'folderPatterns' => [
+							['name' => 'date', 'setting' => 'Y-m-d\TH-i-s-u'],
+						],
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^' . $defaultDateRegex . '$/',
+			],
+			'unknown-pattern-ignored' => [
+				'data' => [
+					'name' => 'Doc',
+					'settings' => [
+						'separator' => '+',
+						'folderPatterns' => [
+							['name' => 'foo'],
+							['name' => 'name'],
+						],
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^Doc$/',
+			],
+			'name-empty-token-ignored' => [
+				'data' => [
+					'name' => '',
+					'settings' => [
+						'separator' => '_',
+						'folderPatterns' => [
+							['name' => 'date', 'setting' => 'Y-m-d\TH-i-s-u'],
+							['name' => 'name'],
+							['name' => 'userId'],
+						],
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => null,
+				'expectRegex' => '/^' . $defaultDateRegex . '_u1$/',
+			],
+			'only-userId' => [
+				'data' => [
+					'settings' => [
+						'separator' => '=',
+						'folderPatterns' => [
+							['name' => 'userId'],
+						],
+					],
+				],
+				'uid' => 'user-XYZ',
+				'expectExact' => 'user-XYZ',
+				'expectRegex' => null,
+			],
+			'only-name' => [
+				'data' => [
+					'name' => 'OnlyName',
+					'settings' => [
+						'separator' => '--',
+						'folderPatterns' => [
+							['name' => 'name'],
+						],
+					],
+				],
+				'uid' => 'u1',
+				'expectExact' => 'OnlyName',
+				'expectRegex' => null,
+			],
+			'multichar-separator-with-all' => [
+				'data' => [
+					'name' => 'DocX',
+					'settings' => [
+						'separator' => '~~',
+						'folderPatterns' => [
+							['name' => 'userId'],
+							['name' => 'name'],
+							['name' => 'date', 'setting' => 'YmdHis'],
+						],
+					],
+				],
+				'uid' => 'U777',
+				'expectExact' => null,
+				'expectRegex' => '/^U777~~DocX~~[0-9]{14}$/',
+			],
+			'settings-present-without-folderPatterns' => [
+				'data' => [
+					'name' => 'WithSettings',
+					'settings' => [],
+				],
+				'uid' => 'u9',
+				'expectExact' => null,
+				'expectRegex' => '/^' . $defaultDateRegex . '_WithSettings_u9$/',
+			],
+		];
 	}
 }
