842bda7a5ba25845ce88a08b5650f9fa16570f1a
Kemal Oktay <iletisim@oktayaktogan.com.tr>
2025-09-10 17:46:15 +0300

    Missing part added
    Signed-off-by: Kemal Oktay <iletisim@oktayaktogan.com.tr>

--- lib/Service/PdfParserService.php
+++ lib/Service/PdfParserService.php
@@ -118,6 +118,7 @@ class PdfParserService {
 
 		// The output of this command go to STDERR and shell_exec get the STDOUT
 		// With 2>&1 the STRERR is redirected to STDOUT
+		$pdfinfo = shell_exec('pdfinfo ' . $filename . ' -l -1 2>&1');
 		if (!$pdfinfo) {
 			return [];
 		}
