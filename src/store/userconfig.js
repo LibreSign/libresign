76c1c4ba4c18b30150ec01ef3cd0bafbefa3ddb5
Crisciany Souza <criscianysilva1997@gmail.com>
2025-09-12 17:02:18 -0400

    chore: configurations to fix the button and try to save the state
    Signed-off-by: Crisciany Souza <criscianysilva1997@gmail.com>

--- src/store/userconfig.js
+++ src/store/userconfig.js
@@ -5,32 +5,45 @@
 
 import { defineStore } from 'pinia'
 import { set } from 'vue'
-import axios from 'axios'
+import { generateUrl } from '@nextcloud/router'
+import axios from '@nextcloud/axios'
+import { emit, subscribe } from '@nextcloud/event-bus'
 
 export const useUserConfigStore = defineStore('userconfig', {
 	state: () => ({
-		grid_view: true,
+		grid_view: true, // valor inicial, mas será sobrescrito pelo servidor
 	}),
 	actions: {
+		onUpdate(key, value) {
+			set(this, key, value)
+		},
+
 		async update(key, value) {
 			const oldValue = this[key]
-
-			set(this, key, value)
+			this.onUpdate(key, value)
 
 			try {
 				const response = await axios.put(
-					`/apps/files/api/v1/config/${key}`,
-					{ value }
+					generateUrl('/apps/files/api/v1/config/{key}', { key }),
+					{ value },
 				)
 
 				if (response?.data?.value !== undefined) {
-					set(this, key, response.data.value)
+					this.onUpdate(key, response.data.value)
 				}
+
+				emit('files:config:updated', { key, value })
 			} catch (error) {
 				console.error('Erro ao salvar configuração:', error)
-
-				set(this, key, oldValue)
+				this.onUpdate(key, oldValue)
 			}
+		},
+
+		initListeners() {
+			console.log('Initializing user config listeners...')
+			subscribe('files:config:updated', ({ key, value }) => {
+				this.onUpdate(key, value)
+			})
 		},
 	},
 })
