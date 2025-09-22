bd55348391b00db1a43c9a2709d269a62c0689c6
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-22 18:05:42 -0300

    chore: add docblock
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Components/RightSidebar/RequestSignatureTab.vue
+++ src/Components/RightSidebar/RequestSignatureTab.vue
@@ -128,6 +128,7 @@ const iconMap = {
 	svgEmail,
 	svgSignal,
 	svgSms,
+	// Workaround to rotate telegram icon. MDI does not provide a telegram icon.
 	svgTelegram: '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 28 28"><g transform="rotate(-45, 12, 12)"><path d="M2,21L23,12L2,3V10L17,12L2,14V21Z" /></g></svg>',
 	svgWhatsapp,
 	svgXmpp,
