a795fbf1a03969e5bbc15fb7edee7bd56b96fc6d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-22 17:17:40 -0300

    fix: remove todo and fix method to use twofactor_gateway
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/IdentifyMethod/SignatureMethod/TokenService.php
+++ lib/Service/IdentifyMethod/SignatureMethod/TokenService.php
@@ -9,49 +9,42 @@ declare(strict_types=1);
 namespace OCA\Libresign\Service\IdentifyMethod\SignatureMethod;
 
 use OCA\Libresign\Service\MailService;
+use OCP\AppFramework\OCS\OCSForbiddenException;
+use OCP\IL10N;
 use OCP\Security\IHasher;
 use OCP\Security\ISecureRandom;
+use OCP\Server;
 
 class TokenService {
 	public const TOKEN_LENGTH = 6;
-	public const SIGN_PASSWORD = 'password';
-	public const SIGN_SIGNAL = 'signal';
-	public const SIGN_TELEGRAM = 'telegram';
-	public const SIGN_SMS = 'sms';
-	public const SIGN_EMAIL = 'email';
 
 	public function __construct(
 		private ISecureRandom $secureRandom,
 		private IHasher $hasher,
 		private MailService $mail,
+		private IL10N $l10n,
 	) {
 	}
 
+	public function sendCodeByGateway(string $identifier, string $code, string $gatewayName): string {
+		$gateway = $this->getGateway($gatewayName);
+
+		$code = $this->secureRandom->generate(self::TOKEN_LENGTH, ISecureRandom::CHAR_DIGITS);
+		$gateway->send($identifier, $this->l10n->t('%s is your LibreSign verification code.', $code));
+		return $this->hasher->hash($code);
+	}
+
 	/**
-	 * @todo check this code and put to work
+	 * @throws OCSForbiddenException
+	 * @return \OCA\TwoFactorGateway\Provider\Gateway\IGateway
 	 */
-	public function sendCodeByGateway(string $code, string $gatewayName): void {
-		// 	$user = \OCP\Server::get(IUserSession::class)->getUser();
-		// 	$gateway = $this->getGateway($user, $gatewayName);
-
-		// 	$userAccount = $this->accountManager->getAccount($user);
-		// 	$identifier = $userAccount->getProperty(IAccountManager::PROPERTY_PHONE)->getValue();
-		// 	$gateway->send($user, $identifier, $this->l10n->t('%s is your LibreSign verification code.', $code));
-		// }
-
-		// /**
-		//  * @throws OCSForbiddenException
-		//  */
-		// private function getGateway(IUser $user, string $gatewayName): \OCA\TwoFactorGateway\Service\Gateway\IGateway {
-		// 	if (!$this->appManager->isEnabledForUser('twofactor_gateway', $user)) {
-		// 		throw new OCSForbiddenException($this->l10n->t('Authorize signing using %s token is disabled because Nextcloud Two-Factor Gateway is not enabled.', $gatewayName));
-		// 	}
-		// 	$factory = $this->serverContainer->get('\OCA\TwoFactorGateway\Service\Gateway\Factory');
-		// 	$gateway = $factory->getGateway($gatewayName);
-		// 	if (!$gateway->getConfig()->isComplete()) {
-		// 		throw new OCSForbiddenException($this->l10n->t('Gateway %s not configured on Two-Factor Gateway.', $gatewayName));
-		// 	}
-		// 	return $gateway;
+	private function getGateway(string $gatewayName) {
+		$factory = Server::get('\OCA\TwoFactorGateway\Service\Gateway\Factory');
+		$gateway = $factory->getGateway($gatewayName);
+		if (!$gateway->getConfig()->isComplete()) {
+			throw new OCSForbiddenException($this->l10n->t('Gateway %s not configured on Two-Factor Gateway.', $gatewayName));
+		}
+		return $gateway;
 	}
 
 	public function sendCodeByEmail(string $email, string $displayName): string {
