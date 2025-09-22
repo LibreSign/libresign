88ff9a35758f105f7144d567402fd7a115e11cac
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-09-22 17:24:59 -0300

    feat: change way to add a signer, use a modal to be possible split
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/Components/Request/IdentifySigner.vue
+++ src/Components/Request/IdentifySigner.vue
@@ -4,13 +4,12 @@
 -->
 <template>
 	<div class="identifySigner">
-		<AccountOrEmail v-if="methods.account.enabled || methods.email.enabled"
-			:required="methods.account.required || methods.email.required"
+		<SignerSelect
 			:signer="signer"
 			:placeholder="placeholder"
-			@update:account="updateAccount"
-			@update:email="updateEmail"
-			@update:display-name="updateDisplayName" />
+			:method="method"
+			@update:signer="updateSigner" />
+
 		<label v-if="signerSelected" for="name-input">{{ t('libresign', 'Signer name') }}</label>
 		<NcTextField v-if="signerSelected"
 			v-model="displayName"
@@ -40,8 +39,7 @@ import { loadState } from '@nextcloud/initial-state'
 
 import NcButton from '@nextcloud/vue/components/NcButton'
 import NcTextField from '@nextcloud/vue/components/NcTextField'
-
-import AccountOrEmail from './AccountOrEmail.vue'
+import SignerSelect from './SignerSelect.vue'
 
 import { useFilesStore } from '../../store/files.js'
 
@@ -50,7 +48,7 @@ export default {
 	components: {
 		NcButton,
 		NcTextField,
-		AccountOrEmail,
+		SignerSelect,
 	},
 	props: {
 		signerToEdit: {
@@ -60,7 +58,14 @@ export default {
 				displayName: '',
 				identifyMethods: [],
 			}),
-			required: false,
+		},
+		method: {
+			type: String,
+			default: 'all',
+		},
+		placeholder: {
+			type: String,
+			default: t('libresign', 'Name'),
 		},
 	},
 	setup() {
@@ -74,51 +79,15 @@ export default {
 			nameHaveError: false,
 			displayName: '',
 			identify: '',
-			methods: {
-				account: {
-					enabled: false,
-					required: false,
-					value: '',
-				},
-				email: {
-					enabled: false,
-					required: false,
-					value: '',
-				},
-			},
+			signer: {},
 		}
 	},
 	computed: {
-		signer() {
-			if (this.methods.account.value.length > 0) {
-				return {
-					id: this.methods.account.value,
-					icon: 'icon-user',
-					isNoUser: false,
-					displayName: this.signerToEdit.displayName,
-				}
-			} else if (this.methods.email.value.length > 0) {
-				return {
-					id: this.methods.email.value,
-					icon: 'icon-mail',
-					isNoUser: true,
-					displayName: this.signerToEdit.displayName,
-				}
-			} else {
-				return {}
-			}
-		},
 		signerSelected() {
-			return this.methods.account.value || this.methods.email.value
+			return !!this.signer?.id
 		},
 		isNewSigner() {
 			return this.id === null || this.id === undefined
-		},
-		placeholder() {
-			if (!this.methods.account.enabled && this.methods.email.enabled) {
-				return t('libresign', 'Email')
-			}
-			return t('libresign', 'Name')
 		},
 		saveButtonText() {
 			if (this.isNewSigner) {
@@ -128,82 +97,41 @@ export default {
 		},
 	},
 	beforeMount() {
-		this.displayName = ''
-		this.identify = ''
-		this.methods.account.value = ''
-		this.methods.email.value = ''
-		if (Object.keys(this.signerToEdit).length > 0) {
-			this.identify = this.signerToEdit.identify ?? this.signerToEdit.signRequestId
-			this.updateDisplayName(this.signerToEdit.displayName)
-			this.signerToEdit.identifyMethods.forEach(method => {
-				if (method.method === 'email') {
-					this.methods.email.value = method.value
-				} else if (method.method === 'account') {
-					this.methods.account.value = method.value
-				}
-			})
-		}
-		const methods = loadState('libresign', 'identify_methods')
-		methods.forEach((method) => {
-			if (method.name === 'account') {
-				this.methods.account.enabled = method.enabled
-				this.methods.account.required = method.mandatory
-			} else if (method.name === 'email') {
-				this.methods.email.enabled = method.enabled
-				this.methods.email.required = method.mandatory
+		this.displayName = this.signerToEdit.displayName ?? ''
+		this.identify = this.signerToEdit.identify ?? this.signerToEdit.signRequestId ?? ''
+		if (Object.keys(this.signerToEdit).length > 0 && this.signerToEdit.identifyMethods?.length) {
+			const method = this.signerToEdit.identifyMethods[0]
+			this.signer = {
+				id: method.value,
+				method: method.method,
+				displayName: this.signerToEdit.displayName,
 			}
-		})
+		}
 	},
 	methods: {
+		updateSigner(signer) {
+			this.signer = signer
+			this.displayName = signer.displayName ?? ''
+			this.identify = signer.id ?? ''
+		},
 		saveSigner() {
-			const signer = {
+			if (!this.signer?.method || !this.signer?.id) {
+				return
+			}
+			this.filesStore.signerUpdate({
 				displayName: this.displayName,
 				identify: this.identify,
-				identifyMethods: [],
-			}
-			let canSave = false
-			if (this.methods.account.enabled) {
-				if (Object.keys(this.methods.account.value).length > 0) {
-					canSave = true
-					signer.identifyMethods.push({
-						method: 'account',
-						value: this.methods.account.value,
-					})
-				}
-			}
-			if (this.methods.email.enabled) {
-				if (Object.keys(this.methods.email.value).length > 0) {
-					canSave = true
-					signer.identifyMethods.push({
-						method: 'email',
-						value: this.methods.email.value,
-					})
-				}
-			}
-			if (canSave) {
-				this.filesStore.signerUpdate(signer)
-			}
+				identifyMethods: [
+					{
+						method: this.signer.method,
+						value: this.signer.id,
+					},
+				],
+			})
 			this.displayName = ''
 			this.identify = ''
-			this.identifyMethods = []
+			this.signer = {}
 			this.filesStore.disableIdentifySigner()
-		},
-		updateDisplayName(name) {
-			this.displayName = name ?? ''
-		},
-		updateEmail(email) {
-			if (typeof email !== 'object') {
-				this.methods.email.value = ''
-			} else {
-				this.methods.email.value = email.id
-			}
-		},
-		updateAccount(account) {
-			if (typeof account !== 'object') {
-				this.methods.account.value = ''
-			} else {
-				this.methods.account.value = account.id
-			}
 		},
 		onNameChange() {
 			const name = this.displayName.trim()
