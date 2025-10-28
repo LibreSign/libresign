eb59525831230fa38aadb6872c9e5faefe7d50ec
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-10-28 09:54:18 -0300

    fix: add pending rows after backport from main PR
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -44,6 +44,7 @@ import RootCertificateCfssl from './RootCertificateCfssl.vue'
 import RootCertificateOpenSsl from './RootCertificateOpenSsl.vue'
 import SignatureHashAlgorithm from './SignatureHashAlgorithm.vue'
 import SignatureStamp from './SignatureStamp.vue'
+import SupportProject from './SupportProject.vue'
 import TSA from './TSA.vue'
 import Validation from './Validation.vue'
 
@@ -65,6 +66,7 @@ export default {
 		RootCertificateOpenSsl,
 		SignatureHashAlgorithm,
 		SignatureStamp,
+		SupportProject,
 		TSA,
 		Validation,
 		Reminders,
