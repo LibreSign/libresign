36cab0f7cfa406954598e52d7c0a1e7e49f8636a
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-12-04 12:52:23 -0300

    fix: prevent error when generating QR code without validationSite
    Only generate QR code if validationSite is available in templateVars.
    This prevents undefined array key error when uuid is not set.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Handler/FooterHandler.php
+++ lib/Handler/FooterHandler.php
@@ -151,7 +151,7 @@ class FooterHandler {
 			}
 		}
 
-		if ($this->appConfig->getValueBool(Application::APP_ID, 'write_qrcode_on_footer', true)) {
+		if ($this->appConfig->getValueBool(Application::APP_ID, 'write_qrcode_on_footer', true) && isset($this->templateVars['validationSite'])) {
 			$this->templateVars['qrcode'] = $this->getQrCodeImageBase64($this->templateVars['validationSite']);
 		}
 
