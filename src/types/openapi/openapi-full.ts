2af3f8c80a0e3af634494c383e72e3188098347a
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2025-12-24 18:54:30 -0300

    fix: return oid to API
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-full.ts
+++ src/types/openapi/openapi-full.ts
@@ -1776,6 +1776,7 @@ export type components = {
             identifyMethods?: components["schemas"]["IdentifyMethod"][];
             visibleElements?: components["schemas"]["VisibleElement"][];
             signatureMethods?: components["schemas"]["SignatureMethods"];
+            uid?: string;
         };
         UserElement: {
             /** Format: int64 */
