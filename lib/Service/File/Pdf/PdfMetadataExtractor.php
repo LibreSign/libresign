3c300d51b418c85aa2722805ce58c78ab406c1e9
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-01-14 19:40:29 -0300

    fix: date interval of this file
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/File/Pdf/PdfMetadataExtractor.php
+++ lib/Service/File/Pdf/PdfMetadataExtractor.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 /**
- * SPDX-FileCopyrightText: 2025 LibreCode coop and contributors
+ * SPDX-FileCopyrightText: 2020-2026 LibreCode coop and contributors
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
