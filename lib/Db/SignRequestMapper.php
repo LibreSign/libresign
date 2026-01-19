d82ee30546d84f0129385abb7540226cdaf5ec3b
Crisciany Souza <crisciany.souza@librecode.coop>
2026-01-19 14:17:37 -0400

    feat: remove orderBy line
    Signed-off-by: Crisciany Souza <crisciany.souza@librecode.coop>

--- lib/Db/SignRequestMapper.php
+++ lib/Db/SignRequestMapper.php
@@ -474,9 +474,6 @@ class SignRequestMapper extends QBMapper {
 
 		$qb = $this->getFilesAssociatedFilesWithMeQueryBuilder($user->getUID(), $filter, false, $sort);
 
-
-		$qb->orderBy('f.created_at', 'DESC');
-
 		$result = $qb->executeQuery();
 		$files = [];
 
