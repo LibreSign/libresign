f0da11e4efb61e73a6442b3b777e08ba3de0c4ab
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-10 18:40:18 -0300

    fix: whatsapp name
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/IdentifyMethod/SignatureMethod/Whatsapp.php
+++ lib/Service/IdentifyMethod/SignatureMethod/Whatsapp.php
@@ -16,7 +16,7 @@ class Whatsapp extends TwofactorGatewayToken {
 		protected TokenService $tokenService,
 	) {
 		// TRANSLATORS Name of possible authenticator method. This signalize that the signer could be identified by WhatsApp
-		$this->setFriendlyName($this->identifyService->getL10n()->t('WhatsApp'));
+		$this->setFriendlyName($this->identifyService->getL10n()->t('WhatsApp token'));
 		parent::__construct(
 			$identifyService,
 			$tokenService,
