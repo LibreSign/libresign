d1cae9d0c75019ba69bc6b64877bce1e186630cb
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-10 16:04:17 -0300

    chore: remove unecessary code
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -6,8 +6,3 @@
 import './testHelpers/jsdomMocks.js'
 import './testHelpers/nextcloudMocks.js'
 import './testHelpers/vueMocks.js'
-
-// Suppress expected error logs from tests that verify error handling
-// These are intentional - tests mock component error handlers and verify they log properly
-import { vi } from 'vitest'
-vi.spyOn(console, 'error').mockImplementation(() => {})
