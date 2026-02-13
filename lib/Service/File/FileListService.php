8de7f3cacb26219dbb663028cf1e86d574f9148c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-13 13:46:06 -0300

    feat: add signersCount field to file list responses
    - Add signersCount to formatSingleFile() output
    - Add signersCount to formatFileWithChildren() output
    - Add signersCount to formatChildFilesResponse() output
    - Provides immediate count without iterating signers array in frontend
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/File/FileListService.php
+++ lib/Service/File/FileListService.php
@@ -256,6 +256,8 @@ class FileListService {
 
 		$file['statusText'] = $this->fileMapper->getTextOfStatus((int)$file['status']);
 
+		$file['signersCount'] = count($file['signers']);
+
 		if (count($file['signers']) > 0) {
 			usort($file['signers'], function ($a, $b) {
 				$orderA = $a['signingOrder'] ?? PHP_INT_MAX;
@@ -540,6 +542,7 @@ class FileListService {
 			'metadata' => $metadata,
 			'signatureFlow' => SignatureFlow::fromNumeric($mainEntity->getSignatureFlow())->value,
 			'signers' => $signers,
+			'signersCount' => count($signers),
 			'requested_by' => [
 				'userId' => $mainEntity->getUserId(),
 				'displayName' => $this->userManager->get($mainEntity->getUserId())?->getDisplayName() ?? $mainEntity->getUserId(),
@@ -785,6 +788,7 @@ class FileListService {
 				'name' => $file->getName(),
 				'status' => $file->getStatus(),
 				'statusText' => $this->fileMapper->getTextOfStatus($file->getStatus()),
+				'signersCount' => count($signers),
 				'file' => $this->urlGenerator->linkToRoute('libresign.page.getPdf', ['uuid' => $file->getUuid()]),
 				'metadata' => $metadata,
 				'signers' => $signersFormatted,
