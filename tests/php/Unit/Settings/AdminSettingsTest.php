c5132ebab9726fb6300935bfaaca23861655d286
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-16 14:17:07 -0300

    fix: align test namespace for Unit/Settings/AdminSettingsTest.php
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Settings/AdminSettingsTest.php
+++ tests/php/Unit/Settings/AdminSettingsTest.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\Libresign\Tests\Unit\Service;
+namespace OCA\Libresign\Tests\Unit\Settings;
 
 use OCA\Libresign\AppInfo\Application;
 use OCA\Libresign\Settings\AdminSettings;
