4dc80c7ecb818e7c234047a8f112284003281a2e
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-16 14:17:06 -0300

    fix: align test namespace for Unit/Controller/AEnvironmentPageAwareControllerTest.php
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Controller/AEnvironmentPageAwareControllerTest.php
+++ tests/php/Unit/Controller/AEnvironmentPageAwareControllerTest.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-namespace OCA\Libresign\Tests\Unit;
+namespace OCA\Libresign\Tests\Unit\Controller;
 
 use OCA\Files_Trashbin\Trash\ITrashManager;
 use OCA\Libresign\AppInfo\Application;
