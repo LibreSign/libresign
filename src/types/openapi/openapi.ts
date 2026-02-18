b9bd1bb80eec39cdda935e00adda64bb3958d351
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-18 13:16:02 -0300

    docs: add docmdpLevel to OpenAPI file schemas
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -1181,6 +1181,8 @@ export type components = {
                 userId: string;
                 displayName: string | null;
             };
+            /** Format: int64 */
+            docmdpLevel: number;
             signatureFlow: number | string;
             signers: components["schemas"]["Signer"][];
             /** Format: int64 */
@@ -1203,6 +1205,8 @@ export type components = {
             /** Format: int64 */
             status: number;
             statusText: string;
+            /** Format: int64 */
+            docmdpLevel: number;
             /** Format: int64 */
             signersCount: number;
             file: string;
@@ -1279,6 +1283,8 @@ export type components = {
             created_at: string;
             signUuid?: string | null;
             metadata: components["schemas"]["ValidateMetadata"];
+            /** Format: int64 */
+            docmdpLevel: number;
             /** @enum {string} */
             signatureFlow: "none" | "parallel" | "ordered_numeric";
             visibleElements: components["schemas"]["VisibleElement"][];
