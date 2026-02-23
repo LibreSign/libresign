e25de0eabf9a9fc8922732bfa4aa291e6fe44462
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-23 16:17:17 -0300

    refactor(views): migrate Validation to composition api (vue 3)
    Migrate Validation.vue to Vue 3 composition API.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 9aac040c84f5eb790ffebda949cf7a662850936d)

--- src/views/Validation.vue
+++ src/views/Validation.vue
@@ -98,7 +98,7 @@ import JSConfetti from 'js-confetti'
 import axios from '@nextcloud/axios'
 import { formatFileSize } from '@nextcloud/files'
 import { loadState } from '@nextcloud/initial-state'
-import { translate as t, translatePlural as n } from '@nextcloud/l10n'
+import { n, t } from '@nextcloud/l10n'
 import Moment from '@nextcloud/moment'
 import { generateUrl, generateOcsUrl } from '@nextcloud/router'
 
@@ -284,7 +284,7 @@ export default {
 			this.validate(this.uuidToValidate)
 		} else if (this.hasInfo && this.document.signers) {
 			this.document.signers.forEach(signer => {
-				this.$set(signer, 'opened', false)
+				signer.opened = false
 			})
 		} else if (this.uuidToValidate.length > 0) {
 			this.validate(this.uuidToValidate)
@@ -342,10 +342,10 @@ export default {
 			return Moment(Date.parse(date)).format('LL LTS')
 		},
 		toggleDetail(signer) {
-			this.$set(signer, 'opened', !signer.opened)
+			signer.opened = !signer.opened
 		},
 		toggleFileDetail(file) {
-			this.$set(file, 'opened', !file.opened)
+			file.opened = !file.opened
 		},
 		getSignerStatus(status) {
 			const statusMap = {
@@ -576,7 +576,7 @@ export default {
 			this.validate(this.uuidToValidate)
 		},
 		toggleState(stateObject, index) {
-			this.$set(stateObject, index, !stateObject[index])
+			stateObject[index] = !stateObject[index]
 		},
 		hasValidationStatus(signer) {
 			return signer.signature_validation
@@ -621,10 +621,10 @@ export default {
 			}
 			this.document = data
 			this.document.signers?.forEach(signer => {
-				this.$set(signer, 'opened', false)
+				signer.opened = false
 			})
 			this.document.files?.forEach(file => {
-				this.$set(file, 'opened', false)
+				file.opened = false
 			})
 			this.hasInfo = true
 			const isSignedStatus = status => Number(status) === FILE_STATUS.SIGNED
