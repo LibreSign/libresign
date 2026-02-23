56a0d36a8bcd4a58bb405d17cbf2ce9b4f18917d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-23 16:17:10 -0300

    test(setup): update test setup for vue 3
    Update src/tests/setup.js to work with Vue 3 and Vitest.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 8744e0c93e80cba8131f3127dca2cd2a2b638c55)

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -3,6 +3,65 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+import { vi } from 'vitest'
+import { createPinia, setActivePinia } from 'pinia'
+
+vi.mock('@vue/test-utils', async (importOriginal) => {
+	const actual = await importOriginal()
+	const wrap = (fn) => (...args) => {
+		if (typeof fn !== 'function') {
+			return fn
+		}
+		const wrapper = fn(...args)
+		if (wrapper && !wrapper.destroy && typeof wrapper.unmount === 'function') {
+			wrapper.destroy = wrapper.unmount
+		}
+		return wrapper
+	}
+
+	return {
+		...actual,
+		mount: wrap(actual.mount),
+		shallowMount: wrap(actual.shallowMount),
+	}
+})
+
+vi.mock('vue-select', () => ({
+	default: {
+		name: 'VueSelect',
+		render() {
+			return null
+		},
+	},
+}))
+
+vi.mock('vue-select/dist/vue-select.es.js', () => ({
+	default: {
+		name: 'VueSelect',
+		render() {
+			return null
+		},
+	},
+}))
+
+vi.mock('@nextcloud/vue/components/NcSelect', () => ({
+	default: {
+		name: 'NcSelect',
+		template: '<div></div>',
+	},
+}))
+
+
+vi.mock('@nextcloud/vue/components/NcRichText', () => ({
+	default: {
+		name: 'NcRichText',
+		template: '<div></div>',
+	},
+}))
+
+
+setActivePinia(createPinia())
+
 import './testHelpers/jsdomMocks.js'
 import './testHelpers/nextcloudMocks.js'
 import './testHelpers/vueMocks.js'
