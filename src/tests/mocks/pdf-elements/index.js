806c20422ad0970e86c21cc2c8d3624dc09a54d9
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-23 16:17:17 -0300

    test(mocks): update test mocks for vue 3
    Update test mock files for Vue 3 compatibility and Vitest.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 780cc43ef714ae19e907e343ab69e7e0e39f31ef)

---
+++ src/tests/mocks/pdf-elements/index.js
@@ -0,0 +1,4 @@
+export { default as PDFElements } from './src/components/PDFElements.vue'
+export { setWorkerPath } from './src/utils/asyncReader.js'
+
+export default {}
---
+++ src/tests/mocks/pdf-elements/src/components/PDFElements.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="pdf-elements-mock"></div>
+</template>
+
+<script>
+export default {
+	name: 'PDFElements',
+}
+</script>
