34f3ac49be2df8bbad2714bba89b947b3fb7ae68
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-24 17:55:54 -0300

    refactor: update app init typing
    Update initialization typing to satisfy strict TypeScript checks.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/init.ts
+++ src/init.ts
@@ -5,15 +5,22 @@
 
 import axios from '@nextcloud/axios'
 import { addNewFileMenuEntry, Permission, getSidebar } from '@nextcloud/files'
+import type { NewMenuEntry, IFolder, INode } from '@nextcloud/files'
 import { registerDavProperty } from '@nextcloud/files/dav'
 import { n, t } from '@nextcloud/l10n'
 import { generateOcsUrl } from '@nextcloud/router'
 import { getUploader } from '@nextcloud/upload'
+import type { Uploader } from '@nextcloud/upload'
 
 import logger from './logger'
 import LibreSignLogoSvg from '../img/app-colored.svg?raw'
 import LibreSignLogoDarkSvg from '../img/app-dark.svg?raw'
 import { useIsDarkTheme } from './helpers/useIsDarkTheme'
+
+// Extend NewMenuEntry to include uploadManager
+interface ExtendedNewMenuEntry extends NewMenuEntry {
+	uploadManager?: Uploader
+}
 
 registerDavProperty('nc:libresign-signature-status', { nc: 'http://nextcloud.org/ns' })
 registerDavProperty('nc:libresign-signed-node-id', { nc: 'http://nextcloud.org/ns' })
@@ -24,10 +31,10 @@ addNewFileMenuEntry({
 	iconSvgInline: useIsDarkTheme() ? LibreSignLogoDarkSvg : LibreSignLogoSvg,
 	uploadManager: getUploader(),
 	order: 1,
-	enabled() {
-		return Permission.CREATE !== 0
+	enabled(context: IFolder) {
+		return (context.permissions & Permission.CREATE) !== 0
 	},
-	async handler(context, content) {
+	async handler(this: ExtendedNewMenuEntry, context: IFolder, content: INode[]) {
 		const input = document.createElement('input')
 		input.accept = 'application/pdf'
 		input.type = 'file'
@@ -38,7 +45,7 @@ addNewFileMenuEntry({
 				return
 			}
 
-			this.uploadManager.addNotifier(async (upload: any) => {
+			this.uploadManager?.addNotifier(async (upload: any) => {
 				const path = context.path + '/' + upload.file.name
 				await axios.post(generateOcsUrl('/apps/libresign/api/v1/file'), {
 					file: {
@@ -48,16 +55,16 @@ addNewFileMenuEntry({
 				})
 					.then(async ({ data }) => {
 						const sidebar = getSidebar()
-						sidebar.open({ path }, 'libresign')
+					sidebar.open({ path } as INode, 'libresign')
 						sidebar.setActiveTab('libresign')
 					})
 					.catch((error) => logger.error('Error uploading file:', error))
 			})
 			this.uploadManager
-				.upload(file.name, file)
+			?.upload(file.name, file)
 				.catch((error) => logger.debug('Error while uploading', { error }))
 		}
 
 		input.click()
 	},
-})
+} as ExtendedNewMenuEntry)
