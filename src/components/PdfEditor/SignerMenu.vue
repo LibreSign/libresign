08c25be852b3059aebb82c3b22d8a850c09777aa
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-24 11:12:03 -0300

    fix(signer-menu): remove unused t import and method
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/components/PdfEditor/SignerMenu.vue
+++ src/components/PdfEditor/SignerMenu.vue
@@ -38,8 +38,6 @@
 </template>
 
 <script>
-import { t } from '@nextcloud/l10n'
-
 import NcActionButton from '@nextcloud/vue/components/NcActionButton'
 import NcActions from '@nextcloud/vue/components/NcActions'
 import NcAvatar from '@nextcloud/vue/components/NcAvatar'
@@ -79,7 +77,6 @@ export default {
 		}
 	},
 	methods: {
-		t,
 		label(signer) {
 			if (this.getSignerLabel) {
 				return this.getSignerLabel(signer)
