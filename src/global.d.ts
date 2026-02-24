c6b6438661ee2671f024f75f2013606fe0a1cf88
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-24 18:40:53 -0300

    refactor: tighten typing in global.d.ts
    Replace broad any usage with safer TypeScript types in src/global.d.ts, preserving existing behavior.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/global.d.ts
+++ src/global.d.ts
@@ -6,15 +6,39 @@
 /// <reference types="@nextcloud/typings" />
 
 declare global {
+	interface LibreSignGlobalNamespace {
+		fileInfo?: unknown
+		pendingEnvelope?: {
+			nodeType?: string
+			files?: Array<{
+				fileId?: number | string
+				[key: string]: unknown
+			}>
+			filesCount?: number
+			signers?: unknown[]
+			settings?: {
+				path?: string
+				[key: string]: unknown
+			}
+			[key: string]: unknown
+		}
+		[key: string]: unknown
+	}
+
+	interface OCAGlobalNamespace {
+		Libresign: LibreSignGlobalNamespace
+		[key: string]: unknown
+	}
+
 	interface Window {
 		// Nextcloud Globals
 		OC: Nextcloud.v29.OC
-		OCA: Record<string, any>
+		OCA: OCAGlobalNamespace
 		OCP: Nextcloud.v29.OCP
 	}
 
 	const OC: Nextcloud.v29.OC
-	const OCA: Record<string, any>
+	const OCA: OCAGlobalNamespace
 }
 
 export {}
