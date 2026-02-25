7eadea150d8082365c6236960d85b1d949d2470f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-25 13:08:29 -0300

    test: migrate SignerDetails.spec.ts propsData to props
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 05dfcb07f08766a4d91f25b9012dccd86603f16e)
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/components/validation/SignerDetails.spec.ts
+++ src/tests/components/validation/SignerDetails.spec.ts
@@ -15,7 +15,7 @@ describe('SignerDetails.vue - Business Logic', () => {
 
 	const createWrapper = (propsData: SignerDetailsProps = {}) => {
 		return shallowMount(SignerDetails, {
-			propsData: {
+			props: {
 				signer: {
 					signed: '2024-06-01T12:00:00Z',
 					displayName: 'Test Signer',
