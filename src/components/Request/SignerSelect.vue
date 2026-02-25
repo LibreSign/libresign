7e84a712b1b7b1a92e15a2dbefca6c43a84b0a04
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-24 22:51:10 -0300

    fix: tighten signer select icon mapping
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/components/Request/SignerSelect.vue
+++ src/components/Request/SignerSelect.vue
@@ -16,6 +16,24 @@
 			:user-select="true"
 			:options="options"
 			@search="asyncFind">
+			<template #option="slotProps">
+				<div class="account-or-email__option">
+					<NcAvatar :display-name="getOptionLabel(slotProps)"
+						:size="28"
+						:is-no-user="true" />
+					<div class="account-or-email__option__text">
+						<div class="account-or-email__option__title">{{ getOptionLabel(slotProps) }}</div>
+						<div v-if="getOptionSubname(slotProps)"
+							class="account-or-email__option__subtitle">
+							{{ getOptionSubname(slotProps) }}
+						</div>
+					</div>
+					<NcIconSvgWrapper v-if="getOptionIcon(slotProps)"
+						class="account-or-email__option__type"
+						:svg="getOptionIcon(slotProps)"
+						:size="18" />
+				</div>
+			</template>
 			<template #no-options="{ search }">
 				{{ search ? noResultText : t('libresign', 'No recommendations. Start typing.') }}
 			</template>
@@ -33,26 +51,42 @@ import { t } from '@nextcloud/l10n'
 import {
 	mdiAlertCircle,
 } from '@mdi/js'
+import svgAccount from '@mdi/svg/svg/account.svg?raw'
+import svgEmail from '@mdi/svg/svg/email.svg?raw'
 import svgSms from '@mdi/svg/svg/message-processing.svg?raw'
 import svgWhatsapp from '@mdi/svg/svg/whatsapp.svg?raw'
 import svgXmpp from '@mdi/svg/svg/xmpp.svg?raw'
 import debounce from 'debounce'
 import axios from '@nextcloud/axios'
 import { generateOcsUrl } from '@nextcloud/router'
+import NcAvatar from '@nextcloud/vue/components/NcAvatar'
 import NcSelect from '@nextcloud/vue/components/NcSelect'
 import NcIconSvgWrapper from '@nextcloud/vue/components/NcIconSvgWrapper'
 import svgSignal from '../../../img/logo-signal-app.svg?raw'
 import svgTelegram from '../../../img/logo-telegram-app.svg?raw'
 const iconMap = {
+	svgAccount,
+	svgEmail,
 	svgSignal,
 	svgSms,
 	svgTelegram,
 	svgWhatsapp,
 	svgXmpp,
 }
+
+const apiIconToKey = {
+	'icon-user': 'svgAccount',
+	'icon-mail': 'svgEmail',
+	'icon-sms': 'svgSms',
+	'icon-whatsapp': 'svgWhatsapp',
+	'icon-signal': 'svgSignal',
+	'icon-telegram': 'svgTelegram',
+	'icon-xmpp': 'svgXmpp',
+}
 export default {
 	name: 'SignerSelect',
 	components: {
+		NcAvatar,
 		NcSelect,
 		NcIconSvgWrapper,
 	},
@@ -134,15 +168,41 @@ export default {
 		}, 500),
 		injectIcons(items) {
 			return items.map(item => {
-				const icon = item.iconSvg ? iconMap[item.iconSvg] : undefined
+				const { iconSvg: _iconSvg, ...safeItem } = item
+				const iconFromApi = item.icon ? iconMap[apiIconToKey[item.icon]] : undefined
+				const iconFromSvgKey = item.iconSvg ? iconMap[item.iconSvg] : undefined
+				const icon = iconFromApi || iconFromSvgKey
 				return {
-					...item,
+					...safeItem,
 					...(icon ? { iconSvg: icon } : {}),
 					label: item.label ?? item.displayName ?? item.id ?? item.subname ?? '',
 					displayName: item.displayName ?? '',
 					subname: item.subname ?? '',
 				}
 			})
+		},
+		getOption(slotProps) {
+			if (slotProps?.option) {
+				return slotProps.option
+			}
+			if (slotProps && typeof slotProps === 'object') {
+				return slotProps
+			}
+			return {}
+		},
+		getOptionLabel(slotProps) {
+			const option = this.getOption(slotProps)
+			return option.displayName || option.label || option.id || option.subname || ''
+		},
+		getOptionSubname(slotProps) {
+			const option = this.getOption(slotProps)
+			if (!option.subname || option.subname === option.displayName) {
+				return ''
+			}
+			return option.subname
+		},
+		getOptionIcon(slotProps) {
+			return this.getOption(slotProps).iconSvg || ''
 		},
 		focusInput() {
 			if (this.selectedSigner) {
@@ -202,6 +262,37 @@ export default {
 		}
 		&--error {
 			color: var(--color-error);
+		}
+	}
+
+	&__option {
+		display: flex;
+		align-items: center;
+		gap: 8px;
+		width: 100%;
+
+		&__text {
+			display: flex;
+			flex-direction: column;
+			min-width: 0;
+			flex: 1;
+		}
+
+		&__title,
+		&__subtitle {
+			overflow: hidden;
+			text-overflow: ellipsis;
+			white-space: nowrap;
+		}
+
+		&__subtitle {
+			opacity: 0.7;
+			font-size: 12px;
+		}
+
+		&__type {
+			flex-shrink: 0;
+			opacity: 0.8;
 		}
 	}
 }
