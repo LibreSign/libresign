473cc11bf99d3dc7f8fba5c858e5f0e25078c92c
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-02-25 13:08:51 -0300

    style: fix Signers.vue prop closing bracket alignment
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/components/Signers/Signers.vue
+++ src/components/Signers/Signers.vue
@@ -73,7 +73,7 @@ export default {
 				const file = this.filesStore.getFile()
 				file.signers = value
 			},
-	},
+		},
 		isOrderedNumeric() {
 			const file = this.filesStore.getFile()
 			let flow = file?.signatureFlow
