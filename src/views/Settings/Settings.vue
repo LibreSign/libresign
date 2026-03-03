6af1a35b382443e9a47380c2bc39dbeea5edd711
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-02 21:50:38 -0300

    fix: remove duplciated import
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -37,7 +37,6 @@ import { t } from '@nextcloud/l10n'
 import AllowedGroups from './AllowedGroups.vue'
 import CertificateEngine from './CertificateEngine.vue'
 import SignatureEngine from './SignatureEngine.vue'
-import DownloadBinaries from './DownloadBinaries.vue'
 import ConfigureCheck from './ConfigureCheck.vue'
 import DefaultUserFolder from './DefaultUserFolder.vue'
 import DocMDP from './DocMDP.vue'
@@ -64,7 +63,6 @@ export default {
 		AllowedGroups,
 		CertificateEngine,
 		SignatureEngine,
-		DownloadBinaries,
 		ConfigureCheck,
 		DefaultUserFolder,
 		DocMDP,
