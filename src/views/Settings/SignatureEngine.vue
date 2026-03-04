aae6dd4baa48f44c012b0fac44281ff50a5c20bc
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-04 14:22:22 -0300

    fix: add SPDX header
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/views/Settings/SignatureEngine.vue
+++ src/views/Settings/SignatureEngine.vue
@@ -1,3 +1,7 @@
+<!--
+  - SPDX-FileCopyrightText: 2026 LibreCode coop and LibreCode contributors
+  - SPDX-License-Identifier: AGPL-3.0-or-later
+-->
 <template>
 	<NcSettingsSection
 		:name="t('libresign', 'Signature engine')"
