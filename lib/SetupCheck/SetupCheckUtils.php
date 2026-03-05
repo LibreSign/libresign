bc674fa4919512de5d6ae2815800aa2d6bd18520
Guilherme Carvalho <dev.gcarvalho@gmail.com>
2026-03-05 19:31:00 +0000

    style(setup-check): apply code style fixes and improve Psalm annotations
    This commit applies automated code style fixes using `composer cs:fix` and
    addresses issues identified by Psalm static analysis across the SetupCheck
    classes and their related tests.
    
    The changes include:
    - Normalizing indentation (tabs vs spaces) and adding missing trailing commas in argument lists across all SetupCheck classes and `SetupCheckUtils.php`.
    - Fixing the formatting of the `@deprecated` comment in `ConfigureCheckService.php`.
    - Adding `#[\Override]` attributes to all methods implementing `ISetupCheck`.
    
    Related issue: #6590
    
    Type: Maintenance
    
    Checklist:
    - [x] Run `composer cs:fix` to apply coding standards
    - [x] Address Psalm static analysis findings
    - [x] Add `#[\Override]` attributes to interface methods
    - [x] Fix deprecation comment formatting
    
    Signed-off-by: Guilherme Carvalho <dev.gcarvalho@gmail.com>

--- lib/SetupCheck/SetupCheckUtils.php
+++ lib/SetupCheck/SetupCheckUtils.php
@@ -9,66 +9,63 @@ declare(strict_types=1);
 namespace OCA\Libresign\SetupCheck;
 
 use OCA\Libresign\Service\Install\SignSetupService;
-use OCP\IURLGenerator;
 use OCP\App\IAppManager;
-use Psr\Log\LoggerInterface;
 use OCP\IL10N;
+use OCP\IURLGenerator;
+use Psr\Log\LoggerInterface;
 
-trait SetupCheckUtils
-{
-  private SignSetupService $signSetupService;
-  private IURLGenerator $urlGenerator;
-  private IAppManager $appManager;
-  private LoggerInterface $logger;
+trait SetupCheckUtils {
+	private SignSetupService $signSetupService;
+	private IURLGenerator $urlGenerator;
+	private IAppManager $appManager;
+	private LoggerInterface $logger;
 
-  private function verifyResourceIntegrity(string $resource, bool $debugEnabled): array
-  {
-    $this->signSetupService->willUseLocalCert($debugEnabled);
-    $result = $this->signSetupService->verify(php_uname('m'), $resource);
-    if (count($result) === 1 && $debugEnabled) {
-      if (isset($result['SIGNATURE_DATA_NOT_FOUND']) || isset($result['EMPTY_SIGNATURE_DATA'])) {
-        return [];
-      }
-    }
-    return $result;
-  }
+	private function verifyResourceIntegrity(string $resource, bool $debugEnabled): array {
+		$this->signSetupService->willUseLocalCert($debugEnabled);
+		$result = $this->signSetupService->verify(php_uname('m'), $resource);
+		if (count($result) === 1 && $debugEnabled) {
+			if (isset($result['SIGNATURE_DATA_NOT_FOUND']) || isset($result['EMPTY_SIGNATURE_DATA'])) {
+				return [];
+			}
+		}
+		return $result;
+	}
 
-  private function getErrorAndTipFromVerify(array $result, string $resource, bool $debugEnabled, IL10N $l10n): array
-  {
-    if (count($result) === 1 && !$debugEnabled) {
-      if (isset($result['SIGNATURE_DATA_NOT_FOUND'])) {
-        return [
-          $l10n->t('Signature data not found.'),
-          $l10n->t("Sounds that you are running from source code of LibreSign.\nEnable debug mode by: occ config:system:set debug --value true --type boolean"),
-        ];
-      }
-      if (isset($result['EMPTY_SIGNATURE_DATA'])) {
-        return [
-          $l10n->t('Your signature data is empty.'),
-          $l10n->t("Sounds that you are running from source code of LibreSign.\nEnable debug mode by: occ config:system:set debug --value true --type boolean"),
-        ];
-      }
-    }
-    if (isset($result['HASH_FILE_ERROR'])) {
-      if ($debugEnabled) {
-        return [
-          $l10n->t('Invalid hash of binaries files.'),
-          $l10n->t('Debug mode is enabled at your config.php and your LibreSign app was signed using a production signature. If you are not working at development of LibreSign, disable your debug mode or run the command: occ libresign install --%s --use-local-cert', [$resource]),
-        ];
-      }
-    }
-    $this->logger->error('Invalid hash of binaries files', ['result' => $result]);
-    if ($this->appManager->isEnabledForUser('logreader')) {
-      return [
-        $l10n->t('Invalid hash of binaries files.'),
-        $l10n->t('Check your nextcloud.log file on %s and run occ libresign:install --all', [
-          $this->urlGenerator->linkToRouteAbsolute('settings.adminsettings.form', ['section' => 'logging'])
-        ]),
-      ];
-    }
-    return [
-      $l10n->t('Invalid hash of binaries files.'),
-      $l10n->t('Check your nextcloud.log file and run occ libresign:install --all'),
-    ];
-  }
+	private function getErrorAndTipFromVerify(array $result, string $resource, bool $debugEnabled, IL10N $l10n): array {
+		if (count($result) === 1 && !$debugEnabled) {
+			if (isset($result['SIGNATURE_DATA_NOT_FOUND'])) {
+				return [
+					$l10n->t('Signature data not found.'),
+					$l10n->t("Sounds that you are running from source code of LibreSign.\nEnable debug mode by: occ config:system:set debug --value true --type boolean"),
+				];
+			}
+			if (isset($result['EMPTY_SIGNATURE_DATA'])) {
+				return [
+					$l10n->t('Your signature data is empty.'),
+					$l10n->t("Sounds that you are running from source code of LibreSign.\nEnable debug mode by: occ config:system:set debug --value true --type boolean"),
+				];
+			}
+		}
+		if (isset($result['HASH_FILE_ERROR'])) {
+			if ($debugEnabled) {
+				return [
+					$l10n->t('Invalid hash of binaries files.'),
+					$l10n->t('Debug mode is enabled at your config.php and your LibreSign app was signed using a production signature. If you are not working at development of LibreSign, disable your debug mode or run the command: occ libresign install --%s --use-local-cert', [$resource]),
+				];
+			}
+		}
+		$this->logger->error('Invalid hash of binaries files', ['result' => $result]);
+		if ($this->appManager->isEnabledForUser('logreader')) {
+			return [
+				$l10n->t('Invalid hash of binaries files.'),
+				$l10n->t('Check your nextcloud.log file on %s and run occ libresign:install --all', [
+					$this->urlGenerator->linkToRouteAbsolute('settings.adminsettings.form', ['section' => 'logging'])
+				]),
+			];
+		}
+		return [
+			$l10n->t('Invalid hash of binaries files.'),
+			$l10n->t('Check your nextcloud.log file and run occ libresign:install --all'),
+		];
+	}
 }
