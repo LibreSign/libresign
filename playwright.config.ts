f3990836467bcb339182f334544ddc44f0621bbe
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-06 12:26:44 -0300

    chore: improve playwright CI log feedback
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
 	/* Opt out of parallel tests on CI. */
 	workers: process.env.CI ? 1 : undefined,
 	/* Reporter to use. See https://playwright.dev/docs/test-reporters */
-	reporter: process.env.CI ? 'github' : 'list',
+	reporter: process.env.CI ? [['list'], ['github']] : 'list',
 	/* Default timeout for each test (60 seconds) */
 	timeout: 60000,
 
