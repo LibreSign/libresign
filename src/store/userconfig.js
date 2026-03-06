e92ac7d8acc6356d505a064393ee915402bf40ae
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-05 23:27:02 -0300

    refactor(store): migrate userconfig to setup store
    Migrate user configuration store to setup syntax with same persistence/update API.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/store/userconfig.js
+++ src/store/userconfig.js
@@ -4,24 +4,30 @@
  */
 
 import { defineStore } from 'pinia'
+import { reactive, toRefs } from 'vue'
 
 import axios from '@nextcloud/axios'
 import { loadState } from '@nextcloud/initial-state'
 import { generateOcsUrl } from '@nextcloud/router'
 
-export const useUserConfigStore = defineStore('userconfig', {
-	state: () => loadState('libresign', 'config', {}),
-	actions: {
-		onUpdate(key, value) {
-			this[key] = value
-		},
+export const useUserConfigStore = defineStore('userconfig', () => {
+	const config = reactive(loadState('libresign', 'config', {}))
 
-		async update(key, value) {
-			this.onUpdate(key, value)
+	const onUpdate = (key, value) => {
+		config[key] = value
+	}
 
-			await axios.put(generateOcsUrl('/apps/libresign/api/v1/account/config/{key}', { key }), {
-				value,
-			})
-		},
-	},
+	const update = async (key, value) => {
+		onUpdate(key, value)
+
+		await axios.put(generateOcsUrl('/apps/libresign/api/v1/account/config/{key}', { key }), {
+			value,
+		})
+	}
+
+	return {
+		...toRefs(config),
+		onUpdate,
+		update,
+	}
 })
