104197673bfd6811ffeec20cfd2b6e6f06e085d2
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-06 15:17:45 -0300

    refactor(vue3): migrate UploadCertificate to script setup ts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/views/UploadCertificate.vue
+++ src/views/UploadCertificate.vue
@@ -24,8 +24,9 @@
 	</NcDialog>
 </template>
 
-<script>
+<script setup lang="ts">
 import { t } from '@nextcloud/l10n'
+import { computed, ref } from 'vue'
 
 import axios from '@nextcloud/axios'
 import { showError, showSuccess } from '@nextcloud/dialogs'
@@ -37,97 +38,105 @@ import NcNoteCard from '@nextcloud/vue/components/NcNoteCard'
 
 import { useSignMethodsStore } from '../store/signMethods.js'
 
-export default {
+defineOptions({
 	name: 'UploadCertificate',
-	emits: ['certificate:uploaded'],
-	components: {
-		NcButton,
-		NcDialog,
-		NcNoteCard,
-	},
-	props: {
-		useModal: {
-			type: Boolean,
-			default: true,
-		},
-		errors: {
-			type: Array,
-			default: () => [],
-		},
-	},
-	setup() {
-		const signMethodsStore = useSignMethodsStore()
-		return { signMethodsStore }
-	},
-	data() {
-		return {
-			localErrors: [],
-		}
-	},
-	computed: {
-		showModal() {
-			return this.useModal && this.signMethodsStore.modal.uploadCertificate
-		},
-		displayErrors() {
-			return [...this.errors, ...this.localErrors]
-		},
-	},
-	methods: {
-		t,
-		closeDialog() {
-			this.localErrors = []
-			if (this.useModal) {
-				this.signMethodsStore.closeModal('uploadCertificate')
-			}
-		},
-		triggerUpload() {
-			const input = document.createElement('input')
-			input.accept = '.pfx'
-			input.type = 'file'
+})
 
-			input.onchange = async (ev) => {
-				const file = ev.target.files[0]
-
-				if (file) {
-					this.doUpload(file)
-				} else if (this.useModal) {
-					this.signMethodsStore.closeModal('uploadCertificate')
-				}
-
-				input.remove()
-			}
-
-			input.oncancel = () => {
-				if (this.useModal) {
-					this.signMethodsStore.closeModal('uploadCertificate')
-				}
-				input.remove()
-			}
-
-			input.click()
-		},
-		async doUpload(file) {
-			const formData = new FormData()
-			formData.append('file', file)
-			await axios.post(generateOcsUrl('/apps/libresign/api/v1/account/pfx'), formData)
-				.then(({ data }) => {
-					showSuccess(data.ocs.data.message)
-					this.signMethodsStore.setHasSignatureFile(true)
-					this.localErrors = []
-					if (this.useModal) {
-						this.signMethodsStore.closeModal('uploadCertificate')
-					}
-					this.$emit('certificate:uploaded')
-				})
-				.catch(({ response }) => {
-					if (response?.data?.ocs?.data?.message) {
-						showError(response.data.ocs.data.message)
-					}
-					if (response?.data?.ocs?.data?.errors) {
-						this.errors = response.data.ocs.data.errors
-					}
-				})
-		},
-	},
+type UploadError = {
+	title?: string
+	message: string
 }
+
+type SignMethodsStore = {
+	modal: {
+		uploadCertificate: boolean
+	}
+	closeModal: (modalName: string) => void
+	setHasSignatureFile: (value: boolean) => void
+}
+
+const props = withDefaults(defineProps<{
+	useModal?: boolean
+	errors?: UploadError[]
+}>(), {
+	useModal: true,
+	errors: () => [],
+})
+
+const emit = defineEmits<{
+	'certificate:uploaded': []
+}>()
+
+const signMethodsStore = useSignMethodsStore() as SignMethodsStore
+const localErrors = ref<UploadError[]>([])
+
+const showModal = computed(() => props.useModal && signMethodsStore.modal.uploadCertificate)
+const displayErrors = computed(() => [...props.errors, ...localErrors.value])
+
+function closeDialog() {
+	localErrors.value = []
+	if (props.useModal) {
+		signMethodsStore.closeModal('uploadCertificate')
+	}
+}
+
+function triggerUpload() {
+	const input = document.createElement('input')
+	input.accept = '.pfx'
+	input.type = 'file'
+
+	input.onchange = async (event) => {
+		const target = event.target as HTMLInputElement | null
+		const file = target?.files?.[0]
+
+		if (file) {
+			await doUpload(file)
+		} else if (props.useModal) {
+			signMethodsStore.closeModal('uploadCertificate')
+		}
+
+		input.remove()
+	}
+
+	input.oncancel = () => {
+		if (props.useModal) {
+			signMethodsStore.closeModal('uploadCertificate')
+		}
+		input.remove()
+	}
+
+	input.click()
+}
+
+async function doUpload(file: File) {
+	const formData = new FormData()
+	formData.append('file', file)
+
+	try {
+		const { data } = await axios.post(generateOcsUrl('/apps/libresign/api/v1/account/pfx'), formData)
+		showSuccess(data.ocs.data.message)
+		signMethodsStore.setHasSignatureFile(true)
+		localErrors.value = []
+		if (props.useModal) {
+			signMethodsStore.closeModal('uploadCertificate')
+		}
+		emit('certificate:uploaded')
+	} catch (error) {
+		const response = (error as { response?: { data?: { ocs?: { data?: { message?: string, errors?: UploadError[] } } } } }).response
+		if (response?.data?.ocs?.data?.message) {
+			showError(response.data.ocs.data.message)
+		}
+		localErrors.value = response?.data?.ocs?.data?.errors ?? []
+	}
+}
+
+defineExpose({
+	signMethodsStore,
+	localErrors,
+	showModal,
+	displayErrors,
+	closeDialog,
+	triggerUpload,
+	doUpload,
+})
 </script>
