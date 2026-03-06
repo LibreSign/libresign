6d1e5c3cf7c58e643e4e6c6319d0e31d3c077ce1
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-05 23:27:03 -0300

    test(vue3): update FileListFilter test mocks
    Adjust FileListFilter tests for migrated store/composable contracts and complete l10n mock API.
    
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/FilesList/FileListFilter/FileListFilter.spec.ts
+++ src/tests/views/FilesList/FileListFilter/FileListFilter.spec.ts
@@ -12,6 +12,7 @@ import FileListFilter from '../../../../views/FilesList/FileListFilter/FileListF
 
 vi.mock('@nextcloud/l10n', () => ({
 	t: vi.fn((_app: string, text: string) => text),
+	isRTL: vi.fn(() => false),
 }))
 
 vi.mock('@nextcloud/logger', () => ({
