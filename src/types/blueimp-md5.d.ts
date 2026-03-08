8e427f4976e4d51db05fa6b50587b04a55355e46
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-08 02:05:22 -0300

    chore: add SPDX header to blueimp md5 types
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit eb81c71e077097f801c065d5ace6f6baa577f8f5)

--- src/types/blueimp-md5.d.ts
+++ src/types/blueimp-md5.d.ts
@@ -1,3 +1,8 @@
+/**
+ * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 declare module 'blueimp-md5' {
 	const md5: (value: string) => string
 	export default md5
