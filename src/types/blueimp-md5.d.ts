b154d7467e411977172c4a858d7eab9020c97219
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-08 14:11:41 -0300

    refactor(types): remove scattered md5 declarations
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit c210183e5a99f8128716e49accc7c4202bc7de5d)

--- src/types/blueimp-md5.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-declare module 'blueimp-md5' {
-	const md5: (value: string) => string
-	export default md5
-}
