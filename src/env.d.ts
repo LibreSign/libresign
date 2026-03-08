c210183e5a99f8128716e49accc7c4202bc7de5d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-08 14:11:41 -0300

    refactor(types): remove scattered md5 declarations
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/env.d.ts
+++ src/env.d.ts
@@ -17,11 +17,6 @@ declare module '@nextcloud/vue/components/*' {
 	export default component
 }
 
-declare module 'blueimp-md5' {
-	const md5: (value: string) => string
-	export default md5
-}
-
 declare module '@vue/runtime-core' {
 	interface ComponentCustomProperties {
 		$t: typeof import('@nextcloud/l10n').translate
--- src/types/crypto-js-md5.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later
- */
-
-declare module 'crypto-js/md5' {
-	const md5: (value: string) => { toString(): string }
-	export default md5
-}
