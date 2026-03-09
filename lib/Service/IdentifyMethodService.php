3c5f972e5cbacb3532cfe63f1da7289bd75f0106
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-09 16:19:24 -0300

    fix: cs
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit b775fbda0f55ce3b5032860e6f9d4289872d8425)

--- lib/Service/IdentifyMethodService.php
+++ lib/Service/IdentifyMethodService.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
  */
 
 namespace OCA\Libresign\Service;
+
 use OCA\Libresign\Db\IdentifyMethod;
 use OCA\Libresign\Db\IdentifyMethodMapper;
 use OCA\Libresign\Db\SignRequest;
