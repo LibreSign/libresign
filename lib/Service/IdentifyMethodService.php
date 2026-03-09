f1de8ec0fe88bf36f6136b65021865923166682d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-09 16:48:17 -0300

    refactor(openapi): reuse shared identify method setting type
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 19fdad0520e6f4652b169093e63f06f6eb8f837a)

--- lib/Service/IdentifyMethodService.php
+++ lib/Service/IdentifyMethodService.php
@@ -12,6 +12,7 @@ use OCA\Libresign\Db\IdentifyMethod;
 use OCA\Libresign\Db\IdentifyMethodMapper;
 use OCA\Libresign\Db\SignRequest;
 use OCA\Libresign\Exception\LibresignException;
+use OCA\Libresign\ResponseDefinitions;
 use OCA\Libresign\Service\IdentifyMethod\Account;
 use OCA\Libresign\Service\IdentifyMethod\Email;
 use OCA\Libresign\Service\IdentifyMethod\IIdentifyMethod;
@@ -23,6 +24,9 @@ use OCA\Libresign\Service\IdentifyMethod\Xmpp;
 use OCP\IL10N;
 use OCP\IUserManager;
 
+/**
+ * @psalm-import-type LibresignIdentifyMethodSetting from ResponseDefinitions
+ */
 class IdentifyMethodService {
 	public const IDENTIFY_ACCOUNT = 'account';
 	public const IDENTIFY_EMAIL = 'email';
@@ -46,19 +50,7 @@ class IdentifyMethodService {
 	];
 	private bool $isRequest = true;
 	private ?IdentifyMethod $currentIdentifyMethod = null;
-	/**
-	 * @var list<array{
-	 *     name: string,
-	 *     friendly_name: string,
-	 *     enabled: bool,
-	 *     mandatory: bool,
-	 *     signatureMethods?: array{
-	 *         clickToSign?: array{enabled: bool, label: string, name: string},
-	 *         emailToken?: array{blurredEmail: string, hasConfirmCode: bool, hashOfEmail: string, identifyMethod: 'account'|'email', label: string, needCode: bool},
-	 *         password?: array{hasSignatureFile: bool, label: string, name: string}
-	 *     }
-	 * }>
-	 */
+	/** @var list<LibresignIdentifyMethodSetting> */
 	private array $identifyMethodsSettings = [];
 	/**
 	 * @var array<string,array<IIdentifyMethod>>
@@ -337,19 +329,7 @@ class IdentifyMethodService {
 		}
 	}
 
-	/**
-	 * @return list<array{
-	 *     name: string,
-	 *     friendly_name: string,
-	 *     enabled: bool,
-	 *     mandatory: bool,
-	 *     signatureMethods?: array{
-	 *         clickToSign?: array{enabled: bool, label: string, name: string},
-	 *         emailToken?: array{blurredEmail: string, hasConfirmCode: bool, hashOfEmail: string, identifyMethod: 'account'|'email', label: string, needCode: bool},
-	 *         password?: array{hasSignatureFile: bool, label: string, name: string}
-	 *     }
-	 * }>
-	 */
+	/** @return list<LibresignIdentifyMethodSetting> */
 	public function getIdentifyMethodsSettings(): array {
 		if ($this->identifyMethodsSettings) {
 			return $this->identifyMethodsSettings;
