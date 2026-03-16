a214d3709edc4b9a8d8e6e5b5316a97029a16c97
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 15:21:08 -0300

    test: standardize l10n mock in views/Settings/Validation
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 2fcf2b4493d4f0cfabb7ac5b074e34a30a8048fe)
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/Settings/Validation.spec.ts
+++ src/tests/views/Settings/Validation.spec.ts
@@ -4,8 +4,8 @@
  */
 
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { flushPromises, mount } from '@vue/test-utils'
 import { createL10nMock } from '../../testHelpers/l10n.js'
+import { flushPromises, mount } from '@vue/test-utils'
 
 import Validation from '../../../views/Settings/Validation.vue'
 
