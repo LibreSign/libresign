ea48b2898104db9e214ca9928a584dec85b43340
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 15:33:20 -0300

    test: type l10n helper overrides
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/testHelpers/l10n.js
+++ src/tests/testHelpers/l10n.js
@@ -5,6 +5,38 @@
 
 import { vi } from 'vitest'
 
+/**
+ * @typedef {Record<string, any>} L10nParams
+ */
+
+/**
+ * @typedef {(...args: Array<any>) => string} L10nTranslate
+ */
+
+/**
+ * @typedef {(...args: Array<any>) => string} L10nPlural
+ */
+
+/**
+ * @typedef {{
+ * 	t?: L10nTranslate,
+ * 	translate?: L10nTranslate,
+ * 	n?: L10nPlural,
+ * 	translatePlural?: L10nPlural,
+ * 	language?: string,
+ * 	locale?: string,
+ * 	isRTL?: boolean,
+ * }} CreateL10nMockBaseOptions
+ */
+
+/**
+ * @typedef {CreateL10nMockBaseOptions & Record<string, unknown>} CreateL10nMockOptions
+ */
+
+/**
+ * @param {string} message
+ * @param {L10nParams} [params]
+ */
 export const interpolateL10n = (message, params = {}) => {
 	return message.replace(/{(\w+)}/g, (match, key) => {
 		if (Object.prototype.hasOwnProperty.call(params, key)) {
@@ -15,8 +47,10 @@ export const interpolateL10n = (message, params = {}) => {
 	})
 }
 
+/** @type {L10nTranslate} */
 const defaultTranslate = (_app, message, params) => interpolateL10n(message, params)
 
+/** @type {L10nPlural} */
 const defaultPlural = (_app, singular, plural, count, params) => {
 	const message = count === 1 ? singular : plural
 
@@ -26,6 +60,9 @@ const defaultPlural = (_app, singular, plural, count, params) => {
 	})
 }
 
+/**
+ * @param {CreateL10nMockOptions} [options]
+ */
 export const createL10nMock = ({
 	t,
 	translate,
