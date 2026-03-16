06ecb96011b07d9f39de5a202d3ba33bd33d43f4
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 15:21:06 -0300

    test: standardize l10n mock in views/Settings/DefaultUserFolder
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/Settings/DefaultUserFolder.spec.ts
+++ src/tests/views/Settings/DefaultUserFolder.spec.ts
@@ -4,9 +4,9 @@
  */
 
 import { beforeEach, describe, expect, it, vi } from 'vitest'
+import { createL10nMock } from '../../testHelpers/l10n.js'
 import { flushPromises, mount } from '@vue/test-utils'
 import axios from '@nextcloud/axios'
-import { createL10nMock } from '../../testHelpers/l10n.js'
 
 import DefaultUserFolder from '../../../views/Settings/DefaultUserFolder.vue'
 
