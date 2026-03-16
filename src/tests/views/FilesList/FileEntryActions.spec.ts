64396ab9a7dc87497c38653c26f0c31f9d74af29
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 15:05:21 -0300

    test: standardize l10n mock in views/FilesList/FileEntryActions
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/FilesList/FileEntryActions.spec.ts
+++ src/tests/views/FilesList/FileEntryActions.spec.ts
@@ -5,6 +5,7 @@
 
 import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
+import { createL10nMock } from '../../testHelpers/l10n.js'
 
 import FileEntryActions from '../../../views/FilesList/FileEntry/FileEntryActions.vue'
 
@@ -63,9 +64,7 @@ vi.mock('vue-router', () => ({
 	})),
 }))
 
-vi.mock('@nextcloud/l10n', () => ({
-	t: vi.fn((_app: string, text: string) => text),
-}))
+vi.mock('@nextcloud/l10n', () => createL10nMock())
 
 vi.mock('@nextcloud/initial-state', () => ({
 	loadState: vi.fn((_app: string, _key: string, defaultValue: unknown) => defaultValue),
