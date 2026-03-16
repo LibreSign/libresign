d32ba79de6afed46c9014dd8eaef6ebd1c202ed0
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-16 15:21:04 -0300

    test: standardize l10n mock in views/Account/Account
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
    (cherry picked from commit 7aba33edfd9fba3f24e7bf412960a23f20905171)
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/Account/Account.spec.ts
+++ src/tests/views/Account/Account.spec.ts
@@ -4,8 +4,8 @@
  */
 
 import { describe, expect, it, vi } from 'vitest'
-import { mount } from '@vue/test-utils'
 import { createL10nMock } from '../../testHelpers/l10n.js'
+import { mount } from '@vue/test-utils'
 
 import Account from '../../../views/Account/Account.vue'
 
