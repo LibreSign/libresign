264180c30fc430ada4f636632871573eb08f7391
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-17 22:22:40 -0300

    feat: add permission set mapper
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

---
+++ lib/Db/PermissionSetMapper.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * SPDX-FileCopyrightText: 2026 LibreCode coop and contributors
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\Libresign\Db;
+
+use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\DB\QueryBuilder\IQueryBuilder;
+use OCP\ICacheFactory;
+use OCP\IDBConnection;
+
+/**
+ * @template-extends CachedQBMapper<PermissionSet>
+ */
+class PermissionSetMapper extends CachedQBMapper {
+	public function __construct(IDBConnection $db, ICacheFactory $cacheFactory) {
+		parent::__construct($db, $cacheFactory, 'libresign_permission_set');
+	}
+
+	/**
+	 * @throws DoesNotExistException
+	 */
+	public function getById(int $id): PermissionSet {
+		$cached = $this->cacheGet('id:' . $id);
+		if ($cached instanceof PermissionSet) {
+			return $cached;
+		}
+
+		$qb = $this->db->getQueryBuilder();
+		$qb->select('*')
+			->from($this->getTableName())
+			->where($qb->expr()->eq('id', $qb->createNamedParameter($id, IQueryBuilder::PARAM_INT)));
+
+		/** @var PermissionSet */
+		$entity = $this->findEntity($qb);
+		$this->cacheEntity($entity);
+		return $entity;
+	}
+}
