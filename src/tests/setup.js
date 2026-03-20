51a9d2aca3c12aba3e9fcb9d6421ad90fad4dc5f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-03-20 06:38:40 -0300

    test(tests): cover setup behavior
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/setup.js
+++ src/tests/setup.js
@@ -56,6 +56,13 @@ vi.mock('@nextcloud/vue/components/NcSelect', () => ({
 	},
 }))
 
+vi.mock('@nextcloud/vue/components/NcSelectUsers', () => ({
+	default: {
+		name: 'NcSelectUsers',
+		template: '<div></div>',
+	},
+}))
+
 
 vi.mock('@nextcloud/vue/components/NcRichText', () => ({
 	default: {
