e6635f61b48028b8f3f7e9b740f61b6ad75ccaec
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-01 21:07:01 -0300

    build: regenerate administration openapi types
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-administration.ts
+++ src/types/openapi/openapi-administration.ts
@@ -2267,20 +2267,6 @@ export interface operations {
                     };
                 };
             };
-            /** @description Internal server error */
-            500: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": {
-                        ocs: {
-                            meta: components["schemas"]["OCSMeta"];
-                            data: components["schemas"]["ErrorResponse"];
-                        };
-                    };
-                };
-            };
         };
     };
     "policy-get-user-policy-for-user": {
@@ -2370,20 +2356,6 @@ export interface operations {
                     };
                 };
             };
-            /** @description Internal server error */
-            500: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": {
-                        ocs: {
-                            meta: components["schemas"]["OCSMeta"];
-                            data: components["schemas"]["ErrorResponse"];
-                        };
-                    };
-                };
-            };
         };
     };
     "policy-clear-user-policy-for-user": {
@@ -2414,20 +2386,6 @@ export interface operations {
                         ocs: {
                             meta: components["schemas"]["OCSMeta"];
                             data: components["schemas"]["UserPolicyWriteResponse"];
-                        };
-                    };
-                };
-            };
-            /** @description Internal server error */
-            500: {
-                headers: {
-                    [name: string]: unknown;
-                };
-                content: {
-                    "application/json": {
-                        ocs: {
-                            meta: components["schemas"]["OCSMeta"];
-                            data: components["schemas"]["ErrorResponse"];
                         };
                     };
                 };
