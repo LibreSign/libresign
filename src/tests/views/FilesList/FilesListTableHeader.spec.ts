465fbb71d22e10b279f70514cb8aeb3d309506c9
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-03 19:12:59 -0300

    test: standardize l10n mock usage in FilesListTableHeader.spec.ts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/FilesList/FilesListTableHeader.spec.ts
+++ src/tests/views/FilesList/FilesListTableHeader.spec.ts
@@ -4,7 +4,6 @@
  */
 
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { createL10nMock } from '../../testHelpers/l10n.js'
 import { mount } from '@vue/test-utils'
 import { createPinia, setActivePinia } from 'pinia'
 
@@ -31,7 +30,7 @@ type FilesListTableHeaderVm = InstanceType<typeof FilesListTableHeader> & {
 	isSomeSelected: boolean
 }
 
-vi.mock('@nextcloud/l10n', () => createL10nMock())
+vi.mock('@nextcloud/l10n', () => globalThis.mockNextcloudL10n())
 
 vi.mock('@nextcloud/event-bus', () => ({
 	emit: vi.fn(),
