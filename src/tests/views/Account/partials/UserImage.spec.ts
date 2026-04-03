37c98084071218f8e057da91e4179f171e4fce07
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-03 19:12:59 -0300

    test: standardize l10n mock usage in UserImage.spec.ts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/Account/partials/UserImage.spec.ts
+++ src/tests/views/Account/partials/UserImage.spec.ts
@@ -4,12 +4,11 @@
  */
 
 import { describe, expect, it, vi } from 'vitest'
-import { createL10nMock } from '../../../testHelpers/l10n.js'
 import { mount } from '@vue/test-utils'
 
 import UserImage from '../../../../views/Account/partials/UserImage.vue'
 
-vi.mock('@nextcloud/l10n', () => createL10nMock())
+vi.mock('@nextcloud/l10n', () => globalThis.mockNextcloudL10n())
 
 describe('UserImage.vue', () => {
 	it('renders the profile picture heading and passes the user data to the avatar', () => {
