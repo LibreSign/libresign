ffa27aba7882a6249611ce65f52e994fd958ea71
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-03 19:13:00 -0300

    test: standardize l10n mock usage in RenewEmail.spec.ts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/RenewEmail.spec.ts
+++ src/tests/views/RenewEmail.spec.ts
@@ -4,7 +4,6 @@
  */
 
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { createL10nMock } from '../testHelpers/l10n.js'
 import { mount } from '@vue/test-utils'
 import axios from '@nextcloud/axios'
 
@@ -21,7 +20,7 @@ const loadStateMock = vi.fn((_: string, key: string) => {
 	return values[key]
 })
 
-vi.mock('@nextcloud/l10n', () => createL10nMock())
+vi.mock('@nextcloud/l10n', () => globalThis.mockNextcloudL10n())
 
 vi.mock('@nextcloud/axios', () => ({
 	default: {
