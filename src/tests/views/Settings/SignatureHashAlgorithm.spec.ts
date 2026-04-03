cf805b0db6657b84ab815cb625d36cf114aff028
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-03 19:13:01 -0300

    test: standardize l10n mock usage in SignatureHashAlgorithm.spec.ts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/tests/views/Settings/SignatureHashAlgorithm.spec.ts
+++ src/tests/views/Settings/SignatureHashAlgorithm.spec.ts
@@ -4,7 +4,6 @@
  */
 
 import { beforeEach, describe, expect, it, vi } from 'vitest'
-import { createL10nMock } from '../../testHelpers/l10n.js'
 import { flushPromises, mount } from '@vue/test-utils'
 import axios from '@nextcloud/axios'
 
@@ -22,7 +21,7 @@ vi.mock('@nextcloud/password-confirmation', () => ({
 	confirmPassword: (...args: unknown[]) => confirmPasswordMock(...args),
 }))
 
-vi.mock('@nextcloud/l10n', () => createL10nMock())
+vi.mock('@nextcloud/l10n', () => globalThis.mockNextcloudL10n())
 
 vi.mock('@nextcloud/router', () => ({
 	generateOcsUrl: vi.fn((path: string) => path),
