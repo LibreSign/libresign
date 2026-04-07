4e9076a4dca69cf897bd6056a5a772b680afd2a0
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-07 10:14:21 -0300

    test(rector): remove redundant parent calls in CrlUrlParserServiceTest
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Service/Crl/CrlUrlParserServiceTest.php
+++ tests/php/Unit/Service/Crl/CrlUrlParserServiceTest.php
@@ -19,7 +19,6 @@ class CrlUrlParserServiceTest extends TestCase {
 	private CrlUrlParserService $service;
 
 	protected function setUp(): void {
-		parent::setUp();
 		$this->urlGenerator = \OCP\Server::get(IURLGenerator::class);
 		$this->service = new CrlUrlParserService($this->urlGenerator);
 	}
