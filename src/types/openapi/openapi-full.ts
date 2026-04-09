dfd2818cacd7901988d2f194ebf500ff551181db
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-09 15:52:05 -0300

    chore(types): regenerate full openapi types
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-full.ts
+++ src/types/openapi/openapi-full.ts
@@ -2184,7 +2184,7 @@ export type components = {
             effectiveValue: string;
             sourceScope: string;
         };
-        PolicySnapshotIntegerEntry: {
+        PolicySnapshotNumericEntry: {
             /** Format: int64 */
             effectiveValue: number;
             sourceScope: string;
@@ -2451,7 +2451,7 @@ export type components = {
             status_changed_at?: string;
         };
         ValidatePolicySnapshot: {
-            docmdp?: components["schemas"]["PolicySnapshotIntegerEntry"];
+            docmdp?: components["schemas"]["PolicySnapshotNumericEntry"];
             signature_flow?: components["schemas"]["PolicySnapshotEntry"];
         };
         ValidatedChildFile: {
