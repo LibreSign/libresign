bebe0e2f11b050ae9496024fbab108bc4d3e25f0
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-13 23:41:27 -0300

    refactor(request-sign-groups): remove unnecessary context field from definition
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/views/Settings/PolicyWorkbench/settings/realDefinitions.ts
+++ src/views/Settings/PolicyWorkbench/settings/realDefinitions.ts
@@ -4,6 +4,7 @@
  */
 
 import { docMdpRealDefinition } from './docmdp/realDefinition'
+import { requestSignGroupsRealDefinition } from './request-sign-groups/realDefinition'
 import { signatureFooterRealDefinition } from './signature-footer/realDefinition'
 import { signatureFlowRealDefinition } from './signature-flow/realDefinition'
 import type { RealPolicySettingDefinition } from './realTypes'
@@ -12,4 +13,5 @@ export const realDefinitions = {
 	add_footer: signatureFooterRealDefinition,
 	signature_flow: signatureFlowRealDefinition,
 	docmdp: docMdpRealDefinition,
+	groups_request_sign: requestSignGroupsRealDefinition,
 } satisfies Record<string, RealPolicySettingDefinition>
