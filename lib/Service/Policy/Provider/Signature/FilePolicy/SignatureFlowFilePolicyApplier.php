caccacf07ff4bb722b8507fa672c82edd84558c9
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-14 12:30:06 -0300

    refactor: update signature flow file applier contract import
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/Policy/Provider/Signature/FilePolicy/SignatureFlowFilePolicyApplier.php
+++ lib/Service/Policy/Provider/Signature/FilePolicy/SignatureFlowFilePolicyApplier.php
@@ -14,8 +14,8 @@ use OCA\Libresign\Exception\LibresignException;
 use OCA\Libresign\Service\FileService;
 use OCA\Libresign\Service\Policy\Model\ResolvedPolicy;
 use OCA\Libresign\Service\Policy\PolicyService;
-use OCA\Libresign\Service\Policy\Provider\FilePolicy\Contract\IFilePolicyApplier;
 use OCA\Libresign\Service\Policy\Provider\Signature\SignatureFlowPolicy;
+use OCA\Libresign\Service\Policy\Provider\Support\FilePolicy\Contract\IFilePolicyApplier;
 use OCP\IL10N;
 use OCP\IUser;
 
