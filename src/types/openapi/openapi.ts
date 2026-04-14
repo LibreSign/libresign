30c5506d6f9fbb7d5d9f4e49a2afe723dde0d1df
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-14 13:22:43 -0300

    chore: regenerate openapi frontend types
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -4323,6 +4323,20 @@ export interface operations {
                     };
                 };
             };
+            /** @description User-scope not supported */
+            400: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": {
+                        ocs: {
+                            meta: components["schemas"]["OCSMeta"];
+                            data: components["schemas"]["ErrorResponse"];
+                        };
+                    };
+                };
+            };
         };
     };
     "request_signature-request": {
