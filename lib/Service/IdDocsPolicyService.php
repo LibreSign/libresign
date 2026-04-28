e56f54f7e346d208ef0226b7deafcc4f602a129f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-28 20:47:58 -0300

    feat: add approval group policy helpers to id docs service
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/IdDocsPolicyService.php
+++ lib/Service/IdDocsPolicyService.php
@@ -10,17 +10,22 @@ namespace OCA\Libresign\Service;
 
 use OCA\Libresign\Db\IdDocsMapper;
 use OCA\Libresign\Enum\FileStatus;
-use OCA\Libresign\Helper\ValidateHelper;
+use OCA\Libresign\Exception\LibresignException;
 use OCA\Libresign\Service\Policy\PolicyService;
+use OCA\Libresign\Service\Policy\Provider\ApprovalGroups\ApprovalGroupsPolicy;
+use OCA\Libresign\Service\Policy\Provider\ApprovalGroups\ApprovalGroupsPolicyValue;
 use OCA\Libresign\Service\Policy\Provider\IdentificationDocuments\IdentificationDocumentsPolicy;
 use OCA\Libresign\Service\Policy\Provider\IdentificationDocuments\IdentificationDocumentsPolicyValue;
 use OCP\AppFramework\Db\DoesNotExistException;
+use OCP\IGroupManager;
+use OCP\IL10N;
 use OCP\IUser;
 
 class IdDocsPolicyService {
 	public function __construct(
 		private PolicyService $policyService,
-		private ValidateHelper $validateHelper,
+		private IGroupManager $groupManager,
+		private IL10N $l10n,
 		private IdDocsMapper $idDocsMapper,
 	) {
 	}
@@ -29,7 +34,7 @@ class IdDocsPolicyService {
 		if (!$this->isIdentificationDocumentsEnabled($user)) {
 			return false;
 		}
-		if (!$this->validateHelper->userCanApproveValidationDocuments($user, false)) {
+		if (!$this->userCanApproveValidationDocuments($user, false)) {
 			return false;
 		}
 		$readyStatuses = [FileStatus::ABLE_TO_SIGN->value, FileStatus::PARTIAL_SIGNED->value];
@@ -50,5 +55,35 @@ class IdDocsPolicyService {
 			: $this->policyService->resolve(IdentificationDocumentsPolicy::KEY);
 		$value = $resolved->getEffectiveValue();
 		return IdentificationDocumentsPolicyValue::normalize($value, false);
+	}
+
+	public function userCanApproveValidationDocuments(?IUser $user, bool $throw = true): bool {
+		if ($user === null) {
+			return false;
+		}
+
+		$authorized = $this->getApprovalGroups($user);
+		if (empty($authorized)) {
+			$authorized = ApprovalGroupsPolicyValue::DEFAULT_GROUPS;
+		}
+
+		$userGroups = $this->groupManager->getUserGroupIds($user);
+		if (!array_intersect($userGroups, $authorized)) {
+			if ($throw) {
+				throw new LibresignException($this->l10n->t('You are not allowed to approve user profile documents.'));
+			}
+			return false;
+		}
+
+		return true;
+	}
+
+	/** @return list<string> */
+	public function getApprovalGroups(?IUser $user = null): array {
+		$resolved = $user
+			? $this->policyService->resolveForUser(ApprovalGroupsPolicy::KEY, $user)
+			: $this->policyService->resolve(ApprovalGroupsPolicy::KEY);
+
+		return ApprovalGroupsPolicyValue::decode($resolved->getEffectiveValue());
 	}
 }
