1bc729972892e7fbd1782ba5ba05e82693f288ed
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-30 12:58:17 -0300

    feat: update src/views/Settings/Settings.vue
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/views/Settings/Settings.vue
+++ src/views/Settings/Settings.vue
@@ -13,15 +13,8 @@
 		<RootCertificateCfssl />
 		<RootCertificateOpenSsl />
 		<IdentificationFactors />
-		<ExpirationRules />
-		<CrlValidation />
 		<SettingsPolicyWorkbench />
 		<SigningMode v-if="false" />
-		<LegalInformation />
-		<SignatureHashAlgorithm />
-		<DefaultUserFolder />
-		<Envelope />
-		<Reminders />
 		<TSA />
 	</div>
 </template>
@@ -29,19 +22,12 @@
 <script setup lang="ts">
 import CertificateEngine from './CertificateEngine.vue'
 import ConfigureCheck from './ConfigureCheck.vue'
-import CrlValidation from './CrlValidation.vue'
-import DefaultUserFolder from './DefaultUserFolder.vue'
 import DownloadBinaries from './DownloadBinaries.vue'
-import Envelope from './Envelope.vue'
-import ExpirationRules from './ExpirationRules.vue'
 import IdentificationFactors from './IdentificationFactors.vue'
-import LegalInformation from './LegalInformation.vue'
-import Reminders from './Reminders.vue'
+import SettingsPolicyWorkbench from './PolicyWorkbench/RealPolicyWorkbench.vue'
 import RootCertificateCfssl from './RootCertificateCfssl.vue'
 import RootCertificateOpenSsl from './RootCertificateOpenSsl.vue'
 import SignatureEngine from './SignatureEngine.vue'
-import SettingsPolicyWorkbench from './PolicyWorkbench/RealPolicyWorkbench.vue'
-import SignatureHashAlgorithm from './SignatureHashAlgorithm.vue'
 import SigningMode from './SigningMode.vue'
 import SupportProject from './SupportProject.vue'
 import TSA from './TSA.vue'
@@ -51,19 +37,12 @@ defineOptions({
 	components: {
 		CertificateEngine,
 		ConfigureCheck,
-		CrlValidation,
-		DefaultUserFolder,
 		DownloadBinaries,
-		Envelope,
-		ExpirationRules,
 		IdentificationFactors,
-		LegalInformation,
-		Reminders,
 		RootCertificateCfssl,
 		RootCertificateOpenSsl,
 		SettingsPolicyWorkbench,
 		SignatureEngine,
-		SignatureHashAlgorithm,
 		SigningMode,
 		SupportProject,
 		TSA,
