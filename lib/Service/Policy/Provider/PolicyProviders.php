be3cb72de9a7e0aa45255f9b9cca8d227622f90b
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-04-30 12:58:15 -0300

    feat: update lib/Service/Policy/Provider/PolicyProviders.php
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/Policy/Provider/PolicyProviders.php
+++ lib/Service/Policy/Provider/PolicyProviders.php
@@ -10,12 +10,20 @@ namespace OCA\Libresign\Service\Policy\Provider;
 
 use OCA\Libresign\Service\Policy\Provider\ApprovalGroups\ApprovalGroupsPolicy;
 use OCA\Libresign\Service\Policy\Provider\CollectMetadata\CollectMetadataPolicy;
+use OCA\Libresign\Service\Policy\Provider\Confetti\ConfettiPolicy;
+use OCA\Libresign\Service\Policy\Provider\CrlValidation\CrlValidationPolicy;
 use OCA\Libresign\Service\Policy\Provider\DocMdp\DocMdpPolicy;
+use OCA\Libresign\Service\Policy\Provider\Envelope\EnvelopePolicy;
+use OCA\Libresign\Service\Policy\Provider\ExpirationRules\ExpirationRulesPolicy;
 use OCA\Libresign\Service\Policy\Provider\Footer\FooterPolicy;
 use OCA\Libresign\Service\Policy\Provider\IdentificationDocuments\IdentificationDocumentsPolicy;
+use OCA\Libresign\Service\Policy\Provider\DefaultUserFolder\DefaultUserFolderPolicy;
+use OCA\Libresign\Service\Policy\Provider\LegalInformation\LegalInformationPolicy;
+use OCA\Libresign\Service\Policy\Provider\Reminder\ReminderPolicy;
 use OCA\Libresign\Service\Policy\Provider\RequestSignGroups\RequestSignGroupsPolicy;
 use OCA\Libresign\Service\Policy\Provider\Signature\SignatureFlowPolicy;
 use OCA\Libresign\Service\Policy\Provider\SignatureBackground\SignatureBackgroundPolicy;
+use OCA\Libresign\Service\Policy\Provider\SignatureHashAlgorithm\SignatureHashAlgorithmPolicy;
 use OCA\Libresign\Service\Policy\Provider\SignatureText\SignatureTextPolicy;
 use OCA\Libresign\Service\Policy\Provider\ValidationAccess\ValidationAccessPolicy;
 
@@ -24,9 +32,19 @@ final class PolicyProviders {
 	public const BY_KEY = [
 		ApprovalGroupsPolicy::KEY => ApprovalGroupsPolicy::class,
 		CollectMetadataPolicy::KEY => CollectMetadataPolicy::class,
+		ConfettiPolicy::KEY => ConfettiPolicy::class,
+		CrlValidationPolicy::KEY => CrlValidationPolicy::class,
 		FooterPolicy::KEY => FooterPolicy::class,
 		DocMdpPolicy::KEY => DocMdpPolicy::class,
+		EnvelopePolicy::KEY => EnvelopePolicy::class,
+		ExpirationRulesPolicy::KEY_MAXIMUM_VALIDITY => ExpirationRulesPolicy::class,
+		ExpirationRulesPolicy::KEY_RENEWAL_INTERVAL => ExpirationRulesPolicy::class,
+		ExpirationRulesPolicy::KEY_EXPIRY_IN_DAYS => ExpirationRulesPolicy::class,
 		RequestSignGroupsPolicy::KEY => RequestSignGroupsPolicy::class,
+		ReminderPolicy::KEY => ReminderPolicy::class,
+		DefaultUserFolderPolicy::KEY => DefaultUserFolderPolicy::class,
+		LegalInformationPolicy::KEY => LegalInformationPolicy::class,
+		SignatureHashAlgorithmPolicy::KEY => SignatureHashAlgorithmPolicy::class,
 		ValidationAccessPolicy::KEY => ValidationAccessPolicy::class,
 		SignatureFlowPolicy::KEY => SignatureFlowPolicy::class,
 		SignatureBackgroundPolicy::KEY => SignatureBackgroundPolicy::class,
