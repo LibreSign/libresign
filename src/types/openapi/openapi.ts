da6fc1028bbbb4fd8df8882db3e3c2504826f388
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-06 14:58:35 -0300

    fix: remove unknown regressions in empty OCS responses
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi.ts
+++ src/types/openapi/openapi.ts
@@ -2285,7 +2285,7 @@ export interface operations {
                     "application/json": {
                         ocs: {
                             meta: components["schemas"]["OCSMeta"];
-                            data: unknown;
+                            data: Record<string, never>;
                         };
                     };
                 };
@@ -3945,7 +3945,7 @@ export interface operations {
                     "application/json": {
                         ocs: {
                             meta: components["schemas"]["OCSMeta"];
-                            data: unknown;
+                            data: Record<string, never>;
                         };
                     };
                 };
