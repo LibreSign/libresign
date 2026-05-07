3f0653db10bb229c6959b03e59384d5cca1bdad2
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-07 09:47:13 -0300

    test: fix unit regressions after policy migration
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Handler/SignEngine/PhpNativeHandlerTest.php
+++ tests/php/Unit/Handler/SignEngine/PhpNativeHandlerTest.php
@@ -39,6 +39,7 @@ final class PhpNativeHandlerTest extends \OCA\Libresign\Tests\Unit\TestCase {
 
 	#[\Override]
 	public function setUp(): void {
+		parent::setUp();
 		$this->appConfig = $this->getMockAppConfigWithReset();
 		$this->docMdpConfigService = $this->createMock(DocMdpConfigService::class);
 		$this->signatureTextService = $this->createMock(SignatureTextService::class);
@@ -473,6 +474,7 @@ final class PhpNativeHandlerTest extends \OCA\Libresign\Tests\Unit\TestCase {
 			$this->docMdpConfigService,
 			$signatureTextService,
 			$this->signatureBackgroundService,
+			$this->policyService,
 			$this->certificateEngineFactory,
 		);
 
