0d60f6f4c7b35db4836e24ac1268e676081f6a94
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-05-07 17:49:13 -0300

    chore(openapi): refine IdDocsController.php for sdk ergonomics
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Controller/IdDocsController.php
+++ lib/Controller/IdDocsController.php
@@ -152,7 +152,7 @@ class IdDocsController extends AEnvironmentAwareController implements ISignature
 	#[NoCSRFRequired]
 	#[RequireSignRequestUuid(skipIfAuthenticated: true)]
 	#[ApiRoute(verb: 'GET', url: '/api/{apiVersion}/id-docs', requirements: ['apiVersion' => '(v1)'])]
-	public function listOfUnauthenticatedSigner(
+	public function listUnauthenticatedSignerDocuments(
 		?string $userId = null,
 		?int $signRequestId = null,
 		?int $page = null,
@@ -182,6 +182,15 @@ class IdDocsController extends AEnvironmentAwareController implements ISignature
 				Http::STATUS_NOT_FOUND
 			);
 		}
+	}
+
+	public function listOfUnauthenticatedSigner(
+		?string $userId = null,
+		?int $signRequestId = null,
+		?int $page = null,
+		?int $length = null,
+	): DataResponse {
+		return $this->listUnauthenticatedSignerDocuments($userId, $signRequestId, $page, $length);
 	}
 
 	/**
