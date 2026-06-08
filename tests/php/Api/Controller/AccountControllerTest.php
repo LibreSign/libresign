8486b529755252332d9e3c98482f831b445fbedb
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-08 13:22:22 -0300

    test(api): cover account controller updates
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Api/Controller/AccountControllerTest.php
+++ tests/php/Api/Controller/AccountControllerTest.php
@@ -8,7 +8,18 @@ declare(strict_types=1);
 
 namespace OCA\Libresign\Tests\Api\Controller;
 
+use OCA\Libresign\Handler\CertificateEngine\CertificateEngineFactory;
+use OCA\Libresign\Handler\DocMdpHandler;
+use OCA\Libresign\Handler\FooterHandler;
+use OCA\Libresign\Handler\SignEngine\Pkcs12Handler;
+use OCA\Libresign\Service\CaIdentifierService;
+use OCA\Libresign\Service\Crl\CrlService;
+use OCA\Libresign\Service\FolderService;
 use OCA\Libresign\Tests\Api\ApiTestCase;
+use OCP\IAppConfig;
+use OCP\ITempManager;
+use OCP\L10N\IFactory as IL10NFactory;
+use Psr\Log\LoggerInterface;
 
 /**
  * @group DB
@@ -62,5 +73,73 @@ final class AccountControllerTest extends ApiTestCase {
 			]);
 
 		$this->assertRequest();
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testSignatureGenerateRecreatesMissingUserFilesDirectory(): void {
+		$user = $this->createAccount('folderserviceapiuser', 'password');
+		$userFilesDirectory = $user->getHome() . '/files';
+		$this->removeDirectory($userFilesDirectory);
+		$this->overwriteService(Pkcs12Handler::class, $this->getPkcs12HandlerWithStubbedGeneration());
+
+		$this->request
+			->withMethod('POST')
+			->withRequestHeader([
+				'Authorization' => 'Basic ' . base64_encode('folderserviceapiuser:password'),
+				'Content-Type' => 'application/json',
+			])
+			->withRequestBody([
+				'signPassword' => 'secretToSign',
+			])
+			->withPath('/api/v1/account/signature')
+			->assertResponseCode(200);
+
+		$this->assertRequest();
+		self::assertFileExists($userFilesDirectory . '/LibreSign/signature.pfx');
+	}
+
+	private function getPkcs12HandlerWithStubbedGeneration(): Pkcs12Handler {
+		$handler = $this->getMockBuilder(Pkcs12Handler::class)
+			->setConstructorArgs([
+				\OCP\Server::get(FolderService::class),
+				\OCP\Server::get(IAppConfig::class),
+				\OCP\Server::get(CertificateEngineFactory::class),
+				\OCP\Server::get(IL10NFactory::class)->get('libresign'),
+				\OCP\Server::get(FooterHandler::class),
+				\OCP\Server::get(ITempManager::class),
+				\OCP\Server::get(LoggerInterface::class),
+				\OCP\Server::get(CaIdentifierService::class),
+				\OCP\Server::get(DocMdpHandler::class),
+				\OCP\Server::get(CrlService::class),
+			])
+			->onlyMethods(['generateCertificate'])
+			->getMock();
+
+		$handler->method('generateCertificate')->willReturn('test pfx content');
+		return $handler;
+	}
+
+	private function removeDirectory(string $path): void {
+		if (!is_dir($path)) {
+			return;
+		}
+
+		$iterator = new \RecursiveIteratorIterator(
+			new \RecursiveDirectoryIterator($path, \RecursiveDirectoryIterator::SKIP_DOTS),
+			\RecursiveIteratorIterator::CHILD_FIRST,
+		);
+
+		foreach ($iterator as $item) {
+			if ($item->isDir()) {
+				@rmdir($item->getPathname());
+				continue;
+			}
+
+			@unlink($item->getPathname());
+		}
+
+		@rmdir($path);
 	}
 }
