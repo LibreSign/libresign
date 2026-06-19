ded5e6bd1a6ef304e3845fce8ac4075ceb55a1db
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-06-19 18:14:28 -0300

    fix: decouple in-app notification preference checks
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Listener/NotificationListener.php
+++ lib/Listener/NotificationListener.php
@@ -16,6 +16,7 @@ use OCA\Libresign\Events\SendSignNotificationEvent;
 use OCA\Libresign\Events\SignedEvent;
 use OCA\Libresign\Events\SignRequestCanceledEvent;
 use OCA\Libresign\Service\IdentifyMethod\IIdentifyMethod;
+use OCA\Libresign\Service\NotificationPreferenceResolver;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
@@ -34,6 +35,7 @@ class NotificationListener implements IEventListener {
 		private ITimeFactory $timeFactory,
 		protected IURLGenerator $url,
 		private SignRequestMapper $signRequestMapper,
+		private NotificationPreferenceResolver $notificationPreferenceResolver,
 	) {
 	}
 
@@ -169,9 +171,10 @@ class NotificationListener implements IEventListener {
 		if ($identifyMethod->getEntity()->isDeletedAccount()) {
 			return;
 		}
-		$notificationDisabled = $this->isNotificationDisabledAtActivity(
+		$notificationDisabled = $this->notificationPreferenceResolver->isInAppNotificationDisabled(
 			$identifyMethod->getEntity()->getIdentifierValue(),
 			SignRequestCanceledEvent::SIGN_REQUEST_CANCELED,
+			true,
 		);
 		if ($notificationDisabled) {
 			return;
@@ -202,36 +205,6 @@ class NotificationListener implements IEventListener {
 			]);
 
 		$this->notificationManager->notify($notification);
-	}
-
-	private function isNotificationDisabledAtActivity(string $userId, string $type): bool {
-		if (!class_exists(\OCA\Activity\UserSettings::class)) {
-			return false;
-		}
-		$activityUserSettings = \OCP\Server::get(\OCA\Activity\UserSettings::class);
-		if ($activityUserSettings) {
-			$manager = \OCP\Server::get(\OCP\Activity\IManager::class);
-			try {
-				$manager->getSettingById($type);
-			} catch (\Exception) {
-				return false;
-			}
-
-			$adminSetting = $activityUserSettings->getAdminSetting('notification', $type);
-			if (!$adminSetting) {
-				return true;
-			}
-
-			$notificationSetting = $activityUserSettings->getUserSetting(
-				$userId,
-				'notification',
-				$type
-			);
-			if ($notificationSetting === false) {
-				return true;
-			}
-		}
-		return false;
 	}
 
 	/**
