2e3b81d2675a3e5496c6b30b9d4b8b26a672ef93
YvesCesar <yvesamorim73@gmail.com>
2026-06-20 17:22:01 -0400

    refactor(setup-check): use typed result object for formatted checks
    Signed-off-by: YvesCesar <yvesamorim73@gmail.com>

--- lib/Controller/AdminController.php
+++ lib/Controller/AdminController.php
@@ -257,7 +257,9 @@ class AdminController extends AEnvironmentAwareController {
 	#[NoCSRFRequired]
 	#[ApiRoute(verb: 'GET', url: '/api/{apiVersion}/admin/configure-check', requirements: ['apiVersion' => '(v1)'])]
 	public function configureCheck(): DataResponse {
-		return new DataResponse($this->setupCheckResultService->getLegacyFormattedChecks());
+		/** @var LibresignConfigureChecksResponse $checks */
+		$checks = $this->setupCheckResultService->getFormattedChecks();
+		return new DataResponse($checks);
 	}
 
 	/**
@@ -296,7 +298,7 @@ class AdminController extends AEnvironmentAwareController {
 				$this->installService->installCfssl($async);
 			}
 
-			$this->eventSource->send('configure_check', $this->setupCheckResultService->getLegacyFormattedChecks());
+			$this->eventSource->send('configure_check', $this->setupCheckResultService->getFormattedChecks());
 			$seconds = 0;
 			while ($this->installService->isDownloadWip()) {
 				$totalSize = $this->installService->getTotalSize();
@@ -307,7 +309,7 @@ class AdminController extends AEnvironmentAwareController {
 				usleep(200000); // 0.2 seconds
 				$seconds += 0.2;
 				if ($seconds === 5.0) {
-					$this->eventSource->send('configure_check', $this->setupCheckResultService->getLegacyFormattedChecks());
+					$this->eventSource->send('configure_check', $this->setupCheckResultService->getFormattedChecks());
 					$seconds = 0;
 				}
 			}
@@ -321,7 +323,7 @@ class AdminController extends AEnvironmentAwareController {
 			]));
 		}
 
-		$this->eventSource->send('configure_check', $this->setupCheckResultService->getLegacyFormattedChecks());
+		$this->eventSource->send('configure_check', $this->setupCheckResultService->getFormattedChecks());
 		$this->eventSource->send('done', '');
 		$this->eventSource->close();
 		// Nextcloud inject a lot of headers that is incompatible with SSE
