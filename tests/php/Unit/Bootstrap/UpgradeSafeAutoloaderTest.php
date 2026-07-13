395463d93ccb3181d1218ec30ba437285f9a5422
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-07-13 12:35:41 -0300

    test: reproduce stale require-once autoload path
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Bootstrap/UpgradeSafeAutoloaderTest.php
+++ tests/php/Unit/Bootstrap/UpgradeSafeAutoloaderTest.php
@@ -13,6 +13,7 @@ use OCA\Libresign\Bootstrap\UpgradeSafeAutoloader;
 use PHPUnit\Framework\TestCase;
 
 final class UpgradeSafeAutoloaderTest extends TestCase {
+	private const AUTOLOAD_COUNTER_KEY = 'libresign_upgrade_safe_autoloader_boot_count';
 	private const MIGRATION_CLASS = 'OCA\\Libresign\\Migration\\Version99999Date20260713000000';
 	private const NON_MIGRATION_CLASS = 'OCA\\Libresign\\Service\\GeneratedNonMigrationClass';
 	private const MIGRATION_FILE = '/lib/Migration/Version99999Date20260713000000.php';
@@ -50,6 +51,78 @@ PHP,
 			self::assertTrue(class_exists(self::MIGRATION_CLASS));
 		} finally {
 			$loader->unregister();
+			self::removeDirectoryRecursively($appRoot);
+		}
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 */
+	public function testRequireOnceDoesNotReloadComposerAutoloadAfterMigrationFileAppears(): void {
+		$appRoot = sys_get_temp_dir() . '/libresign-upgrade-autoload-' . uniqid('', true);
+		unset($GLOBALS[self::AUTOLOAD_COUNTER_KEY]);
+
+		try {
+			mkdir($appRoot . '/composer', 0755, true);
+			mkdir($appRoot . '/vendor', 0755, true);
+			mkdir($appRoot . '/lib/Migration', 0755, true);
+
+			file_put_contents(
+				$appRoot . '/vendor/autoload.php',
+				"<?php\n"
+				. "declare(strict_types=1);\n"
+				. 'if (!class_exists(\\Composer\\Autoload\\ClassLoader::class, false)) {' . "\n"
+				. '    require_once ' . var_export(__DIR__ . '/../../../../vendor/composer/ClassLoader.php', true) . ';' . "\n"
+				. "}\n"
+				. "static \$loader = null;\n"
+				. "if (\$loader === null) {\n"
+				. "    \$loader = new \\Composer\\Autoload\\ClassLoader(__DIR__);\n"
+				. "    \$loader->addPsr4('OCA\\\\Libresign\\\\', [dirname(__DIR__) . '/lib'], true);\n"
+				. "    \$loader->register(true);\n"
+				. "}\n"
+				. "return \$loader;\n",
+			);
+
+			file_put_contents(
+				$appRoot . '/composer/autoload.php',
+				"<?php\n"
+				. "declare(strict_types=1);\n"
+				. "\$GLOBALS['" . self::AUTOLOAD_COUNTER_KEY . "'] = (\$GLOBALS['" . self::AUTOLOAD_COUNTER_KEY . "'] ?? 0) + 1;\n"
+				. "return require_once __DIR__ . '/../vendor/autoload.php';\n",
+			);
+
+			$loader = require_once $appRoot . '/composer/autoload.php';
+			self::assertInstanceOf(ClassLoader::class, $loader);
+			self::assertSame(1, $GLOBALS[self::AUTOLOAD_COUNTER_KEY]);
+
+			self::assertFalse(class_exists(self::MIGRATION_CLASS));
+
+			file_put_contents(
+				$appRoot . self::MIGRATION_FILE,
+				<<<'PHP'
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Libresign\Migration;
+
+final class Version99999Date20260713000000 {
+}
+PHP,
+			);
+
+			require_once $appRoot . '/composer/autoload.php';
+			self::assertSame(1, $GLOBALS[self::AUTOLOAD_COUNTER_KEY]);
+			self::assertFalse(class_exists(self::MIGRATION_CLASS));
+
+			UpgradeSafeAutoloader::registerCurrentAppLoader($appRoot);
+			self::assertTrue(class_exists(self::MIGRATION_CLASS));
+		} finally {
+			$registeredLoader = ClassLoader::getRegisteredLoaders()[$appRoot . '/vendor'] ?? null;
+			if ($registeredLoader instanceof ClassLoader) {
+				$registeredLoader->unregister();
+			}
+			unset($GLOBALS[self::AUTOLOAD_COUNTER_KEY]);
 			self::removeDirectoryRecursively($appRoot);
 		}
 	}
