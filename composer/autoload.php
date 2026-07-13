b12af534ed5a102788c79d3f478a6d4611ce0520
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-07-13 10:47:22 -0300

    fix: register upgrade-safe autoloader during bootstrap
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- composer/autoload.php
+++ composer/autoload.php
@@ -6,4 +6,10 @@ declare(strict_types=1);
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
-require_once __DIR__ . '/../vendor/autoload.php';
+$loader = require __DIR__ . '/../vendor/autoload.php';
+
+require_once __DIR__ . '/../lib/Bootstrap/UpgradeSafeAutoloader.php';
+
+\OCA\Libresign\Bootstrap\UpgradeSafeAutoloader::register(dirname(__DIR__));
+
+return $loader;
