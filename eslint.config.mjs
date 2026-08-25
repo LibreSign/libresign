f7d1a1aa040f8b972e7bc063897c8a2d7c96b4ed
Luis Amorim <luisfelipeamorim@hotmail.com>
2026-08-25 14:34:21 -0300

    fix(lint): enable no-undef
    Signed-off-by: Luis Amorim <luisfelipeamorim@hotmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -109,7 +109,6 @@ export default [
 			'jsdoc/require-param-description': 'off',
 			'jsdoc/tag-lines': 'off',
 			'jsdoc/valid-types': 'off',
-			'no-undef': 'off',
 			'no-unused-vars': 'off',
 			'no-use-before-define': 'off',
 			'no-useless-assignment': 'off',
