55f060d8bf55e4531330c360f508acc53a83c8a8
Luis Amorim <luisfelipeamorim@hotmail.com>
2026-08-25 14:36:26 -0300

    fix(lint): enable vue/no-template-shadow
    Signed-off-by: Luis Amorim <luisfelipeamorim@hotmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -127,7 +127,6 @@ export default [
 			'vue/multiline-html-element-content-newline': 'off',
 			'vue/new-line-between-multi-line-property': 'off',
 			'vue/no-boolean-default': 'off',
-			'vue/no-template-shadow': 'off',
 			'vue/no-unused-properties': 'off',
 			'vue/no-unused-refs': 'off',
 			'vue/no-v-html': 'off',
