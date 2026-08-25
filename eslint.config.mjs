996514a47933f64de16a4a0ff565f994b811b239
Luis Amorim <luisfelipeamorim@hotmail.com>
2026-08-25 14:39:34 -0300

    fix(lint): enable vue/key-spacing
    Signed-off-by: Luis Amorim <luisfelipeamorim@hotmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -120,7 +120,6 @@ export default [
 			'vue/first-attribute-linebreak': 'off',
 			'vue/html-closing-bracket-newline': 'off',
 			'vue/html-indent': 'off',
-			'vue/key-spacing': 'off',
 			'vue/max-attributes-per-line': 'off',
 			'vue/multi-word-component-names': 'off',
 			'vue/multiline-html-element-content-newline': 'off',
