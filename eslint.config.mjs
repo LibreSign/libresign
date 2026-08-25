ffbee5a571bd72fb9bca948dbcb357db09aad543
Luis Amorim <luisfelipeamorim@hotmail.com>
2026-08-25 14:27:10 -0300

    fix(lint): enable no-useless-escape
    Signed-off-by: Luis Amorim <luisfelipeamorim@hotmail.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -116,7 +116,6 @@ export default [
 			'no-unused-vars': 'off',
 			'no-use-before-define': 'off',
 			'no-useless-assignment': 'off',
-			'no-useless-escape': 'off',
 			'package-json/sort-package-json': 'off',
 			'perfectionist/sort-imports': 'off',
 			'perfectionist/sort-named-imports': 'off',
