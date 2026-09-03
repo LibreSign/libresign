d648a2e600617ed05a564a81ef7a27ddc4dfb39f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-03 11:34:25 -0300

    chore: regenerate OpenAPI validation contracts
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-full.ts
+++ src/types/openapi/openapi-full.ts
@@ -2140,7 +2140,7 @@ export type components = {
             size: number;
             pdfVersion?: string;
             signers: components["schemas"]["SignerSummary"][];
-            file: string;
+            file?: string;
             metadata: components["schemas"]["ValidateMetadata"];
         };
         ValidatedFile: {
