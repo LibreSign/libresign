45dc4a263cb4e2b628237aa8fdb71e23a4b6b15f
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-05 23:25:51 -0300

    fix: import structured validation reasons
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- lib/Service/Signature/PdfSignatureValidationService.php
+++ lib/Service/Signature/PdfSignatureValidationService.php
@@ -11,7 +11,7 @@ namespace OCA\Libresign\Service\Signature;
 use OCA\Libresign\AppInfo\Application;
 use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Exception\UnsignedPdfException;
 use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Model\ExtractedSignature;
-use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Model\TimestampToken;
+use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Model\ValidationReason;
 use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Model\ValidationResult;
 use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Model\ValidationState;
 use OCA\Libresign\Vendor\LibreSign\PdfSignatureValidator\Parser\PdfSignatureValidator;
