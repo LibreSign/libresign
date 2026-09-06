5909287a7918be397f2effd7f73b713220bd2b61
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-06 14:22:43 -0300

    style(stable34): fix JSignPdf handler test formatting
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Handler/SignEngine/JSignPdf/JSignPdfHandlerTest.php
+++ tests/php/Unit/Handler/SignEngine/JSignPdf/JSignPdfHandlerTest.php
@@ -132,7 +132,6 @@ final class JSignPdfHandlerTest extends \OCA\Libresign\Tests\Unit\TestCase {
 		$reflection->setValue($handler, $docMdpConfigService);
 	}
 
-
 	#[DataProvider('providerExtractPdfVersion')]
 	public function testExtractPdfVersion(string $content, ?float $expected): void {
 		if (self::$certificateEngineFactory === null || empty(self::$certificateContent)) {
