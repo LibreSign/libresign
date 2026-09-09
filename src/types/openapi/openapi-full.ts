5e2543b72930137a433d41dd18c7fd47df36e769
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-09 11:40:20 -0300

    chore: update openapi
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-full.ts
+++ src/types/openapi/openapi-full.ts
@@ -2020,6 +2020,7 @@ export type components = {
             nodeId?: number;
             path?: string;
             url?: string;
+            name?: string;
         };
         NewSigner: {
             identifyMethods: {
