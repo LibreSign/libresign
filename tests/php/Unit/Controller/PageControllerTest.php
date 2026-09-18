915b6e7967c7bfe0b34c10b14d9fff08ae5c6d7d
Vitor Mattos <1079143+vitormattos@users.noreply.github.com>
2026-09-18 16:04:02 -0300

    test: cover external validation page template
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Unit/Controller/PageControllerTest.php
+++ tests/php/Unit/Controller/PageControllerTest.php
@@ -23,8 +23,10 @@ use OCA\Libresign\Service\RequestSignatureService;
 use OCA\Libresign\Service\SessionService;
 use OCA\Libresign\Service\SignerElementsService;
 use OCA\Libresign\Service\SignFileService;
+use OCA\Libresign\Middleware\Attribute\RequireSetupOk;
 use OCA\Libresign\Service\Validation\SigningRequestValidator;
 use OCA\Libresign\Tests\Unit\TestCase;
+use OCP\AppFramework\Http\TemplateResponse;
 use OCP\AppFramework\Http\RedirectResponse;
 use OCP\AppFramework\Services\IInitialState;
 use OCP\EventDispatcher\IEventDispatcher;
@@ -227,6 +229,22 @@ final class PageControllerTest extends TestCase {
 		self::assertStringNotContainsString('/apps/libresign/f/', $response->getRedirectURL());
 	}
 
+	public function testValidationUsesExternalTemplate(): void {
+		$response = $this->controller->validation();
+
+		self::assertSame('external', $response->getTemplateName());
+		self::assertSame(TemplateResponse::RENDER_AS_BASE, $response->getRenderAs());
+	}
+
+	public function testValidationRequireSetupOkUsesExternalTemplate(): void {
+		$method = new \ReflectionMethod(PageController::class, 'validation');
+		$attributes = $method->getAttributes(RequireSetupOk::class);
+
+		self::assertCount(1, $attributes);
+		self::assertSame('external', $attributes[0]->newInstance()->getTemplate());
+	}
+
+
 	public function testValidationFilePublicBootstrapsRequesterPoliciesWithoutUserScope(): void {
 		$fileEntity = new FileEntity();
 		$fileEntity->setId(7);
@@ -298,9 +316,11 @@ final class PageControllerTest extends TestCase {
 			urlGenerator: $this->urlGenerator,
 		);
 
-		$controller->validationFilePublic('validation-file-uuid');
+		$response = $controller->validationFilePublic('validation-file-uuid');
 
 		self::assertArrayHasKey('effective_policies', $capturedInitialState);
+		self::assertSame('external', $response->getTemplateName());
+		self::assertSame(TemplateResponse::RENDER_AS_BASE, $response->getRenderAs());
 	}
 
 }
