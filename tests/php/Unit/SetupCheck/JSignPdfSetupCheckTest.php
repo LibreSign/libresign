bf8fb35163519cff980d4a50f1aff52e6bbc776e
Vitor Mattos <vitor@php.rio>
2026-09-20 13:15:21 -0300

    test(setup): validate JSignPdf release version independently

--- tests/php/Unit/SetupCheck/JSignPdfSetupCheckTest.php
+++ tests/php/Unit/SetupCheck/JSignPdfSetupCheckTest.php
@@ -315,7 +315,7 @@ class JSignPdfSetupCheckTest extends TestCase {
 			->disableOriginalConstructor()
 			->onlyMethods(['setParam', 'getVersion'])
 			->getMock();
-		$jsignPdfMock->method('getVersion')->willReturn(InstallService::JSIGNPDF_VERSION);
+		$jsignPdfMock->method('getVersion')->willReturn(JSignPdfRelease::VERSION);
 
 		$jsignParamMock = $this->createJSignParamMock();
 
@@ -326,7 +326,7 @@ class JSignPdfSetupCheckTest extends TestCase {
 
 		$this->assertInstanceOf(SetupResult::class, $result);
 		$this->assertSame('success', $result->getSeverity());
-		$this->assertStringContainsString('JSignPdf version: ' . InstallService::JSIGNPDF_VERSION, $result->getDescription());
+		$this->assertStringContainsString('JSignPdf version: ' . JSignPdfRelease::VERSION, $result->getDescription());
 		$this->assertStringContainsString('JSignPdf path: ' . $jsignPdfPath, $result->getDescription());
 	}
 }
