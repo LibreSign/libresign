8e09f326ab6949785e7b139c106c29bc244069ac
Vitor Mattos <vitor@php.rio>
2026-09-21 17:41:47 -0300

    test: complete file id authorization contract
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- src/types/openapi/openapi-full.ts
+++ src/types/openapi/openapi-full.ts
@@ -3456,6 +3456,11 @@ export interface operations {
                     };
                 };
             };
+            /** @description Forbidden */
+            403: {
+                headers: { [name: string]: unknown; };
+                content: { "application/json": { ocs: { meta: components["schemas"]["OCSMeta"]; data: unknown; }; }; };
+            };
             /** @description Request failed */
             404: {
                 headers: {
