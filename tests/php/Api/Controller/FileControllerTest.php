8a61d09c295c5f6aefaee42172e31870dcc19d7a
Vitor Mattos <vitor@php.rio>
2026-09-21 18:03:08 -0300

    style: fix import order in file controller test
    Signed-off-by: Vitor Mattos <1079143+vitormattos@users.noreply.github.com>

--- tests/php/Api/Controller/FileControllerTest.php
+++ tests/php/Api/Controller/FileControllerTest.php
@@ -8,8 +8,8 @@ declare(strict_types=1);
 
 namespace OCA\Libresign\Tests\Api\Controller;
 
-use OCA\Libresign\Helper\JSActions;
 use OCA\Libresign\AppInfo\Application;
+use OCA\Libresign\Helper\JSActions;
 use OCA\Libresign\Service\Envelope\EnvelopeService;
 use OCA\Libresign\Tests\Api\ApiTestCase;
 
